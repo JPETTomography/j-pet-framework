45ba2b6ad578083a39965a7f911e2a85d3bd201c
Karol Stola <karolstola@gmail.com>
2013-07-30 16:10:47 +0200

    uzupelnienie JPetSigChannel

--- framework/JPetSigChannel/JPetSigChannel.cpp
+++ framework/JPetSigChannel/JPetSigChannel.cpp
@@ -1,1 +1,11 @@
 #include "JPetSigChannel.h"
+
+JPetSigChannel::JPetSigChannel(bool IsSlow):
+	fIsSlow(IsSlow) {}
+	
+void JPetSigChannel::addChan(float EdgeTime, float FallEdgeTime){
+	SingleChan tmp;
+	tmp[kEdge] = EdgeTime;
+	tmp[kFalling] = FallEdgeTime;
+	fChannels.push_back(tmp);
+}
