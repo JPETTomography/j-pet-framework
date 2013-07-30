4f428919b9103dd4d0bc561a98a2645a7c9ed61a
Karol Stola <karolstola@gmail.com>
2013-07-30 16:10:47 +0200

    uzupelnienie JPetSigChannel

--- framework/JPetSigChannel/JPetSigChannel.h
+++ framework/JPetSigChannel/JPetSigChannel.h
@@ -2,15 +2,24 @@
 #define _JPETSIGCHANNEL_H_
 
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class JPetSigChannel {
 public:
-
+	JPetSigChannel(bool IsSlow);
+	typedef enum EdgeType { kEdge, kFalling };
+	bool isSlow() { return fIsSlow; }
+	float getTime(EdgeType Type) { return fChannels[0][Type]; }
+	float getTime(int ChanNo, EdgeType Type) { return fChannels[ChanNo][Type]; }
+	void addChan(float EdgeTime, float FallEdgeTime);
 private:
+	typedef map < EdgeType, float > SingleChan;
+	typedef vector < SingleChan > ChanSet;
 	//float fAmpl;
-	vector<int[2]> fTimes;
+	 
+	ChanSet fChannels;
 	bool fIsSlow;
 };
 
