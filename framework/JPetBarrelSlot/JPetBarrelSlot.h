962319021cf31b6b93babb4283d9079047c77a46
Karol Stola <karolstola@gmail.com>
2013-07-30 18:37:47 +0200

    dodanie szkieletów klas dla PM, Scin, BarrelSlot, TRB,

---
+++ framework/JPetBarrelSlot/JPetBarrelSlot.h
@@ -0,0 +1,14 @@
+#ifndef _JPETBARRELSLOT_H_
+#define	_JPETBARRELSLOT_H_
+
+class JPetBarrelSlot {
+public:
+
+private:
+	int fSlotId;
+	int fLayerId;
+	int fLayerRad;
+	float SlotTheta;
+};
+
+#endif
