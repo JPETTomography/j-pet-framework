7fb0a542277473421cf0a3bc08445412b20f86ed
Karol Stola <karolstola@gmail.com>
2013-08-07 13:17:57 +0200

    rozszerzenie JPetSigCh

--- framework/JPetSigCh/JPetSigCh.cpp
+++ framework/JPetSigCh/JPetSigCh.cpp
@@ -1,13 +1,17 @@
 #include "JPetSigCh.h"
 
 JPetSigCh::JPetSigCh(float EdgeTime, float FallEdgeTime){
+	fPM = NULL;
+	fTRB = NULL;
+	fScin = NULL;
+	fBarrelSlot = NULL;
 	if (FallEdgeTime == 0) fIsSlow = 1;
 	addCh(EdgeTime, FallEdgeTime);
 }
 	
 void JPetSigCh::addCh(float EdgeTime, float FallEdgeTime){
 	SingleCh tmp;
-	tmp[kEdge] = EdgeTime;
+	tmp[kRising] = EdgeTime;
 	tmp[kFalling] = FallEdgeTime;
 	fChannels.push_back(tmp);
 }
