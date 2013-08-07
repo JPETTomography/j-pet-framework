96e40fc48074aae1ca05e241e4c8e062bdeff8c3
Karol Stola <karolstola@gmail.com>
2013-08-07 13:17:57 +0200

    rozszerzenie JPetSigCh

--- framework/JPetSigCh/JPetSigCh.h
+++ framework/JPetSigCh/JPetSigCh.h
@@ -5,6 +5,9 @@
 #include <map>
 
 #include "../JPetPM/JPetPM.h"
+#include "../JPetBarrelSlot/JPetBarrelSlot.h"
+#include "../JPetScin/JPetScin.h"
+#include "../JPetTRB/JPetTRB.h"
 
 using namespace std;
 
@@ -13,20 +16,23 @@ public:
 	JPetSigCh();
 	JPetSigCh(float EdgeTime, float FallEdgeTime = 0);
 	
-	typedef enum EdgeType { kEdge, kFalling };
+	typedef enum EdgeType { kRising, kFalling };
 	
-	float getTime(EdgeType Type) { return fChannels[0][Type]; }
+	float getTime(EdgeType Type)  { return fChannels[0][Type]; }
 	float getTime(int ChNo, EdgeType Type) { return fChannels[ChNo][Type]; }
 	void addCh(float EdgeTime, float FallEdgeTime);
 	void setPM(JPetPM* PM) { fPM = PM; }
 private:
 	typedef map < EdgeType, float > SingleCh;
 	typedef vector < SingleCh > ChSet;
-	//float fAmpl;
+	float fAmpl;
 	 
 	ChSet fChannels;
 	bool fIsSlow;
 	JPetPM* fPM;
+	JPetTRB* fTRB;
+	JPetScin* fScin;
+	JPetBarrelSlot* fBarrelSlot;
 };
 
 #endif
