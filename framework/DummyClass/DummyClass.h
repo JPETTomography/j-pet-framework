02033a1a5142b69268b0b3375db240ecf8de5832
Wojciech Krzemien <wojciech.krzemien@if.uj.edu.pl>
2013-08-08 13:46:43 +0200

    unit tests exemples added to framework/tests

---
+++ framework/DummyClass/DummyClass.h
@@ -0,0 +1,14 @@
+/**
+ *  @copyright Copyright (c) 2013, Wojciech Krzemien
+ *  @file DummyClass.h 
+ *  @author Wojciech Krzemien, wojciech.krzemien@if.uj.edu.pl
+ *  @brief
+ */ 
+
+#ifndef DUMMYCLASS_H 
+#define DUMMYCLASS_H 
+
+class DummyClass {
+
+};
+#endif /*  !DUMMYCLASS_H */
