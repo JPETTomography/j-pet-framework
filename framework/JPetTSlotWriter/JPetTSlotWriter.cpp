0f165a3f7d01f76eacb32bc1d5cdd33614ef9ec5
Karol Stola <karolstola@gmail.com>
2013-08-14 16:02:37 +0200

    próba zrobienia TSlotWritera

--- framework/JPetTSlotWriter/JPetTSlotWriter.cpp
+++ framework/JPetTSlotWriter/JPetTSlotWriter.cpp
@@ -1,1 +1,18 @@
 #include "JPetTSlotWriter.h"
+
+bool JPetTSlotWriter::write(const vector<JPetTSlot>& obj, const char* file_name) const {
+	TFile *file = new TFile(file_name, "RECREATE");
+	TTree tree;
+	
+	if ( !file->IsOpen() ) {
+		ERROR("Could not write to file.");
+		return false;
+	}
+	
+	tree.Branch("JPetTSlot", "vector<JPetTSlot>", (void *)&obj);
+	
+	tree.Write();
+	
+	file->Close();
+	return true;
+}
