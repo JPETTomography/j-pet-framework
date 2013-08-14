83df5c8f5bf2a83dc42c893b0b4d71cc777e8dfa
Wojciech Krzemien <wojciech.krzemien@if.uj.edu.pl>
2013-08-14 19:50:23 +0200

    przerobienie JPetTestAnalysisModule zeby uzywal nowego Readera usuniecie JPetTestReadera

--- framework/JPetAnalysisModule/JPetAnalysisModule.cpp
+++ framework/JPetAnalysisModule/JPetAnalysisModule.cpp
@@ -1,1 +1,13 @@
 #include "JPetAnalysisModule.h"
+
+JPetAnalysisModule::JPetAnalysisModule(const char* name, const char* title) :
+  TNamed(name, title) 
+{ }
+
+JPetAnalysisModule::JPetAnalysisModule() 
+{ }
+
+JPetAnalysisModule::~JPetAnalysisModule()
+{ } 
+
+
