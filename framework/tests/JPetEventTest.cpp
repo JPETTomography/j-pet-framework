4256a9f82a705093f67eb9a4a99b43bcc0283d70
Damian Trybek <trybek@koza.if.uj.edu.pl>
2013-08-16 09:25:00 +0200

    new file:   framework/tests/JPetEventTest.cpp

---
+++ framework/tests/JPetEventTest.cpp
@@ -0,0 +1,37 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE JPetEventTest
+#include <boost/test/unit_test.hpp>
+
+#define private public
+
+#include "../JPetBarrelSlot/JPetBarrelSlot.h"
+#include "../JPetScin/JPetScin.h"
+#include "../JPetTSlot/JPetTSlot.h"
+#include "../JPetSigCh/JPetSigCh.h"
+#include "../JPetSignal/JPetSignal.h"
+#include "../JPetHit/JPetHit.h"
+#include "../JPetEvent/JPetEvent.h"
+
+#include <cstddef>
+
+//class JPetBarrelSlot;
+//class JPetScin;
+//class JPetTSlot;
+//class JPetSigCh;
+//class JPetSignal;
+//class Vector3;
+//class JPetHit;
+//class JPetEvent;
+
+BOOST_AUTO_TEST_SUITE(FirstSuite)
+  
+BOOST_AUTO_TEST_CASE( default_constructor )
+{
+  JPetEvent event;
+  BOOST_CHECK_EQUAL(event.GetTime(), 0.0f);
+  BOOST_CHECK_EQUAL(event.GetQualityOfTime(), 0.0f);
+  
+  BOOST_CHECK(event.fHits == NULL);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
