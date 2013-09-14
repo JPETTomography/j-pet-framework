93d2166bef82806cfdead26de8b88ab192231875
Karol Stola <karolstola@gmail.com>
2013-09-14 12:46:01 +0200

    dodanie JPetTslotRawWriter

---
+++ framework/JPetTslotRawWriter/JPetTslotRawWriter.cpp
@@ -0,0 +1,11 @@
+/*
+ *  JPetTslotRawWriter.cpp
+ *  
+ *
+ *  Created by Karol Stola on 13-09-14.
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "JPetTslotRawWriter.h"
+
---
+++ framework/JPetTslotRawWriter/JPetTslotRawWriter.h
@@ -0,0 +1,14 @@
+/*
+ *  JPetTslotRawWriter.h
+ *  
+ *
+ *  Created by Karol Stola on 13-09-14.
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "../JPetWriter/JPetWriter.h"
+
+class JPetTslotRawWriter: public JPetWriter{
+
+};
