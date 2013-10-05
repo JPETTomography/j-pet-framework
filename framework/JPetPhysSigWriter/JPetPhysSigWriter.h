f4c4846c4c2be89e76945e9ebd45fe82523ee511
Damian Trybek <damian.trybek@uj.edu.pl>
2013-10-05 11:54:55 +0200

    new file:   framework/JPetPhysSigWriter/JPetPhysSigWriter.cpp 	new file:   framework/JPetPhysSigWriter/JPetPhysSigWriter.h 	new file:   framework/JPetPhysSigWriter/Makefile 	new file:   framework/tests/JPetPhysSigWriterTest.cpp 	modified:   Makefile 	modified:   framework/JPetEvent/Makefile 	modified:   framework/JPetHit/Makefile 	modified:   framework/JPetPhysSigWriter/JPetPhysSigWriter.cpp 	modified:   framework/JPetSignal/JPetSignal.h 	modified:   framework/JPetSignal/Makefile 	modified:   framework/JPetTSlotWriter/JPetTSlotWriter.cpp 	modified:   framework/JPetWriter/JPetWriter.h 	modified:   framework/tests/Makefile

---
+++ framework/JPetPhysSigWriter/JPetPhysSigWriter.h
@@ -0,0 +1,22 @@
+#ifndef _JPETPHYSSIGWRITER_H_
+#define _JPETPHYSDIGWRITER_H_
+
+#include "../JPetWriter/JPetWriter.h"
+#include "../JPetSignal/JPetSignal.h"
+
+#include "TFile.h"
+#include "TTree.h"
+
+#include <cstddef>
+#include <vector>
+
+class JPetPhysSigWriter: public JPetWriter{
+  
+  public:
+ 
+  bool Write (const TNamed& data){}
+  bool Write (const vector<JPetSignal>& obj, const char* filename);
+
+};
+
+#endif
