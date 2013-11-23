cf9285432c8cd7d90719f74861d389fcef5d7245
Karol Stola <karolstola@gmail.com>
2013-11-23 19:58:55 +0100

    rozszerzenie TRB

--- framework/JPetTRB/JPetTRB.cpp
+++ framework/JPetTRB/JPetTRB.cpp
@@ -1,3 +1,19 @@
 #include "JPetTRB.h"
 
 ClassImp(JPetTRB);
+
+void JPetTRB::init(){
+    SetNameTitle("JPetTRB", "Trigger Readout Board");
+}
+
+JPetTRB::JPetTRB(){
+    init();
+}
+
+JPetTRB::JPetTRB(int id, int type, int channel)
+: fID(id)
+, fType(type)
+, fChan(channel)
+{
+    init();
+}
