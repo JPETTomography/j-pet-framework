a598e42b954015310dfb94531db14b9b9d828629
Damian Trybek <damian.trybek@uj.edu.pl>
2013-11-28 00:48:09 +0100

    modified:   ../framework/JPetWriter/JPetWriter.cpp 	modified:   ../framework/JPetWriter/JPetWriter.h 	new file:   ../framework/JPetWriter/JPetWriterLinkDef.h 	modified:   ../framework/JPetWriter/Makefile 	modified:   JPetAnalysisModuleA.cpp 	modified:   Makefile 	modified:   main.cpp

--- framework/JPetWriter/JPetWriter.cpp
+++ framework/JPetWriter/JPetWriter.cpp
@@ -20,46 +20,9 @@ JPetWriter::JPetWriter(const char* file_name)
     }
 }
 
-bool JPetWriter::Write(const TNamed& obj){
-    vector<TNamed> wrapper;
-    wrapper.push_back(obj);
-    Write(wrapper);
+JPetWriter::~JPetWriter() {
 }
 
 void JPetWriter::CloseFile(){
     if (fFile.IsOpen() ) fFile.Close();
-}
-
-bool JPetWriter::Write( vector<TNamed>& obj) {
-	
-    if (obj.size() == 0) {
-        WARNING("Vector passed is empty");
-        return false;
-    }
-    
-    if ( !fFile.IsOpen() ) {
-		ERROR("Could not write to file. Have you closed it already?");
-		return false;
-	}
-    
-    fFile.cd(fFileName.c_str()); // -> http://root.cern.ch/drupal/content/current-directory
-    
-    TTree tree;
-	
-	TNamed* filler = &obj[0];
-    
-	tree.Branch(filler->GetName(), filler->GetName(), &filler);
-	
-	for (int i = 0; i < obj.size(); i++){
-        filler = &obj[i]; 
-		tree.Fill();      
-	}
-	
-	tree.Write();
-    
-	return true;
-}
-
-JPetWriter::~JPetWriter(){
-    CloseFile();
 }
