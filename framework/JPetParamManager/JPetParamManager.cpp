d3d3ca6261390f11235ebd538c6e4f6b1838c9a9
Karol Stola <karolstola@gmail.com>
2013-12-05 12:40:17 +0100

    ParamManager po testach

--- framework/JPetParamManager/JPetParamManager.cpp
+++ framework/JPetParamManager/JPetParamManager.cpp
@@ -4,18 +4,22 @@ void JPetParamManager::readFile(const char * file_name){
 	assert(file_name != NULL);
 	
 	string line;
-	istringstream iss(line);
 	
 	ifstream file(file_name);
+	if( ! file.is_open() ){
+		ERROR("No such file!");
+		return;
+	}
 	
 	while ( !file.eof() ){
-		int trb, scin;
+		int trb = 0, scin = 0;
+		
 		getline(file, line);
+		istringstream iss(line);
+		
 		iss >> trb;
 		iss >> scin;
 		fTRBNumbers.push_back(trb);
 		fScinNumbers.push_back(scin);
 	}
-	
-	
 }
