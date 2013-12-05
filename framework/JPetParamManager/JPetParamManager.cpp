d4f2b29e41c11349f188106d21fd87825dba1c42
Karol Stola <karolstola@gmail.com>
2013-12-05 11:33:00 +0100

    dodanie odczytywania pliku - jeszcze nie testowane

--- framework/JPetParamManager/JPetParamManager.cpp
+++ framework/JPetParamManager/JPetParamManager.cpp
@@ -1,1 +1,21 @@
 #include "JPetParamManager.h"
+
+void JPetParamManager::readFile(const char * file_name){
+	assert(file_name != NULL);
+	
+	string line;
+	istringstream iss(line);
+	
+	ifstream file(file_name);
+	
+	while ( !file.eof() ){
+		int trb, scin;
+		getline(file, line);
+		iss >> trb;
+		iss >> scin;
+		fTRBNumbers.push_back(trb);
+		fScinNumbers.push_back(scin);
+	}
+	
+	
+}
