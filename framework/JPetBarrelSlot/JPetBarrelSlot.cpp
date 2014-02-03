799ff869c05301a0880383b406236f6b773b8b12
Wojciech Krzemien <wojciech.krzemien@if.uj.edu.pl>
2014-02-04 00:53:11 +0100

    modified JPetBarrelSlot and added tests

--- framework/JPetBarrelSlot/JPetBarrelSlot.cpp
+++ framework/JPetBarrelSlot/JPetBarrelSlot.cpp
@@ -1,3 +1,16 @@
 #include "JPetBarrelSlot.h"
-
 ClassImp(JPetBarrelSlot);
+
+JPetBarrelSlot::JPetBarrelSlot():
+  fSlotID(0),
+  fLayerID(0),
+  fLayerRad(0),
+  fSlotTheta(0.)
+{ /* */ }
+
+JPetBarrelSlot::JPetBarrelSlot(int slotID, int layerID, int layerRad, float slotTheta):
+  fSlotID(slotID),
+  fLayerID(layerID),
+  fLayerRad(layerRad),
+  fSlotTheta(slotTheta)
+{ /* */ }
