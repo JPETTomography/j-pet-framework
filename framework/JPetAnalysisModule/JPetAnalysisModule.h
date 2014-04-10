a4a6ec3351ec70f6227211a22266f3283433d2e3
Karol Stola <karolstola@gmail.com>
2014-04-10 11:27:12 +0200

    prototype of JPAnalysisModule communication

--- framework/JPetAnalysisModule/JPetAnalysisModule.h
+++ framework/JPetAnalysisModule/JPetAnalysisModule.h
@@ -8,12 +8,14 @@
 #ifndef JPETANALYSISMODULE_H 
 #define JPETANALYSISMODULE_H
 
-#include "TNamed.h" 
+#include <vector>
+#include "TNamed.h"
+#include "TTree.h"
 
 class JPetAnalysisModule: public TNamed {
  public:
   JPetAnalysisModule();
-  JPetAnalysisModule(const char* name, const char* title); 
+  JPetAnalysisModule(const char* name, const char* title, TTree * shared_tree = NULL);
   virtual ~JPetAnalysisModule(); 
   virtual void CreateInputObjects(const char* inputFilename=0)=0; //
   virtual void CreateOutputObjects(const char* outputFilename=0)=0; //
@@ -22,5 +24,9 @@ class JPetAnalysisModule: public TNamed {
   virtual void Terminate()=0; // called once when analysis terminates 
 
   ClassDef(JPetAnalysisModule,1);
+private:
+  TTree fSubmoduleSharedTree;
+  TTree* fSuperSharedTree;
+  std::vector< JPetAnalysisModule* > fSubmodules;
 };
 #endif /*  !JPETANALYSISMODULE_H */
