2dedbdf5d90961517cc3095fa7869d67ecfcf306
Karol Stola <karolstola@gmail.com>
2014-04-10 11:27:12 +0200

    prototype of JPAnalysisModule communication

--- framework/JPetAnalysisModule/JPetAnalysisModule.cpp
+++ framework/JPetAnalysisModule/JPetAnalysisModule.cpp
@@ -2,14 +2,19 @@
 
 ClassImp(JPetAnalysisModule);
 
-JPetAnalysisModule::JPetAnalysisModule(const char* name, const char* title) :
-  TNamed(name, title) 
+JPetAnalysisModule::JPetAnalysisModule(const char* name, const char* title, TTree * shared_tree = NULL)
+  : TNamed(name, title)
+  , fSuperSharedTree( shared_tree )
 { }
 
 JPetAnalysisModule::JPetAnalysisModule() 
 { }
 
 JPetAnalysisModule::~JPetAnalysisModule()
-{ } 
+{
+	for (int i = 0; i < fSubmodules.size(); i++)
+		delete fSubmodules[i];
+	
+}
 
 
