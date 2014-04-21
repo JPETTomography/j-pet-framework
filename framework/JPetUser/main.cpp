bd89f0f6de370fd3b28627d4266bfebea6e406ae
andrzej <agruntek@wp.pl>
2014-04-21 17:31:16 +0200

    Add classes : Functions, JPetFrame, JPetKB, JPetLayer, JPetSetup, JPetSlot, JPetTOMB, JPetUser.

---
+++ framework/JPetUser/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../JPetUser/JPetUser.h"
+
+using namespace std;
+
+
+int main()
+{
+JPetUser l_pet;
+l_pet.time();
+return 0;
+}
+
+// g++ JPetUser.cpp main.cpp -o main
