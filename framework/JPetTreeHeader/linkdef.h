bfa86190f2d77d56b16882e85a66ff83fa259231
alek gajos <alek.gajos@gmail.com>
2014-05-07 15:52:33 +0200

    Fixes in usage of the tree headers.

---
+++ framework/JPetTreeHeader/linkdef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+//tutaj idzie lista klas
+#pragma link C++ class JPetTSlotRawHeader;
+#pragma link C++ class JPetTreeHeader;
+//#pragma link C++ class Event+;
+//#pragma link C++ class TDCHit+;
+//#pragma link C++ class ADCHit+;
+//#pragma link C++ class Sig+;
+#endif
