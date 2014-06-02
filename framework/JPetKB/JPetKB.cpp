40e089666cf0421bf395bdc7ddb2b2b0feeab9a3
andrzej <agruntek@wp.pl>
2014-06-02 02:19:00 +0200

    Add new functionalit\ies. Refactor code.

--- framework/JPetKB/JPetKB.cpp
+++ framework/JPetKB/JPetKB.cpp
@@ -5,80 +5,75 @@
 ClassImp(JPetKB);
 
 
-JPetKB::JPetKBInput::JPetKBInput(int p_KBId) : JPetKBChannel(), m_KBId(p_KBId)
-{
-}
+JPetKB::JPetKBChannel::JPetKBChannel(int p_id, 
+				      bool p_isActive, 
+				      std::string p_status, 
+				      int p_portNumber, 
+				      std::string p_description) :
+								  fId(p_id),
+								  fIsActive(p_isActive),
+								  fStatus(p_status),
+								  fPortNumber(p_portNumber),
+								  fDescription(p_description)
+{}
 
-JPetKB::JPetKBOutput::JPetKBOutput(bool p_passedInformationIsTime,
+JPetKB::JPetKBInput::JPetKBInput(int p_id,
+				  bool p_isActive, 
+				  std::string p_status, 
+				  int p_portNumber, 
+				  std::string p_description,
+				  int p_KBId) :
+					      JPetKBChannel(p_id, 
+							    p_isActive, 
+							    p_status, 
+							    p_portNumber, 
+							    p_description), 
+					      fKBId(p_KBId)
+{}
+
+JPetKB::JPetKBOutput::JPetKBOutput(int p_id,
+				    bool p_isActive, 
+				    std::string p_status, 
+				    int p_portNumber, 
+				    std::string p_description,
+				    bool p_passedInformationIsTime,
 				    std::string p_passedInformation, 
 				    int p_KBId, 
 				    int p_inputId, 
 				    int p_KBInputId) : 
-						      JPetKBChannel(),
-						      m_passedInformationIsTime(p_passedInformationIsTime),
-						      m_passedInformation(p_passedInformation),
-						      m_KBId(p_KBId),
-						      m_inputId(p_inputId),
-						      m_KBInputId(p_KBInputId)
-{
-}
-
+						      JPetKBChannel(p_id, 
+								    p_isActive, 
+								    p_status, 
+								    p_portNumber, 
+								    p_description),
+						      fPassedInformationIsTime(p_passedInformationIsTime),
+						      fPassedInformation(p_passedInformation),
+						      fKBId(p_KBId),
+						      fInputId(p_inputId),
+						      fKBInputId(p_KBInputId)
+{}
 
 JPetKB::JPetKB() :
-		  m_id(0),
-		  m_isActive(false),
-		  m_status(""),
-		  m_description(""),
-		  m_version(0),
-		  m_userId(0)
-{
-}
+		  fId(0),
+		  fIsActive(false),
+		  fStatus(""),
+		  fDescription(""),
+		  fVersion(0)
+{}
 
 JPetKB::JPetKB(int p_id, 
 	       bool p_isActive, 
 	       std::string p_status, 
 	       std::string p_description, 
-	       int p_version, 
-	       int p_userId) :
-			      m_id(p_id),
-			      m_isActive(p_isActive),
-			      m_status(p_status),
-			      m_description(p_description),
-			      m_version(p_version),
-			      m_userId(p_userId)
-{
-}
+	       int p_version,
+	       const JPetUser &p_user) :
+			      fId(p_id),
+			      fIsActive(p_isActive),
+			      fStatus(p_status),
+			      fDescription(p_description),
+			      fVersion(p_version),
+			      fUser(p_user)
+{}
 
 JPetKB::~JPetKB()
-{
-}
-
-int JPetKB::id() const
-{
-  return m_id;
-}
-
-bool JPetKB::isActive() const
-{
-  return m_isActive;
-}
-
-std::string JPetKB::status() const
-{
-  return m_status;
-}
-
-std::string JPetKB::description() const
-{
-  return m_description;
-}
-
-int JPetKB::version() const
-{
-  return m_version;
-}
-
-int JPetKB::userId() const
-{
-  return m_userId;
-}
+{}
