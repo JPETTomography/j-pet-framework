aad629e2687dc63893da157f79716de9ae5c4e9e
alek gajos <alek.gajos@gmail.com>
2014-07-28 20:20:17 +0000

    Simplified the JPetTreeHeader class and added headers to all stages of example analysis.

--- framework/JPetTreeHeader/linkdef.h
+++ framework/JPetTreeHeader/linkdef.h
@@ -3,10 +3,5 @@
 #pragma link off all classes;
 #pragma link off all functions;
 //tutaj idzie lista klas
-#pragma link C++ class JPetTSlotRawHeader;
 #pragma link C++ class JPetTreeHeader;
-//#pragma link C++ class Event+;
-//#pragma link C++ class TDCHit+;
-//#pragma link C++ class ADCHit+;
-//#pragma link C++ class Sig+;
 #endif
