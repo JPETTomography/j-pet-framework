88cba58c980480e61f5fde204cc3e64ace3e3d09
alek gajos <alek.gajos@gmail.com>
2014-07-30 23:55:16 +0000

    Changed pointer members to regular variable members in JPetHit as pointers seem to be wrongly written to ROOT files.

--- framework/JPetHit/JPetHit.cpp
+++ framework/JPetHit/JPetHit.cpp
@@ -12,7 +12,7 @@ ClassImp(JPetHit);
 
 JPetHit::JPetHit() :
     TNamed("JPetHit","Hit Structure"), fEnergy(0.0f), fQualityOfEnergy(0.0f), fTime(0.0f),
-    fQualityOfTime(0.0f), fPos(new TVector3()),
+    fQualityOfTime(0.0f),
     fBarrelSlot(NULL), fScintillator(NULL) {
 
 }
@@ -20,7 +20,7 @@ JPetHit::JPetHit() :
 JPetHit::JPetHit(float e, float qe, float t, float qt, TVector3& pos, JPetSignal& lsig, JPetSignal& rsig, 
                   JPetBarrelSlot& bslot, JPetScin& scin) : 
     TNamed("JPetHit","Hit Structure") ,fEnergy(e), fQualityOfEnergy(qe), fTime(t),
-    fQualityOfTime(qt), fPos(&pos), fLeftSignal(lsig), fRightSignal(rsig), fBarrelSlot(&bslot), fScintillator(&scin) {
+    fQualityOfTime(qt), fPos(pos), fLeftSignal(lsig), fRightSignal(rsig), fBarrelSlot(&bslot), fScintillator(&scin) {
 }
 
 JPetHit::~JPetHit() {
