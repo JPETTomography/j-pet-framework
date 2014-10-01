0b2edc6452bf28c5254c71aca190568f880ed45d
alek gajos <alek.gajos@gmail.com>
2014-10-01 10:54:05 +0200

    Fixed and updated JPetReaderTest.

--- framework/tests/JPetReaderTest/JPetReaderTest.cpp
+++ framework/tests/JPetReaderTest/JPetReaderTest.cpp
@@ -4,9 +4,10 @@
 
 #define private public
 #define protected public
+
 #include "../../JPetReader/JPetReader.h"
 #include "../../JPetWriter/JPetWriter.h"
-#include "../../JPetSigCh/JPetSigCh.h"
+#include "../../JPetSignal/JPetSignal.h"
 
 #include <cstddef>
 #include <iostream>
@@ -15,7 +16,24 @@
 #include <TError.h> /// gErrorIgnoreLevel
 #include <TObjString.h>
 
+// method list
+/*
+  JPetReader(void);
+  explicit JPetReader(const char* p_filename);
+  virtual ~JPetReader(void);
+  virtual void CloseFile();
+  virtual long long GetEntries () const { return fTree->GetEntries(); }
+  virtual int GetEntry (int entryNo);
+  virtual bool OpenFile(const char* filename);
+  virtual void ReadData(const char* objname = "");
+  virtual TNamed& GetData () {return *fObject;}
+  JPetTreeHeader * GetHeaderClone()const;
+  void fillContainer(std::vector<T> &p_container, const std::string &p_objectName);
+  virtual void closeTFile(void){};
+*/
+
 BOOST_AUTO_TEST_SUITE (FirstSuite)
+
 
 BOOST_AUTO_TEST_CASE (default_constructor)
 {
@@ -24,7 +42,6 @@ BOOST_AUTO_TEST_CASE (default_constructor)
   BOOST_CHECK(reader.fFile == NULL);
   BOOST_CHECK(reader.fObject == 0);
   BOOST_CHECK(reader.fTree == 0);
-
 }
 
 BOOST_AUTO_TEST_CASE (bad_file)
@@ -43,22 +60,37 @@ BOOST_AUTO_TEST_CASE (bad_file)
 
 BOOST_AUTO_TEST_CASE (open_file)
 {
-    //std::cout << "test if open constructor works" << std::endl;
-    JPetReader constructor_open("test.root");
-    constructor_open.ReadData("");
-    constructor_open.CloseFile();
-    
-    //std::cout << "test if OpenFile works" << std::endl;
-    JPetReader reader;
-    BOOST_CHECK( reader.OpenFile("test.root") );
-    reader.ReadData("");
-    reader.CloseFile();
+  //std::cout << "test if open constructor works" << std::endl;
+  JPetReader constructor_open("test.root");
+  constructor_open.CloseFile();
+  
+  //std::cout << "test if OpenFile works" << std::endl;
+  JPetReader reader;
+  BOOST_CHECK( reader.OpenFile("test.root") );
+  reader.ReadData("");
+  reader.CloseFile();
+}
+
+BOOST_AUTO_TEST_CASE (read_and_write_objects )
+{
+  JPetWriter writer("test2.root");
+  JPetSignal signal;
+  signal.SetTime(101.43);
+  writer.Write( signal );
+  writer.CloseFile();
+  
+  JPetReader reader("test2.root");
+  BOOST_CHECK_EQUAL(reader.GetEntries(), 1);
+  reader.GetEntry(0);
+  JPetSignal & signal2 = (JPetSignal&)reader.GetData();
+  reader.CloseFile();
+  BOOST_CHECK_EQUAL(signal.GetTime(), signal2.GetTime());
 }
 
 BOOST_AUTO_TEST_CASE (proper_file)
 {
   JPetReader reader;
-  bool openedPropery = reader.OpenFile("./phys.sig.root");
+  bool openedPropery = reader.OpenFile("test2.root");
   BOOST_CHECK(openedPropery );
   if (openedPropery) {
     reader.ReadData("");
@@ -68,7 +100,7 @@ BOOST_AUTO_TEST_CASE (proper_file)
     BOOST_CHECK(reader.fTree != 0);
 
     BOOST_CHECK(reader.GetEntries() > 0);
-    BOOST_CHECK(reader.GetEntry(1) > 0);
+    BOOST_CHECK(reader.GetEntry(0) > 0);
 
     reader.CloseFile();
 
@@ -80,16 +112,16 @@ BOOST_AUTO_TEST_CASE (proper_file)
 
 BOOST_AUTO_TEST_CASE (read_and_write_header )
 {
-    TObjString input_string("Ala ma kota");
-    JPetWriter writer("test_file.root");
-    writer.WriteHeader(&input_string);
-    JPetSigCh signal;
-    writer.Write(signal);
-    writer.CloseFile();
-    
-    JPetReader reader("test_file.root");
-    //TObjString* output_string = static_cast<TObjString *>(reader.GetHeader());
-    //BOOST_CHECK(input_string.Compare(output_string) == 0);
+  JPetTreeHeader input_header(43, "test", 1);
+  JPetWriter writer("test3.root");
+  writer.WriteHeader(&input_header);
+  JPetTSlot tslot;
+  writer.Write(tslot);
+  writer.CloseFile();
+  
+  JPetReader reader("test3.root");
+  JPetTreeHeader* output_header = reader.GetHeaderClone();
+  BOOST_CHECK_EQUAL(input_header.GetRunNumber(), output_header->GetRunNumber());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
