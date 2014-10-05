7afa6110e4b6d7495f4b31b1c8d767209d87e850
Wojciech Krzemien <wojciech.krzemien@if.uj.edu.pl>
2014-10-05 14:59:49 +0200

    refactorization of JPetTSlot: removed index variable with setters and getters correction of JPetTSlot Tests

--- framework/tests/JPetTSlotTest/JPetTSlotTest.cpp
+++ framework/tests/JPetTSlotTest/JPetTSlotTest.cpp
@@ -3,35 +3,57 @@
 #include <boost/test/unit_test.hpp>
 
 #define private public
+#include "../../JPetSigCh/JPetSigCh.h"
 #include "../../JPetTSlot/JPetTSlot.h"
 
 /// @todo update methods tests are outdated
 //#include <TError.h> /// gErrorIgnoreLevel
 //  gErrorIgnoreLevel = 7000;
+
+
+/// class JPetTSlot:
+///  JPetTSlot();
+///  void AddCh(JPetSigCh& new_ch);
+///  inline size_t size() const ;
+///  inline size_t getNumberOfSigCh() const;
+///  inline const TClonesArray & getSigChVect() const;
+///  inline JPetSigCh & operator[](int i) const;
+///  virtual ~JPetTSlot();
+
+
+
 BOOST_AUTO_TEST_SUITE(FirstSuite)
 
+BOOST_AUTO_TEST_CASE( default_constructor )
+{
+  JPetTSlot test;
+  BOOST_REQUIRE(test.size() == 0);
+  BOOST_REQUIRE(test.getNumberOfSigCh() == 0);
+  BOOST_REQUIRE(test.getSigChVect().GetEntries() == 0);
 
-//BOOST_AUTO_TEST_CASE( first )
-//{
-//  JPetTSlot test;
-//  JPetSigCh ch_test(1.3, 1.2), ch_test2(1, 0), ch_test3(123, 98);
-//  test.addCh(ch_test);
-//  test.addCh(ch_test2);
-//  test.addCh(ch_test3);
-//
-//
-//  JPetTSlot test2(test.getSigChVect());
-//
-//  for (size_t i = 0; i < test.size() || i < test2.size() ; i++) {
-//    BOOST_CHECK_EQUAL(test[i].getTime(JPetSigCh::Trailing), test2[i].getTime(JPetSigCh::Trailing));
-//    BOOST_CHECK_EQUAL(test[i].getTime(JPetSigCh::Leading), test2[i].getTime(JPetSigCh::Leading));
-//  }
-//}
-//
-//BOOST_AUTO_TEST_CASE( second )
-//{
-//  JPetTSlot test;
-//  JPetTSlot test2(test.getSigChVect());
-//}
+}
+
+BOOST_AUTO_TEST_CASE( some_channels )
+{
+  JPetTSlot test;
+  JPetSigCh ch_test(JPetSigCh::Trailing, 1.2), ch_test2(JPetSigCh::Leading, 1.5), ch_test3(JPetSigCh::Charge, 98);
+  test.AddCh(ch_test);
+  test.AddCh(ch_test2);
+  test.AddCh(ch_test3);
+
+  BOOST_REQUIRE(test.size() == 3);
+  BOOST_REQUIRE(test.getNumberOfSigCh() == 3);
+  BOOST_REQUIRE(test.getSigChVect().GetEntries() == 3);
+  double epsilon = 0.001;
+  BOOST_REQUIRE_CLOSE(test[0].GetValue(), 1.2, epsilon);
+  BOOST_REQUIRE_CLOSE(test[1].GetValue(), 1.5, epsilon);
+  BOOST_REQUIRE_CLOSE(test[2].GetValue(), 98, epsilon);
+
+  TClonesArray array = test.getSigChVect();
+  BOOST_REQUIRE_CLOSE(static_cast<JPetSigCh*>(array.At(0))->GetValue(), 1.2, epsilon);
+  BOOST_REQUIRE_CLOSE(static_cast<JPetSigCh*>(array.At(1))->GetValue(), 1.5, epsilon);
+  BOOST_REQUIRE_CLOSE(static_cast<JPetSigCh*>(array.At(2))->GetValue(), 98, epsilon);
+
+}
 
 BOOST_AUTO_TEST_SUITE_END()
