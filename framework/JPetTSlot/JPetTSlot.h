7c22ac67d6bb036e22383c21a87ba011bdb97b66
alek gajos <alek.gajos@gmail.com>
2014-10-06 16:10:19 +0200

    Restored GetIndex() and SetIndex() methods deleted from JPetTSlot

--- framework/JPetTSlot/JPetTSlot.h
+++ framework/JPetTSlot/JPetTSlot.h
@@ -28,10 +28,10 @@ public:
   inline size_t size() const {
     return fSigChannels.GetEntries();
   }
-  inline size_t getNumberOfSigCh() const {
+  inline size_t GetNumberOfSigCh() const {
     return fSigChannels.GetEntries();
   }
-  inline const TClonesArray& getSigChVect() const {
+  inline const TClonesArray& GetSigChVect() const {
     return fSigChannels;
   }
   /**
@@ -48,10 +48,22 @@ public:
     //    fSigChannels.Clear("C");
   }
 
+  /**
+   * @brief Get the index number of this TSlot
+   *
+   * Each TSlot (time window) in a HLD file is assigned an index number, counting from first TSlot in the file. This number may be useful if empty TSlots are skipped during analysis.
+   * @oaram index a squential number of this TSlot counting from sirst TSlot in a HLD file
+   */
+  inline unsigned int GetIndex() const { return fIndex; }
+
+  inline void SetIndex(unsigned int index) { fIndex = index; }
+
   ClassDef(JPetTSlot, 1);
+
 private:
   TClonesArray fSigChannels; 
   int fSize;
+  unsigned int fIndex; ///< sequential number of this TSlot in the HLD file
 };
 
 #endif
