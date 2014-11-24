9ad577bc17ae4cd5d358d36b9d0223932165fa86
alek gajos <alek.gajos@gmail.com>
2014-11-24 17:44:09 +0100

    Updated JPetTreeHeader to contain processing history and source position (closes #558).
    Former-commit-id: 15eb19dde7db5ad3264346cf008d2c35ba1dd1c0

--- framework/JPetTreeHeader/JPetTreeHeader.h
+++ framework/JPetTreeHeader/JPetTreeHeader.h
@@ -12,6 +12,15 @@
 #include <ostream>
 #include <vector>
 
+struct ProcessingStageInfo
+{
+  TString fModuleName;
+  TString fModuleDescription;
+  int fModuleVersion;
+  TString fCreationTime;
+
+};
+
 /**
  * @brief A class representing a header with info on JPet ROOT Trees.
  *
@@ -19,75 +28,39 @@
  */
 class JPetTreeHeader: public TObject{
  public:
-  JPetTreeHeader() {};
-  JPetTreeHeader( int run, TString producer = "", int version = -1 );
-  void Print(Option_t* opt = "") const {
-    std::cout << this->Stringify();
-  }
+  JPetTreeHeader();
+  JPetTreeHeader(int run);
+  void Print(Option_t* opt = "") const { std::cout << this->stringify(); }
+
   /// produce a TString edscribing all the information dontained in this object
-  TString Stringify() const;
+  TString stringify() const;
 
-  // Get name of the mudule which produced this tree
-  inline const TString & GetProducerName() const { return fProducerName; }
-  // Get version of the mudule which produced this tree
-  inline int GetProducerVersion()  const { return fProducerVersion; }
-  // Get number if the run from whose data this tree was produced
-  inline int GetRunNumber()  const { return fRunNo; }
+  inline int getRunNumber()  const { return fRunNo; }
+  inline void setRunNumber(int p_run_no) { fRunNo = p_run_no; }
   
-  // Set name of the mudule which produced this tree
-  inline void SetProducerName(TString name) { fProducerName = name; }
-  // Set version of the mudule which produced this tree
-  inline void SetProducerVersion(int version) { fProducerVersion = version; }
+  inline TString getBaseFileName() const {return fBaseFilename;}
+  inline void setBaseFileName(const char * p_name){ fBaseFilename = p_name; }
   
-  /**
-   * @brief Add information on a raw HLD file used in this run
-   *
-   * @param name a string with the filename of the raw HLD file
-   * @param id ID of the run
-   * @param nEvts number of time windows contained in the raw HLD file
-   * @param width width of the DAQ time window in picoseconds
-   * @param firstEvt number if the first TSlot belonging to this file
-   * @param startT absolute starting time of the measurement constained in this file
-   *
-   * It is possible to produce one ROOT Tree with porocessed data from multiple HLD files. In such a case, this method should be called for each file used. The events from multiple files would be put subsequently into one tree, and then the startT and firstEvt values should be provided. 
-   *
-   */
-  int addHLDinfo(TString name, int id, int nEvts, float width, 
-		 int firstEvt=0., double startT=0.);
-  /// Get the number of HLD files used to produce the Tree described byb this header
-  int getNoHLDfiles() { return fHLDnames.size(); }
-  
-  /// Get file name of HLD file number no
-  TString getHLDname(int no) const;
-  /// Get run ID for HLD file number no
-  int getHLDid(int no) const;
-  /// Get number of events in HLD file number no
-  int getHLDnEvts(int no) const;
-  /// Get the width of time window for HLD file number no, in picoseconds
-  float getHLDwindowWidth(int no) const;
-  int getHLDfirstEvt(int no) const;
-  double getHLDstartTime(int no) const;
+  /// set source position in mm or -1 of no source was used
+  inline double getSourcePosition() const { return fSourcePosition; }
+  /// get source position in mm; -1 means no source was used
+  inline void setSourcePosition(double p_pos) { fSourcePosition = p_pos; }
 
-  ClassDef(JPetTreeHeader, 1);
+  /// add information on one processing stage, i.e. one module
+  int addStageInfo(TString name, TString title, int version, TString time_stamp);
+  int getStagesNb()const{ return fStages.size(); }
+
+  const ProcessingStageInfo & getProcessingStageInfo(int i)const{ return fStages.at(i); }
+
+  ClassDef(JPetTreeHeader, 2);
 
 protected:
-  TString fProducerName;
-  int fProducerVersion;
   int fRunNo;
+  TString fBaseFilename;
+  double fSourcePosition;
 
-  std::vector<TString> fHLDnames;
-  std::vector<int> fHLDids;
-  std::vector<float> fHLDwinWidths;
-  std::vector<int> fHLDnEvts;
-  std::vector<int> fHLDfirstEvt;
-  std::vector<double> fHLDstartTimes;
+  std::vector<ProcessingStageInfo> fStages;
 
 };
-
-
-/* ostream& operator<<(ostream& os, const JPetTreeHeader& header){ */
-/*   os << header.Stringify(); */
-/*   return os; */
-/* } */
 
 #endif
