22c8da9c2b670d08ad6d8a35198e2675527a5eff
alek gajos <alekgajos@gmail.com>
2014-12-17 13:41:57 +0100

    Added the missing JPetCommonAnalysisModule files. Fixed a problem with filanames stored as TString.
    Former-commit-id: 154be1572486cf3a189e08e2c81bfa818f2932e8

---
+++ framework/JPetCommonAnalysisModule/JPetCommonAnalysisModule.cpp
@@ -0,0 +1,110 @@
+#include "../JPetManager/JPetManager.h"
+
+#include "JPetCommonAnalysisModule.h"
+#include "TSystem.h"
+
+ClassImp(JPetCommonAnalysisModule);
+
+/*
+JPetCommonAnalysisModule::JPetCommonAnalysisModule():
+  JPetAnalysisModule(),
+  fEventNb(0)
+{
+  gSystem->Load("libTree");
+}
+*/
+
+JPetCommonAnalysisModule::JPetCommonAnalysisModule(const char* name, const char* title,
+				       const char * in_file_suffix,
+				       const char * out_file_suffix):
+  JPetAnalysisModule(name, title),
+  fRunNb(0),
+  fEventNb(0),
+  fEvent(0),
+  fWriter(0),
+  fReader(0),
+  fHeader(0),
+  fInFileName(""),
+  fOutFileName(""),
+  fInFileSuffix(in_file_suffix),
+  fOutFileSuffix(out_file_suffix)//,
+  //  fParamManager(JPetManager::GetManager().getParamManagerInstance())
+{
+  gSystem->Load("libTree");
+}
+
+
+JPetCommonAnalysisModule::~JPetCommonAnalysisModule()
+{
+  if (fWriter != NULL) delete fWriter;
+  if (fReader != NULL) delete fReader;
+}
+
+void JPetCommonAnalysisModule::createInputObjects(const char* inputFilename)
+{
+  fInFileName = inputFilename;
+  fInFileName += ".";
+  fInFileName += fInFileSuffix;
+  
+  // create the JPetReader and load the tree
+  fReader = new JPetReader();
+  if ( fReader->openFile( fInFileName.c_str() ) == 0) {
+    std::cerr << fName << ": Unable to open the input file. Exiting." << std::cerr;
+    exit(-1);
+  }
+  fReader->readData();
+  fEventNb = fReader->getEntries();
+
+  INFO( Form("Starting %s.", GetName() ) );
+  
+  //fParamManager.readParametersFromFile( fReader );
+  JPetManager::GetManager().getParamManagerInstance().readParametersFromFile(fReader);
+
+  // read the header from the previous analysis stage
+  fHeader = fReader->getHeaderClone();
+  // add info about this module to the processing stages' history in Tree header
+  fHeader->addStageInfo( this->GetName(), this->GetTitle(), MODULE_VERSION, 
+			 JPetManager::GetManager().GetTimeString() );
+
+  
+ }
+
+ void JPetCommonAnalysisModule::createOutputObjects(const char* outputFilename)
+ {
+   fOutFileName = outputFilename;
+   fOutFileName += ".";
+   fOutFileName += fOutFileSuffix;
+
+   fEvent = 0;
+   fWriter = new JPetWriter( fOutFileName.c_str() );
+
+   // call user function before starting processing
+   begin();
+ }
+
+
+ void JPetCommonAnalysisModule::terminate()
+ {
+   // cal user function before terminating processing
+   end();
+
+   INFO( Form("Finished processing %s.", GetName() ) );
+   fWriter->writeHeader( fHeader ); 
+   fWriter->writeObject( getStatsObjects(), "Stats" );
+
+   // rewrite the parametric objects from input file to output file
+   JPetManager::GetManager().getParamManagerInstance().saveParametersToFile(fWriter);
+   JPetManager::GetManager().getParamManagerInstance().clearParameters();
+   //   fParamManager.saveParametersToFile( fWriter );
+   //   fParamManager.clearParameters();  
+
+   fWriter->closeFile();
+   fReader->closeFile();
+
+}
+
+
+const JPetParamBank & JPetCommonAnalysisModule::getParamBank(){
+  return JPetManager::GetManager().getParamManagerInstance().getParamBank();
+}
+
