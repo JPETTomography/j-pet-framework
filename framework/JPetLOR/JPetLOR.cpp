a2778330ee2535a763bcfd304d6d0d3b3ad2281c
alek gajos <alek.gajos@gmail.com>
2015-02-11 18:01:30 +0100

    Removed addHit method from JPetLOR. fixes #587, refs #612
    Former-commit-id: d5954366aaaf6911158b53c5d91f6dd4c9b16a50

--- framework/JPetLOR/JPetLOR.cpp
+++ framework/JPetLOR/JPetLOR.cpp
@@ -4,44 +4,29 @@
  * @copyright Copyright (c) 2013, Damian Trybek
  */
 
-
 #include "./JPetLOR.h"
 
 ClassImp(JPetLOR);
 
 JPetLOR::JPetLOR() :
     TNamed("JPetLOR", "Event Structure"), fTime(0.0f), fQualityOfTime(0.0f),
-    fTimeDiff(0.0f), fQualityOfTimeDiff(0.0f), fNhits(0)
+    fTimeDiff(0.0f), fQualityOfTimeDiff(0.0f)
+{
+  fIsHitSet[0] = false;
+  fIsHitSet[1] = false;
+}
+
+JPetLOR::JPetLOR(float Time, float QualityOfTime, JPetHit& firstHit,
+                 JPetHit& secondHit) :
+    TNamed("JPetLOR", "Event Structure"), fTime(Time),
+    fQualityOfTime(QualityOfTime), fFirstHit(firstHit), fSecondHit(secondHit),
+    fTimeDiff(0.0f), fQualityOfTimeDiff(0.0f)
+{
+  fIsHitSet[0] = true;
+  fIsHitSet[1] = true;
+}
+
+JPetLOR::~JPetLOR()
 {
 }
 
-JPetLOR::JPetLOR(float Time, float QualityOfTime, JPetHit& firstHit, JPetHit& secondHit) :
-    TNamed("JPetLOR","Event Structure"), fTime(Time), fQualityOfTime(QualityOfTime),
-    fFirstHit(firstHit), fSecondHit(secondHit),
-    fTimeDiff(0.0f), fQualityOfTimeDiff(0.0f), fNhits(2) {
-}
-
-JPetLOR::~JPetLOR() {
-}
-
-int JPetLOR::addHit(JPetHit & hit){
-  
-  if( fNhits == 0 ){
-    fFirstHit = hit;
-    return fNhits++;
-  }else if( fNhits == 1){
-    fSecondHit = hit;
-    return fNhits++;
-  }else{
-    return 0;
-  }
-}
-
-const JPetHit & JPetLOR::operator[](int i) const{
-  assert( i < fNhits && i > 0 );
-  if( i==0 ){
-    return fFirstHit;
-  }else{
-    return fSecondHit;
-  }
-}
