7c38fe4f6f6ed63167162fa1c054a9bd0371ac97
Wojciech Krzemien <wojciech.krzemien@if.uj.edu.pl>
2015-02-11 01:58:00 +0100

    changed JPetEvent -> JPetLOR
    Former-commit-id: 8316de01c604e41761c7b57849141681b3ba8646

--- framework/tests/JPetLORTest/JPetLORTest.cpp
+++ framework/tests/JPetLORTest/JPetLORTest.cpp
@@ -1,15 +1,15 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE JPetEventTest
+#define BOOST_TEST_MODULE JPetLORTest
 #include <boost/test/unit_test.hpp>
 
 #define private public
-#include "../../JPetEvent/JPetEvent.h"
+#include "../../JPetLOR/JPetLOR.h"
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
 
 BOOST_AUTO_TEST_CASE( default_constructor )
 {
-  JPetEvent event;
+  JPetLOR event;
   BOOST_REQUIRE_EQUAL(event.getTime(), 0.0f);
   BOOST_REQUIRE_EQUAL(event.getQualityOfTime(), 0.0f);
   
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE(constructor)
 {
   JPetHit firstHit;
   JPetHit secondHit;
-  JPetEvent event(8.5f, 4.5f, firstHit, secondHit);
+  JPetLOR event(8.5f, 4.5f, firstHit, secondHit);
   
   float epsilon = 0.0001f;
   BOOST_REQUIRE_CLOSE(event.getTime(), 8.5f, epsilon);
@@ -38,13 +38,13 @@ BOOST_AUTO_TEST_CASE(hitTest)
 {
   JPetHit firstHit;
   JPetHit secondHit;
-  JPetEvent event(8.5f, 4.5f, firstHit, secondHit);
+  JPetLOR event(8.5f, 4.5f, firstHit, secondHit);
   
   BOOST_REQUIRE(event.getFirstHit().getEnergy() == firstHit.getEnergy());
   BOOST_REQUIRE(event.getSecondHit().getEnergy() == firstHit.getEnergy());
   
   BOOST_REQUIRE(event[1].getEnergy() == firstHit.getEnergy());
-  //BOOST_REQUIRE(event[2].getEnergy() == secondHit.getEnergy()); // BUG JPetEvent::operator[](int i)
+  //BOOST_REQUIRE(event[2].getEnergy() == secondHit.getEnergy()); // BUG JPetLOR::operator[](int i)
 
   JPetHit fh;
   JPetHit sh;  
@@ -64,7 +64,7 @@ BOOST_AUTO_TEST_CASE(hitTest)
 
 BOOST_AUTO_TEST_CASE(timeDiffTest)
 {
-  JPetEvent event;
+  JPetLOR event;
   event.setTimeDiff(111.f);
   float epsilon = 0.0001f;
   BOOST_REQUIRE_CLOSE(event.getTimeDiff(), 111.f, epsilon);
@@ -72,7 +72,7 @@ BOOST_AUTO_TEST_CASE(timeDiffTest)
 
 BOOST_AUTO_TEST_CASE(qualityOfTimeTest)
 {
-  JPetEvent event;
+  JPetLOR event;
   event.setQualityOfTimeDiff(111.f);
   float epsilon = 0.0001f;
   BOOST_REQUIRE_CLOSE(event.fQualityOfTime, 111.f, epsilon);
@@ -80,7 +80,7 @@ BOOST_AUTO_TEST_CASE(qualityOfTimeTest)
 
 BOOST_AUTO_TEST_CASE(timeTest)
 {
-  JPetEvent event;
+  JPetLOR event;
   event.setTime(111.f);
   float epsilon = 0.0001f;
   BOOST_REQUIRE_CLOSE(event.getTime(), 111.f, epsilon);
