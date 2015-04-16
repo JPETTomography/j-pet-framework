7fd180371a4624946ba89e757fc924d82945ba92
Damian Trybek <damian.trybek@uj.edu.pl>
2015-04-16 13:19:45 +0200

    New types of config files.
    Former-commit-id: f39db2a875b37407f0a40e96aa66af5011bf281e

--- framework/tests/JPetScopeReaderTest/JPetScopeReaderTest.cpp
+++ framework/tests/JPetScopeReaderTest/JPetScopeReaderTest.cpp
@@ -15,14 +15,14 @@ BOOST_FIXTURE_TEST_CASE (signal_generation_test, signal_generation_fixture) {
 
   check_header(
     [] (int osc_file_size, int reco_signal_size) -> void {
-      BOOST_CHECK_EQUAL(osc_file_size, reco_signal_size);
+      BOOST_REQUIRE_EQUAL(osc_file_size, reco_signal_size);
     }
   );
 
   check_data(
     [] (float osc_file_time, float reco_signal_time, float osc_file_ampl, float reco_signal_ampl) -> void {
-      BOOST_CHECK_CLOSE(osc_file_time, reco_signal_time, osc_file_time / 131072.f);
-      BOOST_CHECK_CLOSE(osc_file_ampl, reco_signal_ampl, osc_file_ampl / 131072.f);
+      BOOST_CHECK_CLOSE_FRACTION(osc_file_time, reco_signal_time, 1.f / 131072.f);
+      BOOST_CHECK_CLOSE_FRACTION(osc_file_ampl, reco_signal_ampl, 1.f / 131072.f);
     }
   );
 }
@@ -31,7 +31,7 @@ BOOST_FIXTURE_TEST_CASE (tref_correctness_test, tref_correctness_fixture) {
   
   check_tref_simple(
     [] (const void* tref_pointer) -> void {
-      BOOST_CHECK_PREDICATE(std::not_equal_to <size_t> (), ((size_t) tref_pointer)((size_t) 0));
+      BOOST_CHECK_PREDICATE(std::not_equal_to <size_t> (), ((size_t) tref_pointer)((size_t) nullptr));
     }
   );
   //check_tref_simple(nullptr);
