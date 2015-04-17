69d9bc06743893cda667ddc22433c46f88f4c118
Damian Trybek <damian.trybek@uj.edu.pl>
2015-04-17 16:55:59 +0200

    Documentation for new JPetScopeReader.
    Former-commit-id: 2f10ba5df6171ed4c77ed0782d4677a88e8310c5

--- framework/JPetScopeReader/JPetScopeReader.h
+++ framework/JPetScopeReader/JPetScopeReader.h
@@ -1,3 +1,11 @@
+/**
+ * @file JPetScopeReader.h
+ * @author Damian Trybek, damian.trybek@uj.edu.pl
+ * @copyright Copyright (c) 2015, Damian Trybek
+ * @brief Module for oscilloscope data
+ * Reads oscilloscope ASCII data and procudes JPetRecoSignal structures.
+ */
+
 #ifndef _SCOPE_READER_MODULE_H_
 #define _SCOPE_READER_MODULE_H_
 
@@ -20,55 +28,121 @@ class JPetScin;
 class JPetTreeHeader;
 class JPetWriter;
 
+/** @brief Analysis Module for oscilloscope ASCII files.
+ *
+ * This module reads oscilloscope ACSII data based on config file passed through command line.
+ * Example of usign this module is located in workdir/ScopeReaderExample/ .
+ */
 class JPetScopeReader: public JPetAnalysisModule {
   
   private:
-
+  
+  /** @brief Collection of system parameters read from config file.
+   *
+   * One object per signle configuration + collimator position is created.
+   */
   typedef struct ScopeConfig {
     
-    std::string pName;
-    int pCollPosition;
+    std::string pName; /**< @brief Config name. */
+    int pCollPosition; /**< @brief Collimator position. */
 
-    JPetParamBank const* pParamBank;
+    JPetParamBank const* pParamBank; /**< @brief Pointer to JPetParamBank for current configuration.
+                                      *
+				      * @ref JPetParamBank
+				      */
     
-    JPetPM *pPM1, *pPM2, *pPM3, *pPM4;
-    JPetScin *pScin1, *pScin2;
+    JPetPM *pPM1, *pPM2, *pPM3, *pPM4; /**< @ref JPetPM */
+    JPetScin *pScin1, *pScin2; /**< @ref JPeScin */
 
-    std::string pPrefix1, pPrefix2, pPrefix3, pPrefix4;
+    std::string pPrefix1, pPrefix2, pPrefix3, pPrefix4; /**< @brief Oscilloscope ACII files prefixes. */
 
-    std::set <std::string> pFiles;
-    std::set <std::string> :: iterator pIter;
+    std::set <std::string> pFiles; /**< @brief Set of files to process. */
+    std::set <std::string> :: iterator pIter; /**< @ref pIter */
 
   } ScopeConfig;
 
   public:
-  
+
+  /** @brief Default Constructor.
+   */
   JPetScopeReader();
+
+  /** @brief Constructor
+   *
+   * @param name Module name.
+   * @param title Short description.
+   */
   JPetScopeReader(const char* name, const char* title);
 
+  /* @brief Default Destructor.
+   */
   virtual ~JPetScopeReader();
 
+  /** @brief Prepare list of input files.
+   *
+   * Parse config file, prepare list of oscilloscope ASCII files to be processed and prepares parameters bank.
+   *
+   * @param inputFilename Config file name.
+   */
   virtual void createInputObjects(const char* inputFilename = 0);
+
+  /** @brief Required by JPetManager.
+   *
+   * Currently does nothing
+   */
   virtual void createOutputObjects(const char* outputFilename = 0);
 
+  /** @brief Execute analysis for single event.
+   *
+   * Function containing per event analysis.
+   */
   virtual void exec();
-
+  
+  /** @brief Return number of events to be processed.
+   *
+   * @return fEventNb
+   */
   virtual long long getEventNb() {return fEventNb;}
-
+  
+  /** @brief End analysis.
+   */
   virtual void terminate();
 
+  /** @biref Set input file name.
+   *
+   * @param name input file name.
+   */
   void setFileName(const char* name);
-
+  
+  /** @brief Create writer for new configuration.
+   *
+   * When processing events for new configuration new output file is created.
+   */
   void createNewWriter(const char* outputFilename = 0);
+
+  /** @brief Create bank with system describing objects.
+   *
+   * Param bank is created basing on configuration file.
+   * 
+   * @param conf_data branch of property tree for single configuration.
+   * @return JPetParamBank with data describing current configuration.
+   *
+   * @todo Add possibility to read parameters for SQL database.
+   */
   JPetParamBank const& createParamBank (boost::property_tree::ptree const& conf_data);
 
+  /** @brief Produce JPetRecoSignal from single oscilloscope ASCII file.
+   *
+   * Functionality of old JPetScopeReader.
+   *
+   * @param filename oscilloscpe ASCII filename.
+   * @return generated JPetRecoSignal
+   */
   static JPetRecoSignal generateSignal (const char* filename);
 
   private:
 
-  long long fEventNb;
-
-  std::ifstream fConfigFile;
+  long long fEventNb; /**< @brief Number of events to process. */ 
   
   JPetTreeHeader* fHeader; /**< @ref JPetTreeHeader. */ 
   JPetWriter* fWriter; /**< @ref JPetWriter. */
@@ -76,8 +150,12 @@ class JPetScopeReader: public JPetAnalysisModule {
   TString fInFilename; /**< @brief Location of config file. */
   TString fOutFilename; /**< @brief Location of output root file. */
 
-  std::vector <ScopeConfig> fConfigs;
-  std::vector <ScopeConfig> :: iterator fIter;
+  std::vector <ScopeConfig> fConfigs; /**< @brief Vector of all configurations.
+                                       * 
+				       * @ref ScopeConfig
+				       */
+
+  std::vector <ScopeConfig> :: iterator fIter; /**< @ref fConfigs */
 
 };
 
