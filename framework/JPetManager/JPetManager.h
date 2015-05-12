508b0a45344f1d4ddb25b96fcb0188dbdd9dcd64
Karol Stola <karolstola@gmail.com>
2015-05-12 20:05:34 +0200

    created AnalysisModule generation structures suitable for paralelization
    Former-commit-id: d8cb0d46e44fd0861dbcdf0f747333aa257ff3a9

--- framework/JPetManager/JPetManager.h
+++ framework/JPetManager/JPetManager.h
@@ -27,6 +27,22 @@ class JPetWriter;
  *
  * Each anaylsis program needs an instance of the JPetManager which is responsible for parsing command  line arguments, handling data from the database and for executing subsequent Analysis Modules.
  */
+
+using TaskGenerator = std::function< std::unique_ptr<JPetAnalysisModule>() >;
+using TaskGeneratorChain = std::vector<const TaskGenerator>;
+
+template <typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts&&... params)
+{
+ return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
+}
+
+template <typename T, typename... Ts>
+std::function<std::unique_ptr<T>()> makeTaskGenerator(Ts&&... params)
+{
+ return [&](){return make_unique<T>(std::forward<Ts>(params)...);};
+}
+
 class JPetManager: public TNamed {
  public:
   static JPetManager& GetManager();
@@ -34,6 +50,7 @@ class JPetManager: public TNamed {
   void Init(){};
   void Run();
   void AddTask(JPetAnalysisModule* mod);
+  void AddTaskGeneratorChain(std::shared_ptr< TaskGeneratorChain > taskGeneratorChain) {ftaskGeneratorChain = taskGeneratorChain;}
   void ParseCmdLine(int argc, char** argv);
   std::vector<std::string> getInputFileNames() const;
   int getRunNumber() const;
@@ -61,6 +78,7 @@ class JPetManager: public TNamed {
   JPetUnpacker fUnpacker;
   std::list<JPetAnalysisModule*> fTasks;
   std::list<JPetAnalysisModule*>::iterator currentTask;
+  std::shared_ptr<TaskGeneratorChain> ftaskGeneratorChain;
   bool fIsProgressBarEnabled;
 };
 #endif /*  !JPETMANAGER_H */
