cd82cbeeef02bac939a7f9c967eb10686ce96290
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-09-14 17:57:55 +0200

    Add JPetTaskInterface - general interface for all computing tasks
    Former-commit-id: 3eeeb142081d5103f1551f752ae5f282b7f82b9e

---
+++ framework/JPetTaskInterface/JPetTaskInterface.h
@@ -0,0 +1,23 @@
+/**
+ *  @copyright Copyright (c) 2014, Wojciech Krzemien
+ *  @file JPetTaskInterface.h 
+ *  @author Wojciech Krzemien, wojciech.krzemien@if.uj.edu.pl
+ *  @brief
+ */ 
+
+#ifndef JPETTASKINTERFACE_H 
+#define JPETTASKINTERFACE_H 
+
+#include <map>
+#include <string>
+
+class JPetTaskInterface {
+ public:
+  typedef std::map<std::string, std::string> Options;
+  virtual ~JPetTaskInterface() {;}
+  virtual void init(const Options& options)=0;
+  virtual void exec()=0;
+  virtual void terminate()=0;
+
+};
+#endif /*  !JPETTASKINTERFACE_H */
