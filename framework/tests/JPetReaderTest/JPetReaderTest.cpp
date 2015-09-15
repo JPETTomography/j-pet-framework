74ad7e54ddc7eed8800fbb233b31207bbd18d449
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-09-15 18:45:27 +0200

    Add several JPetReader tests for new methods
    Also change fixed JPetReader methods that didnt pass the tests,
    and add small.root test file.
    
    
    Former-commit-id: 1cd5a3d16122ba63d7d6b1ac187c3c471889f655

--- framework/tests/JPetReaderTest/JPetReaderTest.cpp
+++ framework/tests/JPetReaderTest/JPetReaderTest.cpp
@@ -16,20 +16,23 @@
 #include <TObjString.h>
 
 // method list
-/*
-  JPetReader(void);
-  explicit JPetReader(const char* p_filename);
-  virtual ~JPetReader(void);
-  virtual void closeFile();
-  virtual long long GetEntries () const { return fTree->GetEntries(); }
-  virtual int GetEntry (int entryNo);
-  virtual bool OpenFile(const char* filename);
-  virtual void readData(const char* objname = "");
-  virtual TNamed& GetData () {return *fObject;}
-  JPetTreeHeader * GetHeaderClone()const;
-  void fillContainer(std::vector<T> &p_container, const std::string &p_objectName);
-  virtual void closeTFile(void){};
-*/
+  //JPetReader(void); //maybe remove this one
+  //explicit JPetReader(const char* p_filename);
+
+  //virtual Event& getCurrentEvent();
+  //virtual bool nextEvent();
+  //virtual bool firstEvent();
+  //virtual bool lastEvent();
+  //virtual bool nthEvent(int n);
+  //virtual long long getCurrentEventNumber();
+  //virtual long long getNbOfAllEvents();
+
+  //virtual bool openFileAndLoadData(const char* filename, const char* treename = "tree");
+  //virtual void closeFile();
+  //JPetTreeHeader* getHeaderClone() const;
+
+  //virtual TObject* getObject(const char* name);
+  //virtual bool isOpen() const;
 
 BOOST_AUTO_TEST_SUITE (FirstSuite)
 
@@ -37,10 +40,18 @@ BOOST_AUTO_TEST_SUITE (FirstSuite)
 BOOST_AUTO_TEST_CASE (default_constructor)
 {
   JPetReader reader;
-  BOOST_REQUIRE(reader.fBranch == 0);
+  BOOST_REQUIRE(std::string(reader.getCurrentEvent().GetName())==std::string("Empty event"));
   BOOST_REQUIRE(!reader.isOpen());
-  BOOST_REQUIRE(reader.fEvent == 0);
-  BOOST_REQUIRE(reader.fTree == 0);
+  BOOST_REQUIRE(!reader.nextEvent());
+  BOOST_REQUIRE(!reader.firstEvent());
+  BOOST_REQUIRE(!reader.lastEvent());
+  BOOST_REQUIRE(!reader.nthEvent(0));
+  BOOST_REQUIRE(!reader.nthEvent(1));
+  BOOST_REQUIRE(!reader.nthEvent(-1));
+  BOOST_REQUIRE_EQUAL(reader.getCurrentEventNumber(), -1);
+  BOOST_REQUIRE_EQUAL(reader.getNbOfAllEvents(), 0);
+  BOOST_REQUIRE(!reader.getHeaderClone());
+  BOOST_REQUIRE(!reader.getObject("testObj"));
 }
 
 BOOST_AUTO_TEST_CASE (bad_file)
@@ -48,11 +59,77 @@ BOOST_AUTO_TEST_CASE (bad_file)
   gErrorIgnoreLevel = 6000; /// we turn off the ROOT error messages
   JPetReader reader;
   /// not a ROOT file
-  BOOST_REQUIRE(!reader.openFile("bad_file.txt"));
-  BOOST_REQUIRE(reader.fBranch == 0);
+  BOOST_REQUIRE(!reader.openFileAndLoadData("bad_file.txt", "tree"));
   BOOST_REQUIRE(!reader.isOpen());
-  BOOST_REQUIRE(reader.fEvent == 0);
-  BOOST_REQUIRE(reader.fTree == 0);
+  BOOST_REQUIRE(std::string(reader.getCurrentEvent().GetName())==std::string("Empty event"));
+  BOOST_REQUIRE(!reader.nextEvent());
+  BOOST_REQUIRE(!reader.firstEvent());
+  BOOST_REQUIRE(!reader.lastEvent());
+  BOOST_REQUIRE(!reader.nthEvent(0));
+  BOOST_REQUIRE(!reader.nthEvent(1));
+  BOOST_REQUIRE(!reader.nthEvent(-1));
+  BOOST_REQUIRE_EQUAL(reader.getCurrentEventNumber(), -1);
+  BOOST_REQUIRE_EQUAL(reader.getNbOfAllEvents(), 0);
+  BOOST_REQUIRE(!reader.getHeaderClone());
+  BOOST_REQUIRE(!reader.getObject("testObj"));
+}
+
+BOOST_AUTO_TEST_CASE (good_file_with_constructor)
+{
+  JPetReader reader("small.root");
+  BOOST_REQUIRE(reader.isOpen());
+  BOOST_REQUIRE(std::string(reader.getCurrentEvent().GetName())==std::string("JPetTSlot"));
+  BOOST_REQUIRE(reader.nextEvent());
+  BOOST_REQUIRE(reader.firstEvent());
+  BOOST_REQUIRE(reader.lastEvent());
+  BOOST_REQUIRE(reader.nthEvent(0));
+  BOOST_REQUIRE(reader.nthEvent(5));
+  BOOST_REQUIRE_EQUAL(reader.getCurrentEventNumber(), 5);
+  BOOST_REQUIRE_EQUAL(reader.getNbOfAllEvents(), 10);
+  BOOST_REQUIRE(reader.getHeaderClone());
+}
+BOOST_AUTO_TEST_CASE (good_file_getObject)
+{
+  JPetReader reader("small.root");
+  BOOST_REQUIRE(!reader.getObject("nonExistentObj"));
+  BOOST_REQUIRE(reader.getObject("tree"));
+}
+
+BOOST_AUTO_TEST_CASE (good_file_openFileAndLoadData)
+{
+  JPetReader reader;
+  BOOST_REQUIRE(reader.openFileAndLoadData("small.root","tree"));
+  BOOST_REQUIRE(reader.isOpen());
+  BOOST_REQUIRE(std::string(reader.getCurrentEvent().GetName())==std::string("JPetTSlot"));
+  BOOST_REQUIRE(reader.firstEvent());
+  BOOST_REQUIRE(reader.nextEvent());
+  BOOST_REQUIRE(reader.lastEvent());
+  BOOST_REQUIRE(reader.nthEvent(0));
+  BOOST_REQUIRE(reader.nthEvent(5));
+  BOOST_REQUIRE_EQUAL(reader.getCurrentEventNumber(), 5);
+  BOOST_REQUIRE_EQUAL(reader.getNbOfAllEvents(), 10);
+  BOOST_REQUIRE(reader.getHeaderClone());
+  
+}
+
+BOOST_AUTO_TEST_CASE (good_file_closeFile)
+{
+  JPetReader reader;
+  BOOST_REQUIRE(reader.openFileAndLoadData("small.root","tree"));
+  BOOST_REQUIRE(reader.isOpen());
+  reader.closeFile();
+  BOOST_REQUIRE(!reader.isOpen());
+  BOOST_REQUIRE(std::string(reader.getCurrentEvent().GetName())==std::string("Empty event"));
+  BOOST_REQUIRE(!reader.nextEvent());
+  BOOST_REQUIRE(!reader.firstEvent());
+  BOOST_REQUIRE(!reader.lastEvent());
+  BOOST_REQUIRE(!reader.nthEvent(0));
+  BOOST_REQUIRE(!reader.nthEvent(1));
+  BOOST_REQUIRE(!reader.nthEvent(-1));
+  BOOST_REQUIRE_EQUAL(reader.getCurrentEventNumber(), -1);
+  BOOST_REQUIRE_EQUAL(reader.getNbOfAllEvents(), 0);
+  BOOST_REQUIRE(!reader.getHeaderClone());
+  BOOST_REQUIRE(!reader.getObject("testObj"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
