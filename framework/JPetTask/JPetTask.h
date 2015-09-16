37005a0a26345789c9ba65381a619963df3d4d1f
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-09-16 09:19:54 +0200

    Add JPetTask class with stub of the class Test
    Former-commit-id: 235192518258fc0561945a5aaf84b670c840c020

---
+++ framework/JPetTask/JPetTask.h
@@ -0,0 +1,25 @@
+/**
+ *  @copyright Copyright (c) 2014, Wojciech Krzemien
+ *  @file JPetTask.h 
+ *  @author Wojciech Krzemien, wojciech.krzemien@if.uj.edu.pl
+ *  @brief
+ */ 
+
+#ifndef JPETTASK_H 
+#define JPETTASK_H 
+#include "../JPetTaskInterface/JPetTaskInterface.h"
+#include <TNamed.h>
+
+class JPetTask: public JPetTaskInterface
+{
+ public:
+  JPetTask();
+  virtual void init(const JPetTaskInterface::Options& opts);
+  virtual void exec();
+  virtual void terminate();
+  virtual void setEvent(TNamed* ev);
+ protected:
+  TNamed* fEvent;
+  
+};
+#endif /*  !JPETTASK_H */
