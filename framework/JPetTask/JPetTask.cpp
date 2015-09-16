2658b68c1e8716d3e90d10292076f0b50616891e
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-09-16 09:19:54 +0200

    Add JPetTask class with stub of the class Test
    Former-commit-id: 4e40ac7dc46c550f397b6a58fac919af66a38660

---
+++ framework/JPetTask/JPetTask.cpp
@@ -0,0 +1,30 @@
+/**
+  *  @copyright Copyright (c) 2014, Wojciech Krzemien
+  *  @file JPetTask.cpp
+  *  @author Wojciech Krzemien, wojciech.krzemien@if.uj.edu.pl
+  */ 
+
+#include "./JPetTask.h"
+
+JPetTask::JPetTask():
+fEvent(0)
+{
+}
+
+
+void JPetTask::init(const JPetTaskInterface::Options& opts)
+{
+}
+
+void JPetTask::exec() 
+{
+  // do something with event
+}
+
+void JPetTask::terminate() 
+{
+}
+
+void JPetTask::setEvent(JPetTask::Event* ev){
+  fEvent = ev;
+}
