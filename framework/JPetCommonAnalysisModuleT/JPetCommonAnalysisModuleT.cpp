245d3517e2217eeff5ecea533d5eb8a2e7ddc3a9
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-09-17 10:39:16 +0200

    Add JPetCommonAnalysisModuleT for migration
    Also, change the main Makefile.
    
    
    Former-commit-id: decc5a06b0f2537bb68d9682cf5489e8fd1af6ac

---
+++ framework/JPetCommonAnalysisModuleT/JPetCommonAnalysisModuleT.cpp
@@ -0,0 +1,19 @@
+/**
+  *  @copyright Copyright (c) 2015, Wojciech Krzemien
+  *  @file JPetCommonAnalysisModuleT.cpp
+  *  @author Wojciech Krzemien, wojciech.krzemien@if.uj.edu.pl
+  */
+
+#include "JPetCommonAnalysisModuleT.h"
+
+#include "../../JPetLoggerInclude.h"
+
+JPetCommonAnalysisModuleT::JPetCommonAnalysisModuleT()
+{
+}
+
+
+JPetCommonAnalysisModuleT::~JPetCommonAnalysisModuleT()
+{
+}
+
