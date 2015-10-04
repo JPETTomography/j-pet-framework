7217b1e65eda9f030d752ca25b2a5bb827fcb6aa
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-04 21:08:10 +0200

    Small refactoring of JPetOptions and add one test to JPetOptionsTest
    Former-commit-id: 140ceecc4e97c741e3352a984211c1644c00e0ba

--- framework/tests/JPetOptionsTest/JPetOptionsTest.cpp
+++ framework/tests/JPetOptionsTest/JPetOptionsTest.cpp
@@ -4,12 +4,30 @@
 
 #define private public
 #include "../../JPetOptions/JPetOptions.h"
+//JPetOptions
+//public methods
+//JPetOptions();
+//explicit JPetOptions(const Options& opts);
+
+//bool areCorrect(const Options& opts) const;
+//const char* getInputFile() const;
+//const char* getOutputFile() const;
+//long long getFirstEvent() const;
+//long long getLastEvent() const;
+//int getRunNumber() const;
+//bool isProgressBar() const;
+//FileType getInputFileType() const;
+//FileType getOutputFileType() const;
+//inline Options getOptions() const;
+//static  Options getDefaultOptions();
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
 
 BOOST_AUTO_TEST_CASE( my_test1 )
 {
-  BOOST_REQUIRE(1==0);
+  JPetOptions opts; 
+  auto default_opts =  JPetOptions::getDefaultOptions();
+  BOOST_REQUIRE(opts.getOptions() == default_opts);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
