1dff8d822785823811ddf5d3c164ebeb86c41267
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-10 01:04:30 +0200

    Move and rename ModuleLoader to JPetTaskLoader and at it to core framework
    Former-commit-id: 1d8da5ecdf203bb330858156962e160fb0bf4b6c

---
+++ framework/JPetTaskLoader/JPetTaskLoader.cpp
@@ -0,0 +1,60 @@
+#include "JPetTaskLoader.h"
+#include <iostream>
+#include "../../framework/JPetTask/JPetTask.h"
+//ClassImp(JPetTaskLoader);
+
+JPetTaskLoader::JPetTaskLoader(const char* name, const char* title,
+                               const char* in_file_type,
+                               const char* out_file_type,
+                               JPetTask* taskToExecute):
+  TNamed(name, title),
+  fInFileType(in_file_type),
+  fOutFileType(out_file_type)
+{
+  addSubTask(taskToExecute);
+}
+
+
+
+void JPetTaskLoader::init(const JPetOptions::Options& opts)
+{
+  auto newOpts(opts);
+  auto inFile = newOpts.at("inputFile");
+  auto outFile = inFile;
+  inFile = generateProperNameFile(inFile, fInFileType);
+  outFile = generateProperNameFile(outFile, fOutFileType);
+  newOpts.at("inputFile") = inFile;
+  newOpts.at("inputFileType") = fInFileType;
+  newOpts.at("outputFile") = outFile;
+  newOpts.at("outputFileType") = fOutFileType;
+  for (auto opt : newOpts) {
+    std::cout << opt.first << " = " << opt.second << std::endl;
+  }
+  setOptions(JPetOptions(newOpts));
+  //here we should call some function to parse options
+  auto inputFilename = fOptions.getInputFile();
+  auto outputFilename = fOptions.getOutputFile();
+  createInputObjects(inputFilename);
+  createOutputObjects(outputFilename);
+}
+
+std::string JPetTaskLoader::generateProperNameFile(const std::string& srcFilename, const std::string& fileType) const
+{
+  auto baseFileName = getBaseFileName(srcFilename);
+  return baseFileName + "." + fileType + ".root";
+}
+
+std::string JPetTaskLoader::getBaseFileName(const std::string& srcName) const
+{
+  auto name(srcName);
+  auto pos = name.find(".");
+  if ( pos != std::string::npos ) {
+    name.erase( pos );
+  }
+  return name;
+}
+
+JPetTaskLoader::~JPetTaskLoader()
+{
+}
+
