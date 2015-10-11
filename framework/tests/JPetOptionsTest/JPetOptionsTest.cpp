f777b398aaa6ccb7c3217f1decf2926f7fa89882
Andrzej Gruntowski <andrzej.gruntowski@uj.edu.pl>
2015-10-11 22:55:39 +0200

    Add unittests to JPetOptions
    Former-commit-id: 3705097332e28be6663644313bf17522c3df1427

--- framework/tests/JPetOptionsTest/JPetOptionsTest.cpp
+++ framework/tests/JPetOptionsTest/JPetOptionsTest.cpp
@@ -4,6 +4,7 @@
 
 #define private public
 #include "../../JPetOptions/JPetOptions.h"
+
 //JPetOptions
 //public methods
 //JPetOptions();
@@ -28,6 +29,52 @@ BOOST_AUTO_TEST_CASE( my_test1 )
   JPetOptions opts; 
   auto default_opts =  JPetOptions::getDefaultOptions();
   BOOST_REQUIRE(opts.getOptions() == default_opts);
+}
+
+BOOST_AUTO_TEST_CASE(petOptionsDefaultConstrutorTest)
+{
+    JPetOptions::Options options = {
+        {"inputFile", ""},
+        {"inputFileType", ""},
+        {"outputFile", ""},
+        {"outputFileType", ""},
+        {"firstEvent", "-1"},
+        {"lastEvent", "-1"},
+        {"progressBar", "false"},
+        {"runId", "-1"}
+    };
+
+    JPetOptions petOptions;
+    BOOST_REQUIRE_EQUAL(CommonTools::mapComparator(petOptions.getOptions(), options), true);
+}
+
+BOOST_AUTO_TEST_CASE(petOptionsBasicTest)
+{
+    JPetOptions::Options options = {
+        {"inputFile", "input"},
+        {"outputFile", "output"},
+        {"firstEvent", "8246821 0xffff 020"},
+        {"lastEvent", "8246821 0xffff 020"},
+        {"runId", "2001, A Space Odyssey"},
+        {"progressBar", "true"},
+        {"inputFileType", "root"},
+        {"outputFileType", "scope"}
+    };
+
+    JPetOptions petOptions(options);
+    BOOST_REQUIRE_EQUAL(petOptions.getInputFile(), "input");
+    BOOST_REQUIRE_EQUAL(petOptions.getOutputFile(), "output");
+    auto firstEvent = petOptions.getFirstEvent();
+    BOOST_REQUIRE_EQUAL(firstEvent, 8246821);
+    auto lastEvent = petOptions.getLastEvent();
+    BOOST_REQUIRE_EQUAL(lastEvent, 8246821);
+    int runNumberHex = petOptions.getRunNumber();
+    BOOST_REQUIRE_EQUAL(runNumberHex, 2001);
+    BOOST_REQUIRE_EQUAL(petOptions.isProgressBar(), true);
+    BOOST_REQUIRE_EQUAL(petOptions.getInputFileType(), JPetOptions::FileType::kRoot);
+    BOOST_REQUIRE_EQUAL(petOptions.getOutputFileType(), JPetOptions::FileType::kScope);
+    BOOST_REQUIRE_EQUAL(CommonTools::mapComparator(petOptions.getOptions(), options), true);
+    BOOST_REQUIRE_EQUAL(CommonTools::mapComparator(petOptions.getDefaultOptions(), options), false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
