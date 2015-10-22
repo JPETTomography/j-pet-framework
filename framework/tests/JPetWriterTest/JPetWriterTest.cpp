10f366bf087b351e0b7eccf1ae7ec1f86dcbcab7
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-22 18:23:36 +0200

    Add some unit tests to JPetWriter
    Former-commit-id: bb4f74b2152c50956fac8d4113299e6f82db768e

--- framework/tests/JPetWriterTest/JPetWriterTest.cpp
+++ framework/tests/JPetWriterTest/JPetWriterTest.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE JPetWriterTest
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>  // for filesystem::remove
 #include <TNamed.h>
 
 #define private public
@@ -8,13 +9,20 @@
 #include "../../JPetWriter/JPetWriter.h"
 #include "../../JPetReader/JPetReader.h"
 
+#include "../../JPetSigCh/JPetSigCh.h"
+#include "../../JPetTSlot/JPetTSlot.h"
+#include "../../JPetBaseSignal/JPetBaseSignal.h"
+#include "../../JPetRawSignal/JPetRawSignal.h"
+#include "../../JPetRecoSignal/JPetRecoSignal.h"
+#include "../../JPetPhysSignal/JPetPhysSignal.h"
+#include "../../JPetHit/JPetHit.h"
+#include "../../JPetLOR/JPetLOR.h"
+
 //  JPetWriter(const char *p_fileName);
 //  virtual ~JPetWriter(void);
 //
 //  template <class T>
 //  bool Write(const T& obj);
-//  template <class T>
-//  bool Write(std::vector<T>& obj);
 //  //bool OpenFile(const char* filename);
 //  void WriteHeader(TObject* header);
 //  inline bool IsOpenFile() const {return fFile.IsOpen();}
@@ -92,5 +100,126 @@ BOOST_AUTO_TEST_CASE( my_test3 )
     BOOST_REQUIRE(std::string(objOut.GetTitle())==Form("Title of this testObj%d",i));
   }
 }
+
+
+BOOST_AUTO_TEST_CASE( saving_different_objects1 )
+{
+  //JPetSigCh testJPetSigCh;
+   auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetSigCh testJPetSigCh;
+    writer.write(testJPetSigCh);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects2 )
+{
+  //JPetTSlot testJPetTSlot;
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetTSlot testJPetTSlot;
+    writer.write(testJPetTSlot);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects3 )
+{
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetBaseSignal testJPetBaseSignal;
+    writer.write(testJPetBaseSignal);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects4 )
+{
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetRawSignal testJPetRawSignal;
+    writer.write(testJPetRawSignal);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects5 )
+{
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetRecoSignal testJPetRecoSignal;
+    writer.write(testJPetRecoSignal);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects6 )
+{
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetPhysSignal testJPetPhysSignal;
+    writer.write(testJPetPhysSignal);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects7 )
+{
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetHit testJPetHit;
+    writer.write(testJPetHit);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
+
+BOOST_AUTO_TEST_CASE( saving_different_objects8 )
+{
+
+  auto fileTest = "saving_different_objectsTest.root";
+  JPetWriter writer(fileTest);
+  const auto kHugeNumberOfObjects = 10000;
+  for (int i = 0; i < kHugeNumberOfObjects; i++) {
+    if (i%1000==0) std::cout<<"*"<<std::flush;
+    JPetLOR testJPetLOR;
+    writer.write(testJPetLOR);
+  }
+  if(boost::filesystem::exists(fileTest))
+          boost::filesystem::remove(fileTest);
+} 
 
 BOOST_AUTO_TEST_SUITE_END()
