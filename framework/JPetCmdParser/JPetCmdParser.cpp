b0e11c250ffb05834a0da88556a084c5eeabaf79
Andrzej Gruntowski <andrzej.gruntowski@uj.edu.pl>
2015-10-31 19:45:36 +0100

    Refactor logging error and add some modifications in add_options() function.
    Former-commit-id: 0c37fd4caea71b002c99c714ed65b8aae0002d7b

--- framework/JPetCmdParser/JPetCmdParser.cpp
+++ framework/JPetCmdParser/JPetCmdParser.cpp
@@ -17,7 +17,7 @@ JPetCmdParser::JPetCmdParser(): fOptionsDescriptions("Allowed options")
 
   fOptionsDescriptions.add_options()
   ("help,h", "produce help message")
-  ("type,t", po::value<std::string>()->required(), "type of file: hld, root or scope")
+  ("type,t", po::value<std::string>()->required()->implicit_value(""), "type of file: hld, root or scope")
   ("file,f", po::value< std::vector<std::string> >()->required()->multitoken(), "File(s) to open")
   ("range,r", po::value< std::vector<int> >()->multitoken()->default_value(tmp, ""), "Range of events to process.")
   ("param,p", po::value<std::string>(), "File with TRB numbers.")
@@ -34,10 +34,10 @@ std::vector<JPetOptions> JPetCmdParser::parseAndGenerateOptions(int argc, const 
 {
   po::variables_map variablesMap;
   if (argc == 1) {
-    WARNING("No options provided.");
-    std::cout << "No options provided" << "\n";
-    std::cout << getOptionsDescription() << "\n";
-    exit(0);
+    ERROR("No options provided.");
+    std::cerr << "No options provided" << "\n";
+    std::cerr << getOptionsDescription() << "\n";
+    exit(-1);
   }
 
   po::store(po::parse_command_line(argc, argv, fOptionsDescriptions), variablesMap);
@@ -57,21 +57,24 @@ std::vector<JPetOptions> JPetCmdParser::parseAndGenerateOptions(int argc, const 
 
 bool JPetCmdParser::areCorrectOptions(const po::variables_map& variablesMap) const
 {
-  /* parse range of events */
+  /* Parse range of events */
   if (variablesMap.count("range")) {
     if (variablesMap["range"].as< std::vector<int> >().size() != 2) {
+      ERROR("Wrong number of bounds in range.");
       std::cerr << "Wrong number of bounds in range: " << variablesMap["range"].as< std::vector<int> >().size() << std::endl;
       return false;
     }
     if (
       variablesMap["range"].as< std::vector<int> >()[0]
       > variablesMap["range"].as< std::vector<int> >()[1]) {
+      ERROR("Wrong range of events.");
       std::cerr << "Wrong range of events." << std::endl;
       return false;
     }
   }
 
   if (!isCorrectFileType(getFileType(variablesMap))) {
+    ERROR("Wrong type of file.");
     std::cerr << "Wrong type of file: " << getFileType(variablesMap) << std::endl;
     std::cerr << "Possible options: hld, root or scope" << std::endl;
     return false;
@@ -81,6 +84,7 @@ bool JPetCmdParser::areCorrectOptions(const po::variables_map& variablesMap) con
     int l_runId = variablesMap["runId"].as<int>();
 
     if (l_runId <= 0) {
+      ERROR("Wrong number of run id.");
       std::cerr << "Wrong number of run id: " << l_runId << std::endl;
       return false;
     }
@@ -90,6 +94,7 @@ bool JPetCmdParser::areCorrectOptions(const po::variables_map& variablesMap) con
     int l_progressBar = variablesMap["progressBar"].as<int>();
 
     if (l_progressBar != 0 && l_progressBar != 1) {
+      ERROR("Wrong parameter of progressbar.");
       std::cerr << "Wrong parameter of progressbar: " << l_progressBar << std::endl;
       return false;
     }
@@ -98,6 +103,8 @@ bool JPetCmdParser::areCorrectOptions(const po::variables_map& variablesMap) con
   std::vector<std::string> fileNames(variablesMap["file"].as< std::vector<std::string> >());
   for (int i = 0; i < fileNames.size(); i++) {
     if ( ! CommonTools::ifFileExisting(fileNames[i]) ) {
+      std::string fileName = fileNames[i];
+      ERROR("File : " + fileName + " does not exist.");
       std::cerr << "File : " << fileNames[i] << " does not exist" << std::endl;
       return false;
     }
@@ -123,7 +130,7 @@ std::vector<JPetOptions> JPetCmdParser::generateOptions(const po::variables_map&
   if (firstEvent >= 0) options.at("firstEvent") = std::to_string(firstEvent);
   if (lastEvent >= 0) options.at("lastEvent") = std::to_string(lastEvent);
   auto files = getFileNames(optsMap); 
-  std::vector<JPetOptions>  optionContainer; 
+  std::vector<JPetOptions>  optionContainer;
   for (auto file :files) {
     options.at("inputFile") = file;
     optionContainer.push_back(JPetOptions(options));
