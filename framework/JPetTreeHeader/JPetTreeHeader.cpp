5f31e9e4367dac96241fe58556f183a7218b56be
Aleksander Gajos <alek.gajos@gmail.com>
2016-01-03 00:58:53 +0100

    Refactorised JPetTreeHeader and replaced TString usages with std::string.
    Former-commit-id: 48c039db25a76a9a94277159fcfcfe139bb99a47

--- framework/JPetTreeHeader/JPetTreeHeader.cpp
+++ framework/JPetTreeHeader/JPetTreeHeader.cpp
@@ -1,11 +1,12 @@
 /**
  *  @file JPetTreeHeader.cpp
  *  @author Aleksander Gajos, aleksander.gajos@uj.edu.pl
- *  @copyright Copyright (c) 2014, Aleksander Gajos
+ *  @copyright Copyright (c) 2015, The J-PET Framework Authors
  */
 
 #include "JPetTreeHeader.h"
 #include "../CommonTools/CommonTools.h"
+#include <sstream>
 
 ClassImp(JPetTreeHeader);
 
@@ -16,7 +17,6 @@ JPetTreeHeader::JPetTreeHeader():
 {
 }
 
-
 JPetTreeHeader::JPetTreeHeader(int run):
   fRunNo(run),
   fSourcePosition(-1),
@@ -24,34 +24,23 @@ JPetTreeHeader::JPetTreeHeader(int run):
 {
 }
 
-TString JPetTreeHeader::stringify() const
+std::string JPetTreeHeader::stringify() const
 {
-  TString tmp;
-  tmp.Append("-----------------------------------------------------------------\n");
-  tmp.Append("------------------------- General Info --------------------------\n");
-  tmp.Append("-----------------------------------------------------------------\n");
-  tmp.Append( "Run number              : " ).Append( CommonTools::Itoa(fRunNo)).Append("\n");
-  tmp.Append( "Base file name          : " ).Append( getBaseFileName() ).Append("\n");
-  tmp.Append( "Source (if any) position: " ).Append( Form("%lf",getSourcePosition()) ).Append("\n");
-  tmp.Append("-----------------------------------------------------------------\n");
-  tmp.Append("-------------- Processing history (oldest first) ----------------\n");
-  tmp.Append("-----------------------------------------------------------------\n");
-  // iterate over all stages of processing history
-  for (int i = 0; i < getStagesNb(); i++ ) {
-    const ProcessingStageInfo & info = getProcessingStageInfo(i);
-    tmp.Append( "Module Name         : " ).Append( info.fModuleName ).Append("\n");
-    tmp.Append( "Module desc.        : " ).Append( info.fModuleDescription ).Append("\n");
-    tmp.Append( "Module version      : " ).Append( CommonTools::Itoa( info.fModuleVersion ) ).Append("\n");
-    tmp.Append( "Started processing  : " ).Append( info.fCreationTime ).Append("\n");
-    tmp.Append("-----------------------------------------------------------------\n");
-  }
-  
-  tmp.Append( stringifyDictionary() );
+  std::ostringstream tmp;
+  tmp<<"-----------------------------------------------------------------\n" ;
+  tmp<<"------------------------- General Info --------------------------\n" ;
+  tmp<<"-----------------------------------------------------------------\n" ;
+  tmp<< "Run number              : " << CommonTools::Itoa(fRunNo) <<"\n";
+  tmp<< "Base file name          : "<<getBaseFileName()<<"\n";
+  tmp<< "Source (if any) position: "<< Form("%lf",getSourcePosition())<<"\n";
 
-  return tmp;
+tmp << stringifyHistory();
+tmp << stringifyDictionary();
+
+return tmp.str();
 }
 
-int JPetTreeHeader::addStageInfo(TString p_name, TString p_title, int p_version, TString p_time_stamp )
+int JPetTreeHeader::addStageInfo(std::string p_name, std::string p_title, int p_version, std::string p_time_stamp )
 {
   ProcessingStageInfo stage;
   stage.fModuleName = p_name;
@@ -61,14 +50,66 @@ int JPetTreeHeader::addStageInfo(TString p_name, TString p_title, int p_version,
   fStages.push_back( stage );
 }
 
-TString JPetTreeHeader::stringifyDictionary() const {
-  TString tmp;
-  tmp.Append("-----------------------------------------------------------------\n");
-  tmp.Append("--------------------- User-defined variables --------------------\n");
-  tmp.Append("-----------------------------------------------------------------\n");
+std::string JPetTreeHeader::stringifyDictionary() const {
+
+  std::ostringstream tmp;
+  tmp<<"-----------------------------------------------------------------\n";
+  tmp<<"--------------------- User-defined variables --------------------\n";
+  tmp<<"-----------------------------------------------------------------\n";
+
   for(auto const &entry : fDictionary){
-    tmp.Append(entry.first).Append("   =   ").Append(entry.second).Append("\n");
-    tmp.Append("-----------------------------------------------------------------\n");
+    tmp<<entry.first << "   =   " << entry.second <<"\n";
+    tmp<<"-----------------------------------------------------------------\n";
   }
-  return tmp;
+  return tmp.str();
+}
+
+std::string JPetTreeHeader::stringifyHistory() const {
+
+  std::ostringstream tmp;
+  tmp<<"-----------------------------------------------------------------\n";
+  tmp<<"-------------- Processing history (oldest first) ----------------\n";
+  tmp<<"-----------------------------------------------------------------\n";
+
+  for (int i = 0; i < getStagesNb(); i++ ) {
+    const ProcessingStageInfo & info = getProcessingStageInfo(i);
+    tmp<< "Module Name         : " << info.fModuleName<<"\n";
+    tmp<< "Module desc.        : " << info.fModuleDescription<<"\n";
+    tmp<< "Module version      : " << CommonTools::Itoa( info.fModuleVersion ) <<"\n";
+    tmp<< "Started processing  : " << info.fCreationTime <<"\n";
+    tmp<<"-----------------------------------------------------------------\n";
+  }  
+  return tmp.str();
+}
+
+/**
+ * @brief Sets the textual value of an existing variable named 'name' or 
+ * creates such variable if it was nonexistent
+ *
+ * @param name name of the variable (existing or not)
+ * @param value value of the variable
+ *
+ * Use this method to store any additional information in the TreeHeader.
+ * All values must be encoded as text.
+ *
+ * Example:
+ * header.setVariable("source position", "X=10cm; Y=2cm; Z=20cm")
+ *
+ */
+void JPetTreeHeader::setVariable(std::string name, std::string value){
+  fDictionary[name] = value;
+}
+
+/**
+ * @brief Returns the textual value of a variable (see the setVariable method)
+ *
+ * @param name name of the variable
+ *
+ * Use this method to access any additional information previously stored
+ * in the TreeHeader with the setVariable method. If a name of a non-existent variable
+ * is provided, an empty string will be returned.
+ *
+ */
+std::string JPetTreeHeader::getVariable(std::string name) const {
+  return fDictionary.at(name); 
 }
--- framework/JPetTreeHeader/JPetTreeHeader.h
+++ framework/JPetTreeHeader/JPetTreeHeader.h
@@ -17,15 +17,22 @@
  * @brief A class representing a header with info on JPet ROOT Trees.
  *
  * Encapsulates information from the top-level of JPet data files as defined in Report 19, tables 37+. A TreeHeader object is intended to be stored in evert JPet ROOT file along with a respective TTree which it describes.
+ *
+ * The Header also contains the processing history and a dictionary of auxilliary information about the data in the ROOT tree.
+ *
+ * The processing history is a list of records added by each JPetTask which processed the data starting from raw data.
+ *
+ * The auvilliary information if a dictionary of variables with textual names and textual values, which can be used to encode any additional information, e.g. a variable named "source position" with a value of "Xcm;Ycm;Zcm" can be added to the dictionary.
+ *
  */
 class JPetTreeHeader: public TObject{
  protected:
   struct ProcessingStageInfo
   {
-    TString fModuleName;
-    TString fModuleDescription;
+    std::string fModuleName;
+    std::string fModuleDescription;
     int fModuleVersion;
-    TString fCreationTime;
+    std::string fCreationTime;
   };
 
 
@@ -34,13 +41,13 @@ class JPetTreeHeader: public TObject{
   JPetTreeHeader(int run);
   void Print(Option_t* opt = "") const { std::cout << this->stringify(); }
 
-  /// produce a TString edscribing all the information dontained in this object
-  TString stringify() const;
+  /// produce a std::string edscribing all the information dontained in this object
+  std::string stringify() const;
 
   inline int getRunNumber()  const { return fRunNo; }
   inline void setRunNumber(int p_run_no) { fRunNo = p_run_no; }
   
-  inline TString getBaseFileName() const {return fBaseFilename;}
+  inline std::string getBaseFileName() const {return fBaseFilename;}
   inline void setBaseFileName(const char * p_name){ fBaseFilename = p_name; }
   
   /// set source position in mm or -1 of no source was used
@@ -49,20 +56,22 @@ class JPetTreeHeader: public TObject{
   inline void setSourcePosition(double p_pos) { fSourcePosition = p_pos; }
 
   /// add information on one processing stage, i.e. one module
-  int addStageInfo(TString name, TString title, int version, TString time_stamp);
+  int addStageInfo(std::string name, std::string title, int version, std::string time_stamp);
   int getStagesNb()const{ return fStages.size(); }
 
   const ProcessingStageInfo & getProcessingStageInfo(int i)const{ return fStages.at(i); }
 
-  void setVariable(std::string name, std::string value){ fDictionary[name]=value; }
-  std::string getVariable(std::string name) const {return fDictionary.at(name); }
+  void setVariable(std::string name, std::string value);
+  std::string getVariable(std::string name) const;
   
 protected:
 
-  TString stringifyDictionary() const;
+  /// auxilliary methods for the Print() functionality
+  std::string stringifyDictionary() const;
+  std::string stringifyHistory() const;
 
   int fRunNo;
-  TString fBaseFilename;
+  std::string fBaseFilename;
   double fSourcePosition;
 
   std::vector<ProcessingStageInfo> fStages;
