971fb52e0a9af4d8509ec80a21f7f4d1dbd476a2
Aleksander Gajos <alek.gajos@gmail.com>
2016-01-27 01:47:44 +0100

    Add copying of DB config files to the build dir in CMakeLists
    Fix paths referring to the DB config in tests.
    
    
    Former-commit-id: 5237e617cce40b7c49810dbc5156f26db47c3078

--- JPetParamManager/JPetParamManagerTest.cpp
+++ JPetParamManager/JPetParamManagerTest.cpp
@@ -7,7 +7,7 @@
 
 #include <cstddef>
 
-const char* gDefaultConfigFile = "DBConfig/configDB.cfg";
+const char* gDefaultConfigFile = "../DBConfig/configDB.cfg";
 
 BOOST_AUTO_TEST_SUITE(JPetParamManagerTestSuite)
 
