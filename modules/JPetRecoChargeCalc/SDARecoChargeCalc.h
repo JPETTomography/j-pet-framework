72db2eb0f7316567c9e92fdb78842aadc2743480
Aleksander Gajos <alek.gajos@gmail.com>
2016-02-18 14:10:44 +0100

    Add module JPetSDARecoChargeCalc by Szymon

---
+++ modules/JPetRecoChargeCalc/SDARecoChargeCalc.h
@@ -0,0 +1,34 @@
+/**
+ * @file SDARecoChargeCalc.h
+ * @author Szymon Niedźwiecki, szymon.niedzwiecki@googlemail.com
+ * @copyright Copyright (c) 2015, Szymon Niedźwiecki
+ * @brief Producer of charge for JPetRecoSignals
+ * Reads a TTree of Reco Signals and calculates charges for each of them.
+ * For more details look into README
+ */
+
+#ifndef _JPETANALYSISMODULE_SDACHARGE_H_
+#define _JPETANALYSISMODULE_SDACHARGE_H_
+
+#include "../../JPetTask/JPetTask.h"
+#include "TCanvas.h"
+
+class SDARecoChargeCalc: public JPetTask
+{
+
+ public:
+
+  SDARecoChargeCalc(const char* name, const char* description);
+
+  virtual ~SDARecoChargeCalc();
+  virtual void exec();
+  virtual void init(const JPetTaskInterface::Options& /* opts */);
+  virtual void terminate();
+
+private:
+	int fBadSignals;
+	int fCurrentEventNumber;
+};
+
+#endif
+
