c8d1ac774f8d93d841719d9c8ff45c64adf71ceb
Aleksander Gajos <alek.gajos@gmail.com>
2016-02-18 14:18:41 +0100

    Add JPetMakePhysSignal module by Szymon.
    Additionally remove unncecessary includes.

---
+++ modules/JPetMakePhysSignal/SDAMakePhysSignals.cpp
@@ -0,0 +1,32 @@
+#include "./SDAMakePhysSignals.h"
+#include "../../tools/JPetRecoSignalTools/JPetRecoSignalTools.h"
+
+SDAMakePhysSignals::SDAMakePhysSignals(const char* name, const char* description) 
+  : JPetTask(name, description)
+{
+}
+
+SDAMakePhysSignals::~SDAMakePhysSignals()
+{
+}
+
+void SDAMakePhysSignals::init(const JPetTaskInterface::Options& /* opts */)
+{
+}
+
+void SDAMakePhysSignals::exec()
+{
+  const JPetRecoSignal& signal = (JPetRecoSignal&)(*getEvent());
+
+  JPetPhysSignal physSignal;
+  physSignal.setRecoSignal(signal);
+  WARNING( Form("This module currently sets number of photoelectrons equal to charge of JPetRecoSignal!") );
+  physSignal.setPhe(physSignal.getRecoSignal().getCharge() );
+
+  fWriter->write(physSignal);
+}
+
+
+void SDAMakePhysSignals::terminate()
+{
+}
