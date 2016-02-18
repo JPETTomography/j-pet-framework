b680780d60a05750d8e560e29886dd035f6a653f
Tomek <tomegru@gmail.com>
2016-02-18 20:45:56 +0100

    Handle TGraph and TCanvas objects in JPetStatistics.
    Former-commit-id: b63220959f0848a02eb2a9109becea1f7e038c75

--- JPetStatistics/JPetStatistics.cpp
+++ JPetStatistics/JPetStatistics.cpp
@@ -12,12 +12,28 @@ void JPetStatistics::createHistogram(TObject * object){
   fHistos.Add(object);
 }
 
+void JPetStatistics::createGraph(TObject * object){
+  fGraphs.Add(object);
+}
+
+void JPetStatistics::createCanvas(TObject * object){
+  fCanvas.Add(object);
+}
+
 TH1F & JPetStatistics::getHisto1D(const char * name){
   return dynamic_cast<TH1F&>(*(fHistos.FindObject(name)));
 }
 
 TH2F & JPetStatistics::getHisto2D(const char * name){
   return dynamic_cast<TH2F&>(*(fHistos.FindObject(name)));
+}
+
+TGraph & JPetStatistics::getGraph(const char * name){
+  return dynamic_cast<TGraph&>(*(fGraphs.FindObject(name)));
+}
+
+TCanvas & JPetStatistics::getCanvas(const char * name){
+  return dynamic_cast<TCanvas&>(*(fCanvas.FindObject(name)));
 }
 
 void JPetStatistics::createCounter(const char * name){
--- JPetStatistics/JPetStatistics.h
+++ JPetStatistics/JPetStatistics.h
@@ -11,6 +11,8 @@
 #include <TH2F.h>
 #include <TString.h>
 #include <map>
+#include <TGraph.h>
+#include <TCanvas.h>
 
 /**
  * @brief An class for storing statistics of the processing (e.g histograms and counters) during execution of a JPetTask 
@@ -22,8 +24,13 @@ class JPetStatistics: public TObject{
  public:
   
   void createHistogram(TObject * object);
+  void createHistogram(TObject * object);
+  void createGraph(TObject * object);
+  void createCanvas(TObject * object);
   TH1F & getHisto1D(const char * name);
   TH2F & getHisto2D(const char * name);
+  TGraph & getGraph(const char * name);
+  TCanvas & getCanvas(const char * name);
   void createCounter(const char * name);
   double & getCounter(const char * name);
 
@@ -33,6 +40,8 @@ class JPetStatistics: public TObject{
     
  protected:
   THashTable fHistos;
+  THashTable fGraphs;
+  THashTable fCanvas;
   std::map<TString, double> fCounters;
  
 };
