f744daf73b1de95023ba494cb740b253c139a22f
Andrzej <andrzej.gruntowski@uj.edu.pl>
2016-03-22 09:13:50 +0100

    717 Add proper copyrights.
    Former-commit-id: 421e971dad38d4a91fe4af759792ab3761a4b466

--- JPetSimplePhysSignalReco/HelperMathFunctions.h
+++ JPetSimplePhysSignalReco/HelperMathFunctions.h
@@ -1,9 +1,20 @@
 /**
- * @file HelperMathFunctions.h
- * @copyright Copyright (c) 2015, J-PET collaboration 
- * @brief Helper mathematical functions used in the Reconstruction
- * module. 
+ *  @copyright Copyright 2016 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file HelperMathFunctions.h
+ *  @copyright Copyright (c) 2015, J-PET collaboration 
+ *  @brief Helper mathematical functions used in the Reconstruction
  */
+
 #ifndef _HELPERMATHFUNCTIONS_H_
 #define _HELPERMATHFUNCTIONS_H_
 #include <boost/numeric/ublas/vector.hpp>
