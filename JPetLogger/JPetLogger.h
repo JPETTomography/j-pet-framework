1d2017efb298e8f20f972f3d68391ac05a0049d7
Andrzej <andrzej.gruntowski@uj.edu.pl>
2016-03-22 09:13:50 +0100

    717 Add proper copyrights.
    Former-commit-id: 421e971dad38d4a91fe4af759792ab3761a4b466

--- JPetLogger/JPetLogger.h
+++ JPetLogger/JPetLogger.h
@@ -4,11 +4,11 @@
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  *  @file JPetLogger.h
  *  @brief Simple logger class. Don't use directly. Macros from JPetLoggerInclude.h should be used instead. 
