a388576b9f4094120aad2fb56bb8db01c82aa618
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-06 00:13:30 +0200

    Refactor JPetScopeReader to work with a new scheme Part I !THIS COMMIT IS NOT A COMPLETE VERSION - SCOPE EXAMPLE WILL NOT WORK
    Former-commit-id: 36b023adaf31e96033b6890015c7c00ad8c79b92

--- JPetScopeReader/JPetScopeReaderTest.cpp
+++ JPetScopeReader/JPetScopeReaderTest.cpp
@@ -8,6 +8,7 @@
 #include "../JPetManager/JPetManager.h"
 
 #include "JPetScopeReaderFixtures.h"
+#include "../JPetScopeReader/JPetScopeReader.h"
 
 
 BOOST_AUTO_TEST_SUITE (JPetScopeReaderTestSuite)
@@ -51,4 +52,44 @@ BOOST_FIXTURE_TEST_CASE (tref_correctness_test, tref_correctness_fixture) {
   );
   //check_tref_simple(nullptr);
 }
+
+BOOST_AUTO_TEST_CASE (createInputScopeFileNames) 
+{
+  using VecOfStrings = std::vector<std::string>;
+  JPetScopeReader reader(0);
+  BOOST_REQUIRE(reader.createInputScopeFileNames("").empty());
+  BOOST_REQUIRE(reader.createInputScopeFileNames("non_existing").empty());
+  BOOST_REQUIRE(!reader.createInputScopeFileNames("unitTestData/JPetScopeReaderTest/scope_files/0").empty());
+  VecOfStrings expectedRes {
+    "C1_00003.txt",  "C2_00003.txt",  "C3_00003.txt",  "C4_00003.txt",
+    "C1_00004.txt",  "C2_00004.txt",  "C3_00004.txt",  "C4_00004.txt"
+  };
+  std::string pathToFiles = "unitTestData/JPetScopeReaderTest/scope_files/0";
+  std::transform(expectedRes.begin(), expectedRes.end(),expectedRes.begin(), 
+                [&](std::string name) {
+                  return pathToFiles + "/"+name;
+                }); 
+   
+  auto obtainedRes = reader.createInputScopeFileNames(pathToFiles);
+  BOOST_REQUIRE(!obtainedRes.empty());
+  /// to assure the same order of elements for comparison
+  std::sort(obtainedRes.begin(), obtainedRes.end());
+  std::sort(expectedRes.begin(), expectedRes.end());
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(obtainedRes.begin(), obtainedRes.end(), expectedRes.begin(), expectedRes.end());
+}
+
+BOOST_AUTO_TEST_CASE (isCorrectScopeFileName) 
+{
+  JPetScopeReader reader(0);
+  BOOST_REQUIRE(!reader.isCorrectScopeFileName(""));
+  BOOST_REQUIRE(!reader.isCorrectScopeFileName("C1_004.gif"));
+  BOOST_REQUIRE(!reader.isCorrectScopeFileName("C1004.txt"));
+  BOOST_REQUIRE(!reader.isCorrectScopeFileName("004.txt"));
+  BOOST_REQUIRE(!reader.isCorrectScopeFileName("C5_abc.txt"));
+  BOOST_REQUIRE(reader.isCorrectScopeFileName("C1_023.txt"));
+  BOOST_REQUIRE(reader.isCorrectScopeFileName("C5_000.txt"));
+  BOOST_REQUIRE(reader.isCorrectScopeFileName("AA_004.txt"));
+  BOOST_REQUIRE(reader.isCorrectScopeFileName("AA_004.txt"));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
