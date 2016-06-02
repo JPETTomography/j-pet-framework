f9241caabf6e6b5989ac115a84552138076d3e3e
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-06-02 10:21:30 +0200

    Add tests to JPetScopeParamGetter
    Former-commit-id: a3d518ba91e0518a141e355d0468ed953cc56467

--- JPetScopeParamGetter/JPetScopeParamGetterTest.cpp
+++ JPetScopeParamGetter/JPetScopeParamGetterTest.cpp
@@ -12,17 +12,12 @@ BOOST_AUTO_TEST_CASE(defaultConstructorTest)
 
 BOOST_AUTO_TEST_CASE(dummyFillingTest)
 {
+  JPetScopeParamGetter::clearParamCache();
   using namespace scope_config;
   using VecOfStrings = std::vector<std::string>;
   JPetScopeParamGetter paramGetter;
-  Config config;
-  config.fLocation="data";
-  config.fCollimatorPositions = VecOfStrings { "1 5 2", "12", "6"};
-  config.fBSlots= std::vector<BSlot>{ BSlot(-1,false,"",-1., -1), BSlot(-1,false,"",-1., -1)};
-  config.fPMs = std::vector<PM>{PM(3,"C2"), PM(98, "C4"), PM(32, "C1"), PM(42, "C3")}; 
-  config.fScins=std::vector<Scin>{Scin(32), Scin(12)};
-  config.fName="config1";
-  JPetParamBank* bank = paramGetter.generateParamBank(config);
+  const char* testConfigFile = "unitTestData/JPetScopeParamGetterTest/config.json";
+  JPetParamBank* bank = paramGetter.generateParamBank(testConfigFile);
   
   BOOST_REQUIRE(bank->getScintillatorsSize() == 2);
   BOOST_REQUIRE(bank->getPMsSize() == 4);
