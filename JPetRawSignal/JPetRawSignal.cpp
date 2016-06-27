4f05262b229b1aed341a6f7ae91a5587a0ed3d0b
Aleksander Gajos <alek.gajos@gmail.com>
2016-06-27 10:52:26 +0200

    Add convenience methods for TOT in RawSignal

--- JPetRawSignal/JPetRawSignal.cpp
+++ JPetRawSignal/JPetRawSignal.cpp
@@ -64,15 +64,15 @@ std::vector<JPetSigCh> JPetRawSignal::getPoints(
 }
 
 
-const JPetSigCh& JPetRawSignal::getTOTPoint() const {
-  return fTOTPoint;
-}
+/* const JPetSigCh& JPetRawSignal::getTOTPoint() const { */
+/*   return fTOTPoint; */
+/* } */
 
-void JPetRawSignal::setTOTPoint(const JPetSigCh & totSigCh) {
-  if (totSigCh.getType() == JPetSigCh::Charge) {
-    fTOTPoint = totSigCh;
-  }
-}
+/* void JPetRawSignal::setTOTPoint(const JPetSigCh & totSigCh) { */
+/*   if (totSigCh.getType() == JPetSigCh::Charge) { */
+/*     fTOTPoint = totSigCh; */
+/*   } */
+/* } */
 
 std::map<int, double> JPetRawSignal::getTimesVsThresholdNumber(JPetSigCh::EdgeType edge) const {
   std::map<int, double> map;
@@ -95,6 +95,35 @@ std::map<int, double> JPetRawSignal::getTimesVsThresholdValue(JPetSigCh::EdgeTyp
   return map;
 }
 
-double JPetRawSignal::getTOT() const {
-  return fTOTPoint.getValue();
+std::map<int, double> JPetRawSignal::getTOTsVsThresholdNumber() const {
+
+  std::map<int, double> map;
+  
+  for( std::vector<JPetSigCh>::const_iterator it1 = fLeadingPoints.begin(); it1!=fLeadingPoints.end(); ++it1){
+    for( std::vector<JPetSigCh>::const_iterator it2 = fTrailingPoints.begin(); it2!=fTrailingPoints.end(); ++it2){
+      if( it1->getThresholdNumber() == it2->getThresholdNumber() ){
+	map[ it1->getThresholdNumber() ] = it2->getValue() - it1->getValue();	
+      }
+    }
+  }
+  return map;
 }
+
+std::map<int, double> JPetRawSignal::getTOTsVsThresholdValue() const {
+  std::map<int, double> map;
+
+  for( std::vector<JPetSigCh>::const_iterator it1 = fLeadingPoints.begin(); it1!=fLeadingPoints.end(); ++it1){
+    for( std::vector<JPetSigCh>::const_iterator it2 = fTrailingPoints.begin(); it2!=fTrailingPoints.end(); ++it2){
+      if( it1->getThreshold() == it2->getThreshold() ){
+	map[ it1->getThreshold() ] = it2->getValue() - it1->getValue();	
+      }
+    }
+  }
+
+  return map;
+}
+
+
+/* double JPetRawSignal::getTOT() const { */
+/*   return fTOTPoint.getValue(); */
+/* } */
