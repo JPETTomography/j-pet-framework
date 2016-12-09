81d21e3c8e2c216b29235be8bf8a551264fc5688
Grey <grey277@wp.pl>
2016-12-09 20:06:10 +0100

    Fix typo

--- JPetSinogram/JPetSinogramTest.cpp
+++ JPetSinogram/JPetSinogramTest.cpp
@@ -8,14 +8,14 @@ using namespace boost::numeric::ublas;
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
 
-BOOST_AUTO_TEST_CASE( 0_degree_emissionMatrix_sinogram )
+BOOST_AUTO_TEST_CASE( emissionMatrix_sinogram_0_degree )
 {
   matrix<int> m (3, 3);
     for (unsigned i = 0; i < m.size1 (); ++ i)
         for (unsigned j = 0; j < m.size2 (); ++ j)
             m (i, j) = 1;
 
-  JPetSinogram::JPetSinogram sin;
+  JPetSinogram sin;
   BOOST_REQUIRE_EQUAL(sin.forwardProjection(0, 0, m), 3ll);
 }
 
