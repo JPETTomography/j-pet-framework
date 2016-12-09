41b07e23165783a353d849d834ad77383444f300
Grey <grey277@wp.pl>
2016-12-09 21:24:04 +0100

    Add 45theta case

--- JPetSinogram/JPetSinogram.cpp
+++ JPetSinogram/JPetSinogram.cpp
@@ -16,6 +16,17 @@ long long JPetSinogram::forwardProjection(float s, float theta, matrix<int> emis
     for(unsigned int i = 0; i < emissionMatrix.size1(); i++) {
       sum += emissionMatrix(i, s);
     }
+  } else if(theta == 45) {
+    unsigned int j = (emissionMatrix.size2() - 1) - s < 0 ? 0 : (emissionMatrix.size2() - 1) - s;
+    unsigned int i = (emissionMatrix.size2() - 1) - s < 0 ? - ((emissionMatrix.size2() - 1 - s)) : 0;// select row, start from upper right corner
+    for(; j < emissionMatrix.size2(); j++) { //-1 bcs matrix start from 0
+      if(i < emissionMatrix.size1()) {
+        sum += emissionMatrix(i, j);
+        i++;
+      }
+      else { } // exception
+    }
+
   }
 
   return sum;
