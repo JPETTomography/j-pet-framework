3983c622f3f88d21613dc64b0250346b51fa49ef
Grey <grey277@wp.pl>
2016-12-19 21:39:34 +0100

    Attept to create sinogram, not working 2d array on smart pointers

--- JPetSinogram/JPetSinogram.h
+++ JPetSinogram/JPetSinogram.h
@@ -3,16 +3,22 @@
 #define _JPETSINOGRAM_H_
 
 #include <boost/numeric/ublas/matrix.hpp>
+#include <vector>
+#include <utility>
 using namespace boost::numeric::ublas;
 class JPetSinogram {
 public:
   JPetSinogram();
   ~JPetSinogram();
 
+  typedef std::shared_ptr<double[]> ManagedDouble;
+  typedef std::shared_ptr<ManagedDouble[]> resultType;
+
   long long forwardProjection(float s, float theta, matrix<int> emissionMatrix); //row major orientation, element m[i,j]
   //is mapped as i*n + j element
+  resultType sinogram(matrix<int> emissionMatrix, int views, int scans);
 private:
-
+  void normalize2DArray(resultType data, int imax, int jmax, double min, double max);
 };
 
 
