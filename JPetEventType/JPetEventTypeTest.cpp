65d870e94dd51e26a3b79f63e7e5ce0574d6231b
kmuzalewska <klara.muzalewska@gmail.com>
2016-12-30 17:16:40 +0100

    mistakes to check parser

--- JPetEventType/JPetEventTypeTest.cpp
+++ JPetEventType/JPetEventTypeTest.cpp
@@ -20,7 +20,7 @@ BOOST_AUTO_TEST_CASE( JPetEventTest2 )
 {
   auto eventType1 = JPetEventType::kUnknown | JPetEventType::k2Gamma | JPetEventType::kPrompt;
   auto eventTypeRes = eventType1 & ~JPetEventType::k2Gamma; /// unset flag
-  BOOST_REQUIRE((eventTypeRes & JPetEventType::kUnknown) == JPetEventType::kUnknown);
+  BOOST_REQUIRE((eventTypeRes & JPetEventType::kUnknown) != JPetEventType::kUnknown);
   BOOST_REQUIRE((eventTypeRes & JPetEventType::k2Gamma) != JPetEventType::k2Gamma);
   BOOST_REQUIRE((eventTypeRes & JPetEventType::kPrompt) == JPetEventType::kPrompt);
 }
