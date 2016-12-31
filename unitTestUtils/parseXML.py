3172eb384438bbed6d274ee221fa13eaa9f7a84f
kmuzalewska <klara.muzalewska@gmail.com>
2016-12-31 12:59:12 +0100

    git add parseXML.py !

--- unitTestUtils/parseXML.py
+++ unitTestUtils/parseXML.py
@@ -8,14 +8,13 @@ import glob
 def parse():
     for infile in glob.glob('*.xml'):
         try:
-                tree = ET.parse(infile)
+            tree = ET.parse(infile)
+            root = tree.getroot()
+            if ((root.findall('.//FatalError'))!=[]):
+                print "Error detectedA"
+                return 1
         except ParseError:
             print "The file xml isn't correct. There were some mistakes in the tests "
             return 1
-        else:
-            root = tree.getroot()
-            if ((root.findall('.//FatalError'))!=[]):
-                print "Error detected"
-                return 1
 
 parse()
