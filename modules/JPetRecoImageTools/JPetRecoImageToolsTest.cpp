84671841d070279dbb5af7de815c4d918fe0e155
Grey <grey277@wp.pl>
2017-01-01 17:11:32 +0100

    Remove magic number in scale(i dont know what it was for and it seems to work without it, add checks to sinValue, cosValue, a, fix interpolation, add simple 1 point test

--- modules/JPetRecoImageTools/JPetRecoImageToolsTest.cpp
+++ modules/JPetRecoImageTools/JPetRecoImageToolsTest.cpp
@@ -12,60 +12,44 @@ using namespace boost::numeric::ublas;
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
 
-/*BOOST_AUTO_TEST_CASE(ONE_POINT_MATRIX_IN_CENTER)
-{
-  matrix<int> m(10, 10);
-  for (unsigned int i = 0; i < 10; i++)
-  {
-    for (unsigned int j = 0; j < 10; j++)
-    {
-      m(i, j) = 0;
-    }
-  }
-
-  m(5, 5) = 100;
-  int views = 2;
-  int scans = 10;
-  std::vector<std::vector<double>> result = JPetRecoImageTools::sinogram(m, views, scans);
-  for (int i = 0; i < views; i++)
-  {
-    for (int j = 0; j < scans; j++)
-    {
-      std::cout << result[i][j] << " ";
-      //if(j == 4) 
-      //  BOOST_REQUIRE_EQUAL((int)result[i][j], 100);
-      //else 
-      //  BOOST_REQUIRE_EQUAL((int)result[i][j], 0);
-    }
-    std::cout << std::endl;
-  }
-}
-
 BOOST_AUTO_TEST_CASE(ONE_POINT_MATRIX_NOT_IN_CENTER)
 {
-  matrix<int> m(10, 10);
-  for (unsigned int i = 0; i < 10; i++)
+  std::vector<std::vector<int>> m(4, std::vector<int>(4));
+  for (unsigned int i = 0; i < 4; i++)
   {
-    for (unsigned int j = 0; j < 10; j++)
+    for (unsigned int j = 0; j < 4; j++)
     {
-      m(i, j) = 0;
+      m[i][j] = 0;
     }
   }
-
-  m(3, 3) = 100;
+  m[2][2] = 100;
   int views = 2;
-  int scans = 10;
+  int scans = 4;
   std::vector<std::vector<double>> result = JPetRecoImageTools::sinogram(m, views, scans);
   for (int i = 0; i < views; i++)
   {
     for (int j = 0; j < scans; j++)
     {
-      std::cout << (int)result[i][j] << " "; //barray[i + j * views] = (byte) projection[i][j];
+      if((i == 0 && j == 2) || (i == 1 && j == 1))
+        BOOST_REQUIRE_EQUAL(result[i][j], 100);
+      else
+        BOOST_REQUIRE_EQUAL(result[i][j], 0); 
     }
-    std::cout << std::endl;
+  }
+
+  std::vector<std::vector<double>> result2 = JPetRecoImageTools::sinogram(m, views, scans, JPetRecoImageTools::nearestNeighbour);
+  for (int i = 0; i < views; i++)
+  {
+    for (int j = 0; j < scans; j++)
+    {
+      if((i == 0 && j == 2) || (i == 1 && j == 1))
+        BOOST_REQUIRE_EQUAL(result2[i][j], 100);
+      else
+        BOOST_REQUIRE_EQUAL(result2[i][j], 0); 
+    }
   }
 }
-*/
+
 BOOST_AUTO_TEST_CASE(sinogram)
 {
 
@@ -74,7 +58,7 @@ BOOST_AUTO_TEST_CASE(sinogram)
   std::string line;
   getline(in, line);
   if (line != "P2")
-  { /*error*/
+  { 
   }
 
   unsigned int width;
@@ -90,10 +74,7 @@ BOOST_AUTO_TEST_CASE(sinogram)
     {
       in >> val;
       m[i][j] = val;
-      //std::cout << val << " ";
     }
-
-    //std::cout << std::endl;
   }
   int views = 180;
   int scans = 256;
