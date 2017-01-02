c439c26f610dc5cdc92971eeda1e0075a9d7125f
kmuzalewska <klara.muzalewska@gmail.com>
2017-01-02 19:49:40 +0100

    Parser

--- unitTestUtils/parseXML.py
+++ unitTestUtils/parseXML.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from xml.etree.ElementTree import ParseError
 import xml.etree.ElementTree as ET
 import glob
 import sys
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def parse():
     for infile in glob.glob('*.xml'):
-	print infile
         try:
             tree = ET.parse(infile)
             root = tree.getroot()
             if root.findall('.//FatalError'):
-                print "Error detectedA"
-                sys.exit(1)
+                eprint("Error detected")
         except ParseError:
-            print "The file xml isn't correct. There were some mistakes in the tests "
-            sys.exit(1)
+            eprint("The file xml isn't correct. There were some mistakes in the tests ")
 
 parse()
