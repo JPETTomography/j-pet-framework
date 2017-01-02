1d824e262413faf14f7a2718c0a46901356be648
kmuzalewska <klara.muzalewska@gmail.com>
2017-01-02 18:27:32 +0100

    parser

--- unitTestUtils/parseXML.py
+++ unitTestUtils/parseXML.py
@@ -8,17 +8,15 @@ import sys
 
 def parse():
     for infile in glob.glob('*.xml'):
+	print infile
         try:
             tree = ET.parse(infile)
             root = tree.getroot()
-            if ((root.findall('.//FatalError'))!=[]):
+            if root.findall('.//FatalError'):
                 print "Error detectedA"
                 sys.exit(1)
         except ParseError:
             print "The file xml isn't correct. There were some mistakes in the tests "
             sys.exit(1)
-def main():
-    parse()
 
-if __name__ == '_main_':
-    main()
+parse()
