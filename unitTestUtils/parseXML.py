09ab19a21d683140afc1cf188b8b68ccc8a2d588
kmuzalewska <klara.muzalewska@gmail.com>
2017-01-03 23:14:32 +0100

    Fixed problem with travis

--- unitTestUtils/parseXML.py
+++ unitTestUtils/parseXML.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from xml.etree.ElementTree import ParseError
 import xml.etree.ElementTree as ET
 import glob
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 def parse():
     for infile in glob.glob('*.xml'):
-	try:
+        try:
             tree = ET.parse(infile)
-        except ParseError:
-    	    print "The file xml isn't correct. There were some mistakes in the tests "
-            return 1
-	else:
             root = tree.getroot()
-	    if ((root.findall('.//FatalError'))!=[]):
-                print "Error detected"
-	        return 1
+            if root.findall('.//FatalError'):
+                eprint("Error detected")
+                sys.exit(1)
+        except ParseError:
+            eprint("The file xml isn't correct. There were some mistakes in the tests ")
+            sys.exit(1)
 
-parse()
+def main():
+    parse()
+
+if __name__ == '__main__':
+    main()
