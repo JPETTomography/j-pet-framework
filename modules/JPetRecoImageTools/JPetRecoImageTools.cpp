31093840450b6780574ed3a4e34cd90ba4d49bbb
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-01-03 10:35:10 +0100

    Add some typdef to JPetRecoImageTools to  make the code more readable
    One test is not passing!!!

--- modules/JPetRecoImageTools/JPetRecoImageTools.cpp
+++ modules/JPetRecoImageTools/JPetRecoImageTools.cpp
@@ -10,8 +10,8 @@ JPetRecoImageTools::JPetRecoImageTools() {}
 
 JPetRecoImageTools::~JPetRecoImageTools() {}
 
-double JPetRecoImageTools::nearestNeighbour(std::vector<std::vector<int>> &emissionMatrix, double a, double b,
-                                            int center, int x, int y, bool sang)
+double JPetRecoImageTools::nearestNeighbour(Matrix2D& emissionMatrix, double a, double b,
+    int center, int x, int y, bool sang)
 {
   if (sang) {
     y = (int)std::round(a * x + b) + center;
@@ -21,49 +21,37 @@ double JPetRecoImageTools::nearestNeighbour(std::vector<std::vector<int>> &emiss
   } else {
     x = (int)std::round(a * y + b) + center + 1; // not really know why need to +1 to x, but it works
     if (x >= 0 && x < (int)emissionMatrix.size())
-      return emissionMatrix[x][y + center]; 
+      return emissionMatrix[x][y + center];
     else return 0;
   }
 }
 
-double JPetRecoImageTools::linear(std::vector<std::vector<int>> &emissionMatrix, double a, double b,
+double JPetRecoImageTools::linear(Matrix2D& emissionMatrix, double a, double b,
                                   int center, int x, int y, bool sang)
 {
   if (sang) {
     y = (int)std::round(a * x + b) + center;
     double weight = std::abs((a * x + b) - std::ceil(a * x + b));
     if (y >= 0 && y < (int)emissionMatrix[0].size()) {
-        return (1 - weight) * emissionMatrix[x + center][y] + weight * emissionMatrix[x + center][y + 1];
+      return (1 - weight) * emissionMatrix[x + center][y] + weight * emissionMatrix[x + center][y + 1];
     } else return 0;
   } else {
     x = (int)std::round(a * y + b) + center + 1; // not really know why need to +1 to x, but it works
     double weight = std::abs((a * y + b) - std::ceil(a * y + b));
     if (x >= 0 && x + 1 < (int)emissionMatrix.size()) {
-      if(weight == 0)
+      if (weight == 0)
         return emissionMatrix[x][y + center];
-      else 
+      else
         return (1 - weight) * emissionMatrix[x][y + center] + weight * emissionMatrix[x + 1][y + center];
     } else return 0;
   }
 }
 
-/*! \brief Function returning vector of vectors with value of sinogram 
- *  \param emissionMatrix matrix, need to be NxN
- *  \param views number of views on object, degree step is calculated as (ang2 - ang1) / views
- *  \param scans number of scans on object, step is calculated as emissionMatrix[0].size() / scans
- *  \param interpolationFunction function to interpolate values (Optional, default linear)
- *  \param ang1 start angle for projection (Optional, default 0)
- *  \param ang2 end angle for projection (Optional, default 180) 
- *  \param scaleResult if set to true, scale result to <min, max> (Optional, default false)
- *  \param min minimum value in returned sinogram (Optional, default 0)
- *  \param max maximum value in returned sinogram (Optional, default 255)
-*/
 
-std::vector<std::vector<double>> JPetRecoImageTools::sinogram(std::vector<std::vector<int>> &emissionMatrix,
-                                                              int views,int scans, 
-                                                              std::function<double(std::vector<std::vector<int>>&,
-                                                              double, double, int, int, int, bool)> interpolationFunction,
-                                                              float ang1, float ang2, bool scaleResult, int min, int max)
+JPetRecoImageTools::Matrix2DProj JPetRecoImageTools::sinogram(Matrix2D& emissionMatrix,
+    int views, int scans,
+    InterpolationFunc interpolationFunction,
+    float ang1, float ang2, bool scaleResult, int min, int max)
 {
   assert(emissionMatrix.size() > 0);
   assert(emissionMatrix.size() == emissionMatrix[0].size());
@@ -92,44 +80,40 @@ std::vector<std::vector<double>> JPetRecoImageTools::sinogram(std::vector<std::v
   double sinValue = 0., cosValue = 0.;
   double a = 0., aa = 0.;
 
-  for (phi = ang1; phi < ang2; phi = phi + stepsize)
-  {
+  for (phi = ang1; phi < ang2; phi = phi + stepsize) {
     sinValue = std::sin((double)phi * M_PI / 180 - M_PI / 2);
     cosValue = std::cos((double)phi * M_PI / 180 - M_PI / 2);
-    if(std::abs(sinValue) < 0.0000001) {
+    if (std::abs(sinValue) < 0.0000001) {
       sinValue = 0;
     }
-    if(std::abs(cosValue) < 0.0000001) {
+    if (std::abs(cosValue) < 0.0000001) {
       cosValue = 0;
     }
     a = -cosValue / sinValue;
-    if(std::isinf(a) || std::isinf(-a) || std::abs(a) < 0.0000001) {
+    if (std::isinf(a) || std::isinf(-a) || std::abs(a) < 0.0000001) {
       a = 0;
       aa = 0;
     } else aa = 1 / a;
-    for (int scanNumber = 0; scanNumber < scans; scanNumber++)
-    {
+    for (int scanNumber = 0; scanNumber < scans; scanNumber++) {
       N = scanNumber - scans / 2;
       proj[i][scans - 1 - scanNumber] = JPetRecoImageTools::calculateValue(emissionMatrix, std::abs(sinValue) > sang,
-                                                               N, cosValue, sinValue, scale, center,
-                                                               interpolationFunction, a, aa);
+                                        N, cosValue, sinValue, scale, center,
+                                        interpolationFunction, a, aa);
     }
     i++;
   }
 
-  if (scaleResult)
-  {
+  if (scaleResult) {
     JPetRecoImageTools::scale(proj, min, max);
   }
 
   return proj;
 }
 
-double JPetRecoImageTools::calculateValue(std::vector<std::vector<int>> &emissionMatrix, bool sang, int N, double cos,
-                                          double sin, double scale, int center,
-                                          std::function<double(std::vector<std::vector<int>> &,
-                                          double, double, int, int, int, bool)> &interpolationFunction,
-                                          double a, double aa)
+double JPetRecoImageTools::calculateValue(Matrix2D& emissionMatrix, bool sang, int N, double cos,
+    double sin, double scale, int center,
+    InterpolationFunc& interpolationFunction,
+    double a, double aa)
 {
   double b = 0.;
   if (sang)
@@ -140,18 +124,13 @@ double JPetRecoImageTools::calculateValue(std::vector<std::vector<int>> &emissio
   double value = 0.;
   int x = 0;
   int y = 0;
-  if (sang)
-  {
-    for (x = -center; x < center; x++)
-    {
+  if (sang) {
+    for (x = -center; x < center; x++) {
       value += interpolationFunction(emissionMatrix, a, b, center, x, y, sang);
     }
     value /= std::abs(sin);
-  }
-  else
-  {
-    for (y = -center; y < center; y++)
-    {
+  } else {
+    for (y = -center; y < center; y++) {
       value += interpolationFunction(emissionMatrix, aa, b, center, x, y, sang);
     }
     value /= std::abs(cos);
@@ -159,14 +138,14 @@ double JPetRecoImageTools::calculateValue(std::vector<std::vector<int>> &emissio
   return value;
 }
 
-void JPetRecoImageTools::scale(std::vector<std::vector<double>> &v, int min, int max)
+void JPetRecoImageTools::scale(Matrix2DProj& v, int min, int max)
 {
+
   double datamax = v[0][0];
   double datamin = v[0][0];
-  for (unsigned int k = 0; k < v.size(); k++)
-  {
-    for (unsigned int j = 0; j < v[0].size(); j++)
-    {
+  //auto datamin = *std::min(v.begin(), v.end(), [] (const std::vector<double> & a, const std::vector<double>& b) { return std::min(a.begin(), a.end()) <std::min(b.begin(), b.end());});
+  for (unsigned int k = 0; k < v.size(); k++) {
+    for (unsigned int j = 0; j < v[0].size(); j++) {
       if (v[k][j] < min)
         v[k][j] = min;
       if (v[k][j] > datamax)
@@ -179,10 +158,8 @@ void JPetRecoImageTools::scale(std::vector<std::vector<double>> &v, int min, int
   if (datamax == 0.) // if max value is 0, no need to scale
     return;
 
-  for (unsigned int k = 0; k < v.size(); k++)
-  {
-    for (unsigned int j = 0; j < v[0].size(); j++)
-    {
+  for (unsigned int k = 0; k < v.size(); k++) {
+    for (unsigned int j = 0; j < v[0].size(); j++) {
       v[k][j] = (double)((v[k][j] - datamin) * max / datamax);
     }
   }
