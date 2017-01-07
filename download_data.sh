e5e9f4401916f14392b9e71c08415f91af03e629
timorl <tymorl@gmail.com>
2017-01-07 20:25:17 +0100

    Test data fetching with checksums

--- download_data.sh
+++ download_data.sh
@@ -3,71 +3,88 @@
 #e.g. download_data.sh /blabla/path
 #will write the output to /blabla/path 
 
-#for scp
-SCP_INPUT=/home/framework_configs
-SCP_OUTPUT=./DBHandler/Config
-declare -a CFG_FILES=("configDB.cfg" "configDB.cfg.koza" "ReadMe.txt")
-SERVER=sphinx.if.uj.edu.pl
+#if there is an extra argument treat it as the output path 
+#if the argument is empty set some default paths
+BASE_PATH=.
+if [ ! -z $1 ]; then
+  BASE_PATH=$1
+fi
 
+CHECKSUM_COMMAND="shasum -c"
+
+#for scp
+CFG_DIR=/home/framework_configs
+CFG_OUTPUT=$BASE_PATH/DBHandler/Config
+declare -a CFG_FILES=("configDB.cfg" "configDB.cfg.koza" "ReadMe.txt")
+CFG_SERVER=sphinx.if.uj.edu.pl
+
+declare -a CFG_FILES_IN=()
+for FILE in "${CFG_FILES[@]}"
+do
+  CFG_FILES_IN+=" ${CFG_DIR}/${FILE}"
+done
+
+declare -a CFG_FILES_OUT=()
+for FILE in "${CFG_FILES[@]}"
+do
+  CFG_FILES_OUT+=" ${CFG_OUTPUT}/${FILE}"
+done
 
 #for wget
-WGET_DIR="unitTestData"
-WGET_INPUT="http://sphinx.if.uj.edu.pl/framework/"${WGET_DIR}
-WGET_OUTPUT="./"
-# -r  means recursive, 
-# --cut-dirs=1 ignore given level of directories (e.g. remove framework from path) 
+TEST_DATA_DIR="unitTestData"
+TEST_CHECKSUM_FILE=$TEST_DATA_DIR.sha
+TEST_BASE_URL="http://sphinx.if.uj.edu.pl/framework"
+TEST_DATA_URL=$TEST_BASE_URL/$TEST_DATA_DIR
+TEST_CHECKSUM_URL=$TEST_BASE_URL/$TEST_CHECKSUM_FILE
+TEST_OUTPUT=$BASE_PATH
+LOCAL_TEST_CHECKSUM_FILE=$TEST_OUTPUT/$TEST_CHECKSUM_FILE
+# -r  means recursive,
+# --cut-dirs=1 ignore given level of directories (e.g. remove framework from path)
 # -nH  Disable generation of host-prefixed directories (so save only unitTestData and not full path)
 # -np no parents
 # --reject="index.html*" - do not store all index.html files
 # -e robots=off - remove some robot generated files
 declare -a WGET_FLAGS=(-r -nH -np --cut-dirs=1 --reject="index.html*" -e robots=off)
 
-#if there is an extra argument treat it as the output path 
-#if the argument is empty set some default paths
-if [ -z $1 ]; then
-  SCP_OUTPUT=.
-  WGET_OUTPUT=.
-else
-  mkdir -p $1/DBHandler/Config
-  SCP_OUTPUT=$1/DBHandler/Config
-  WGET_OUTPUT=$1
-fi
-
-declare -a CFG_FILES_IN=()
-for FILE in "${CFG_FILES[@]}"
-do
-  CFG_FILES_IN+=" ${SCP_INPUT}/${FILE}"
-done
-
-declare -a CFG_FILES_OUT=()
-for FILE in "${CFG_FILES[@]}"
-do
-  CFG_FILES_OUT+=" ${SCP_OUTPUT}/${FILE}"
-done
-
 #downloading test data via wget
-if [ ! -d "${WGET_OUTPUT}/${WGET_DIR}" ]; then
-  wget "${WGET_INPUT}" "${WGET_FLAGS[@]}" -P "${WGET_OUTPUT}" 
+# first get and check the checksums
+wget "${TEST_CHECKSUM_URL}" "${WGET_FLAGS[@]}" -P "${TEST_OUTPUT}"
+if $CHECKSUM_COMMAND $LOCAL_TEST_CHECKSUM_FILE | grep 'FAIL'; then
+  echo "Some unit test data files don't exist or are wrong."
+  echo "Deleting the unit test data directory..."
+  rm -rf $TEST_OUTPUT/$TEST_DATA_DIR
+  echo "Downloading the unit test data..."
+  wget "${TEST_DATA_URL}" "${WGET_FLAGS[@]}" -P "${TEST_OUTPUT}"
+  if $CHECKSUM_COMMAND $LOCAL_TEST_CHECKSUM_FILE | grep 'FAIL'; then
+    echo "The downloaded data does not match the checksum!"
+    exit 1
+  else
+    echo "All unit test data OK."
+  fi
 else
-  echo "directory ${WGET_OUTPUT}/${WGET_DIR} already exists !"
-  echo "downloading interrupted!"
+  echo "All unit test data OK."
 fi
+rm $LOCAL_TEST_CHECKSUM_FILE
 
 #check if scp files are already downloaded
-not_downloaded=false
+#if at least one exists assume it was downloaded (because you usually overwrite one with the other)
+DOWNLOADED=""
 for FILE in ${CFG_FILES_OUT[@]}
 do
-  if [ ! -f $FILE ]; then
-    not_downloaded=true 
+  if [ -f $FILE ]; then
+    DOWNLOADED="true"
     break
   fi
 done
-echo ${not_downloaded}
-if [ ${not_downloaded} = true ] ; then
-  #downloading sensinble data via scp
-  read -p "username at ${SERVER}:" USERNM;
-  scp ${USERNM}@${SERVER}:"${CFG_FILES_IN}" ${SCP_OUTPUT}
+if [ ! -z $DOWNLOADED ] ; then
+  echo "Config files seem to exist, not downloading them again."
 else
-  echo "config files seems to exist!"
-  echo "downloading interrupted!"
+  #downloading sensinble data via scp
+  mkdir -p $CFG_OUTPUT
+  echo "Fetching database config files."
+  echo "To avoid downloading leave the username empty."
+  read -p "username at ${CFG_SERVER}:" USERNM;
+  if [ ! -z USERNM ]; then
+    scp ${USERNM}@${CFG_SERVER}:"${CFG_FILES_IN}" ${CFG_OUTPUT}
+  fi
 fi
