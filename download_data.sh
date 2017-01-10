df19e31e9b1aaf0ab5c0fef8d8c12aacb1c61477
timorl <tymorl@gmail.com>
2017-01-10 12:39:20 +0100

    Data fetching fixes and improvement
    Now also works when invoked in another directory (as per the manual).
    Only downloads unit test data that doesn't exist or match the checksums.

--- download_data.sh
+++ download_data.sh
@@ -10,7 +10,14 @@ if [ ! -z $1 ]; then
   BASE_PATH=$1
 fi
 
-CHECKSUM_COMMAND="shasum -c"
+
+function check_sums {
+	local CUR_DIR=$PWD
+	cd $BASE_PATH
+	shasum -c $1
+	cd $CUR_DIR
+}
+CHECKSUM_COMMAND="check_sums"
 
 #for scp
 CFG_DIR=/home/framework_configs
@@ -31,36 +38,26 @@ do
 done
 
 #for wget
-TEST_DATA_DIR="unitTestData"
-TEST_CHECKSUM_FILE=$TEST_DATA_DIR.sha
+TEST_CHECKSUM_FILE="all_data.sha"
 TEST_BASE_URL="http://sphinx.if.uj.edu.pl/framework"
-TEST_DATA_URL=$TEST_BASE_URL/$TEST_DATA_DIR
 TEST_CHECKSUM_URL=$TEST_BASE_URL/$TEST_CHECKSUM_FILE
 TEST_OUTPUT=$BASE_PATH
 LOCAL_TEST_CHECKSUM_FILE=$TEST_OUTPUT/$TEST_CHECKSUM_FILE
-# -r  means recursive,
-# --cut-dirs=1 ignore given level of directories (e.g. remove framework from path)
+# -x create subdirectories,
 # -nH  Disable generation of host-prefixed directories (so save only unitTestData and not full path)
-# -np no parents
-# --reject="index.html*" - do not store all index.html files
-# -e robots=off - remove some robot generated files
-declare -a WGET_FLAGS=(-r -nH -np --cut-dirs=1 --reject="index.html*" -e robots=off)
+# --cut-dirs=1 ignore given level of directories (e.g. remove framework from path)
+declare -a WGET_FLAGS=(-x -nH --cut-dirs=1)
 
 #downloading test data via wget
 # first get and check the checksums
 wget "${TEST_CHECKSUM_URL}" "${WGET_FLAGS[@]}" -P "${TEST_OUTPUT}"
+for FILE in $($CHECKSUM_COMMAND $LOCAL_TEST_CHECKSUM_FILE | grep 'FAIL' | sed 's/:.*//'); do
+  TEST_FILE_URL=$TEST_BASE_URL/$FILE
+  wget "${TEST_FILE_URL}" "${WGET_FLAGS[@]}" -P "${TEST_OUTPUT}"
+done
 if $CHECKSUM_COMMAND $LOCAL_TEST_CHECKSUM_FILE | grep 'FAIL'; then
-  echo "Some unit test data files don't exist or are wrong."
-  echo "Deleting the unit test data directory..."
-  rm -rf $TEST_OUTPUT/$TEST_DATA_DIR
-  echo "Downloading the unit test data..."
-  wget "${TEST_DATA_URL}" "${WGET_FLAGS[@]}" -P "${TEST_OUTPUT}"
-  if $CHECKSUM_COMMAND $LOCAL_TEST_CHECKSUM_FILE | grep 'FAIL'; then
-    echo "The downloaded data does not match the checksum!"
-    exit 1
-  else
-    echo "All unit test data OK."
-  fi
+  echo "The downloaded data does not match the checksum!"
+  exit 1
 else
   echo "All unit test data OK."
 fi
