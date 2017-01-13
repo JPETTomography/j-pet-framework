002dc846045775ac1c4fa54015f454209deb9116
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-01-13 10:51:45 +0100

    Add rescale functor to be passed to sinogram method.

--- modules/JPetRecoImageTools/JPetRecoImageTools.h
+++ modules/JPetRecoImageTools/JPetRecoImageTools.h
@@ -27,10 +27,22 @@ public:
   using Matrix2D = std::vector<std::vector<int>>;
   using Matrix2DProj = std::vector<std::vector<double>>;
   using InterpolationFunc = std::function<double(const Matrix2D&, double, double, int, int, int, bool)>;
+  using RescaleFunc = std::function<void(Matrix2DProj& v, double minCutoff, double rescaleFactor)>;
+
   static double nearestNeighbour(const Matrix2D& emissionMatrix,
                                  double a, double b, int center, int x, int y, bool sang);
   static double linear(const Matrix2D& emissionMatrix,
                        double a, double b, int center, int x, int y, bool sang);
+  /// Rescale the Matrix in the following way:
+  /// 1. All the values less than minCutoff are set to minCutoff
+  /// 2. Removes the common backgroud term. So the values start at zero
+  /// 3. Rescales all values by rescaleFactor/maxElement
+  /// The final value range is from 0 to rescaleFactor
+  static void rescale(Matrix2DProj& v, double minCutoff, double rescaleFactor);
+  /// PseudoRescale which does nothing
+  static void nonRescale(Matrix2DProj&, double, double) {
+    return;
+  }
 
   /*! \brief Function returning sinogram matrix.
    *  \param emissionMatrix matrix,  needs to be NxN
@@ -39,21 +51,16 @@ public:
    *  \param interpolationFunction function to interpolate values (Optional, default linear)
    *  \param angleBeg start angle for projection in deg (Optional, default 0)
    *  \param angleEnd end angle for projection in deg(Optional, default 180)
-   *  \param scaleResult if set to true, scale result to <min, max> (Optional, default false)
-   *  \param min minimum value in returned sinogram (Optional, default 0)
-   *  \param max maximum value in returned sinogram (Optional, default 255)
+   *  \param rescaleFunc function that rescales the final result (Optional, default no rescaling)
   */
   static Matrix2DProj sinogram(Matrix2D& emissionMatrix,
                                int nViews, int nScans,
+                               double angleBeg = 0, double angleEnd = 180,
                                InterpolationFunc interpolationFunction = linear,
-                               double angleBeg = 0, double angleEnd = 180, bool scaleResult = false,
-                               int min = 0, int max = 255);
-  /// Rescale the Matrix in the following way:
-  /// 1. All the values less than minCutoff are set to minCutoff
-  /// 2. Removes the common backgroud term. So the values start at zero
-  /// 3. Rescales all values by rescaleFactor/maxElement
-  /// The final value range is from 0 to rescaleFactor
-  static void rescale(Matrix2DProj& v, double minCutoff, double rescaleFactor);
+                               RescaleFunc rescaleFunc = nonRescale,
+                               int rescaleMinCutoff = 0,
+                               int rescaleFactor = 255
+                              );
 private:
   JPetRecoImageTools();
   ~JPetRecoImageTools();
