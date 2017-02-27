c84968c47f6113fd3731cbd02fc8023656bc5f0e
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-02-27 15:26:57 +0100

    Add JPetGeomMappingInterface
    Based on the AbstractLargeBarrelMapping class from BarrelExtensions.
    Also, add implementation of this interface called JPetGeomMapping.
    This is just a copy of BarrelExtension version for a moment.
    Also, remove AbstractLargeBarrelMaping class and replace it
    by the new interface. Fix all the calls.

--- JPetLargeBarrelExtensions/TimeSyncDeltas.cpp
+++ JPetLargeBarrelExtensions/TimeSyncDeltas.cpp
@@ -16,33 +16,36 @@
 #include "TimeSyncDeltas.h"
 using namespace std;
 
-Synchronization::Synchronization(const shared_ptr < AbstractBarrelMapping > map, istream & str, const Synchronization::TimeCalculation timecalc)
-:f_mapping(map),f_time_calc(timecalc){
-    f_offsets=make_shared<JPetMap<SynchroStrip>>(f_mapping->getLayersSizes());
-    str>>(*f_offsets);
+Synchronization::Synchronization(const shared_ptr < JPetGeomMappingInterface > map, istream& str, const Synchronization::TimeCalculation timecalc)
+  : f_mapping(map), f_time_calc(timecalc)
+{
+  f_offsets = make_shared<JPetMap<SynchroStrip>>(f_mapping->getLayersSizes());
+  str >> (*f_offsets);
 }
-Synchronization::~Synchronization(){}
-const SynchroStrip Synchronization::get_times(const JPetHit & hit) const{
-    const auto& offsets=f_offsets->operator[](f_mapping->getStripPos(hit.getBarrelSlot()));
-    map<int,double> lead_times_A = hit.getSignalA().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Leading);
-    map<int,double> lead_times_B = hit.getSignalB().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Leading);
-    vector<double> A,B;
-    for(size_t thr=1;thr<=4;thr++){
-	A.push_back(lead_times_A[thr]);
-	B.push_back(lead_times_B[thr]);
-    }
-    SynchroStrip res;
-    res.A=f_time_calc(A)+offsets.A;
-    res.B=f_time_calc(B)+offsets.B;
-    return res;
+Synchronization::~Synchronization() {}
+const SynchroStrip Synchronization::get_times(const JPetHit& hit) const
+{
+  const auto& offsets = f_offsets->operator[](f_mapping->getStripPos(hit.getBarrelSlot()));
+  map<int, double> lead_times_A = hit.getSignalA().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Leading);
+  map<int, double> lead_times_B = hit.getSignalB().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Leading);
+  vector<double> A, B;
+  for (size_t thr = 1; thr <= 4; thr++) {
+    A.push_back(lead_times_A[thr]);
+    B.push_back(lead_times_B[thr]);
+  }
+  SynchroStrip res;
+  res.A = f_time_calc(A) + offsets.A;
+  res.B = f_time_calc(B) + offsets.B;
+  return res;
 }
 
-const Synchronization::TimeCalculation Thr(const std::list<size_t>&lst){
-    return [lst](const vector<double>&P)->const double{
-	double res=0;
-	for(const size_t i:lst)res+=P[i];
-	res/=lst.size();
-	return res;
-    };
+const Synchronization::TimeCalculation Thr(const std::list<size_t>& lst)
+{
+  return [lst](const vector<double>& P)->const double {
+    double res = 0;
+    for (const size_t i : lst)res += P[i];
+    res /= lst.size();
+    return res;
+  };
 }
 
