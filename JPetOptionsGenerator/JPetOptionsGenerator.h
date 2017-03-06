31d12230bcc2c945f5209fc50a3d8efbc65b5410
kmuzalewska <klara.muzalewska@gmail.com>
2017-03-06 20:32:37 +0100

    variablesMap to map<string, string>

--- JPetOptionsGenerator/JPetOptionsGenerator.h
+++ JPetOptionsGenerator/JPetOptionsGenerator.h
@@ -39,6 +39,8 @@ public:
   bool isOptionSet(const po::variables_map& variablesMap, const std::string& option) const; 
   
   std::string getOptionValue(const po::variables_map& variablesMap, const std::string& option) const;
+
+  std::map<std::string, std::string> variablesMapToOption(const po::variables_map& variablesMap) const; 
   
   inline const std::vector<std::string>& getFileNames(const po::variables_map& variablesMap) const {
     return variablesMap["file"].as< std::vector<std::string> >();
