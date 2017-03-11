7ad8a196d714ff8419f93e37c3a25e266303769a
kmuzalewska <klara.muzalewska@gmail.com>
2017-03-11 16:25:50 +0100

    fabric to JetLayer

--- JPetLayer/JPetLayer.cpp
+++ JPetLayer/JPetLayer.cpp
@@ -99,5 +99,11 @@ void JPetLayer::clearTRefFrame()
 {
   fTRefFrame = NULL;
 }
-
+JPetLayer JPetLayer::makeLayer(int id, bool isActive, std::string name, float radius, JPetFrame& frame)
+{
+  JPetLayer objectLayer(id, isActive, name, radius);
+  objectLayer.fTRefFrame = &frame;
+  return objectLayer;
+}
+  
 ClassImp(JPetLayer);
