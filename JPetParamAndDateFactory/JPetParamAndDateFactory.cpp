63b0e5e99e1ef49763379abdd2f867468d7fba4e
kmuzalewska <klara.muzalewska@gmail.com>
2017-03-14 17:52:02 +0100

    reparing module

--- JPetParamAndDateFactory/JPetParamAndDateFactory.cpp
+++ JPetParamAndDateFactory/JPetParamAndDateFactory.cpp
@@ -20,65 +20,65 @@ namespace factory
 JPetScin makeScin(int id, float attenLen, float length, float height, float width, JPetBarrelSlot& p_barrelSlot )
 {
   JPetScin scinObject(id, attenLen, length, height, width);
-  scinObject.fTRefBarrelSlot = &p_barrelSlot;
+  scinObject.setBarrelSlot(p_barrelSlot);
   return scinObject;
 }
 
 JPetFEB makeFEB(int p_id, bool p_isActive, std::string p_status, std::string p_description, int p_version, int p_userId, int p_n_time_outputs_per_input, int p_n_notime_outputs_per_input, JPetTRB& p_TRB)
 {
   JPetFEB objectFEB(p_id, p_isActive, p_status, p_description, p_version, p_userId, p_n_time_outputs_per_input, p_n_notime_outputs_per_input);
-  objectFEB.fTRefTRBs = &p_TRB;
+  objectFEB.setTRB(p_TRB);
   return objectFEB;
 }
 
 JPetLayer makeLayer(int id, bool isActive, std::string name, float radius, JPetFrame& frame)
 {
   JPetLayer objectLayer(id, isActive, name, radius);
-  objectLayer.fTRefFrame = &frame;
+  objectLayer.setFrame(frame);
   return objectLayer;
 }
 
 JPetHit makeHit(float energy, float qualityOfEnergy, float time, float qualityOfTime, float td, float qtd, TVector3& pos , JPetBarrelSlot& bs, JPetScin& sc, JPetPhysSignal& p_sigA, JPetPhysSignal& p_sigB )
 {
   JPetHit hitObject(energy, qualityOfEnergy, time, qualityOfTime, pos, p_sigA, p_sigB, bs, sc); 
-  hitObject.fTimeDiff=td;
-  hitObject.fQualityOfTimeDiff=qtd; 
+  hitObject.setTimeDiff(td);
+  hitObject.setQualityOfTimeDiff(qtd); 
   return hitObject;
 }
 
 JPetSigCh makeSigCh(const JPetPM & pm, const JPetTRB & trb, const JPetFEB & feb, const JPetTOMBChannel & channel, float val, JPetSigCh::EdgeType type, float thr, Int_t daqch, unsigned int threshold_number){
    JPetSigCh sigChObject(type, val );
-   sigChObject.fPM = const_cast<JPetPM*>(&pm);
-   sigChObject.fTRB = const_cast<JPetTRB*>(&trb);
-   sigChObject.fFEB = const_cast<JPetFEB*>(&feb);
-   sigChObject.fTOMBChannel = const_cast<JPetTOMBChannel*>(&channel);
-   sigChObject.fThreshold = thr;
-   sigChObject.fDAQch = daqch;
-   sigChObject.fThresholdNumber = threshold_number;
+   sigChObject.setPM(pm);
+   sigChObject.setTRB(trb);
+   sigChObject.setFEB(feb);
+   sigChObject.setTOMBChannel(channel);
+   sigChObject.setThreshold(thr);
+   sigChObject.setDAQch(daqch);
+   sigChObject.setThresholdNumber(threshold_number);
    return sigChObject;
   }
 
 JPetBarrelSlot makeBarrelSlot(JPetLayer& p_layer, int id, bool isActive, std::string name, float theta, int inFrameID)
 {
   JPetBarrelSlot barrelSlotObject(id, isActive, name, theta, inFrameID);
-  barrelSlotObject.fTRefLayer = &p_layer;
+  barrelSlotObject.setLayer(p_layer);
   return barrelSlotObject;
 }
 
 JPetTimeWindow makeTimeWindow(JPetSigCh& new_ch, unsigned int index)
 {
   JPetTimeWindow timeWindowObject;
-  timeWindowObject.fSigChannels.push_back( JPetSigCh(new_ch) );
-  timeWindowObject.fIndex = index;
+  timeWindowObject.addCh(new_ch);
+  timeWindowObject.setIndex(index);
   return timeWindowObject;
 }
 
 JPetPM makePM(JPetPM::Side side, int id, int set, int opt, std::pair<float, float>& gain, JPetFEB& p_FEB, JPetScin& p_scin, JPetBarrelSlot& p_barrelSlot)
 {
   JPetPM pmObject(side, id, set, opt, gain);
-  pmObject.fTRefFEB = &p_FEB;
-  pmObject.fTRefScin = &p_scin;
-  pmObject.fTRefBarrelSlot = &p_barrelSlot;
+  pmObject.setFEB(p_FEB);
+  pmObject.setScin(p_scin);
+  pmObject.setBarrelSlot(p_barrelSlot);
   return pmObject;
 }
 
