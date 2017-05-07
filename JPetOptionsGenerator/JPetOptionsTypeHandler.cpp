55bf1da8cffa503bd139a5fd6f51015c71e09bf6
kmuzalewska <klara.muzalewska@gmail.com>
2017-05-07 19:34:11 +0200

    corrected JPetOptionsTypeHandler::anyMapToStringMap
    from swich loop to if loop

--- JPetOptionsGenerator/JPetOptionsTypeHandler.cpp
+++ JPetOptionsGenerator/JPetOptionsTypeHandler.cpp
@@ -19,33 +19,28 @@ JPetOptionsTypeHandler::JPetOptionsTypeHandler(const std::vector<std::string>& a
   fAllowedTypes(allowedTypes)
 { }
 
-///@todo change it to use fAllowedTypes and not typesToSwitch
 std::map<std::string, std::string> JPetOptionsTypeHandler::anyMapToStringMap(const std::map<std::string, boost::any>& optionsMap) const
 {
   using boost::any_cast;
   std::map<std::string, std::string> newOptionsMap;
-  std::map<std::string, optionTypes> typesToSwitch = {{"int", Int}, {"std::string", String}, {"bool", Bool}, {"std::vector<std::string>", VectorString}, {"std::vector<int>", VectorInt}, {"default", Default}};
   for (auto & option : optionsMap) {
-    int typeOfOption = typesToSwitch.at(getTypeOfOption(option.first));
-    /// @todo change switch to series of if and compare just to allowed types
-    /// e.g if(bla == "int") { }
-    /// else if(bla == "std::string") etc.
-    switch (typeOfOption) {
-    case Int:
-      newOptionsMap[getNameOfOption(option.first)] = std::to_string(any_cast<int>(optionsMap.at(option.first)));
-      break;
-    case String:
-      newOptionsMap[getNameOfOption(option.first)] = any_cast<std::string>(optionsMap.at(option.first));
-      break;
-    case Bool:
-      if (any_cast<bool>(optionsMap.at(option.first)))
-        newOptionsMap[getNameOfOption(option.first)] = "true";
-      else
-        newOptionsMap[getNameOfOption(option.first)] = "false";
-      break;
-    case Default:
-      newOptionsMap[option.first] = any_cast<std::string>(optionsMap.at(option.first));
-      break;
+    std::string typeOfOption = getTypeOfOption(option.first);
+    if(std::find(fAllowedTypes.begin(), fAllowedTypes.end(), typeOfOption) != fAllowedTypes.end()){
+      if(typeOfOption == "int"){
+        newOptionsMap[getNameOfOption(option.first)] = std::to_string(any_cast<int>(optionsMap.at(option.first)));
+      }
+      else if(typeOfOption == "std::string"){
+        newOptionsMap[getNameOfOption(option.first)] = any_cast<std::string>(optionsMap.at(option.first));
+      }
+      else if(typeOfOption == "bool"){
+        if (any_cast<bool>(optionsMap.at(option.first)))
+          newOptionsMap[getNameOfOption(option.first)] = "true";
+        else
+          newOptionsMap[getNameOfOption(option.first)] = "false";
+      }
+      else if(typeOfOption == "default"){
+        newOptionsMap[option.first] = any_cast<std::string>(optionsMap.at(option.first));
+      }
     }
   }
   return newOptionsMap;
