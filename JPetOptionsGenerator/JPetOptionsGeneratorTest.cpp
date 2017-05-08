d6382483f1455968118e8d661d6170d451bac6b1
kmuzalewska <klara.muzalewska@gmail.com>
2017-05-08 21:53:29 +0200

    added tests to JPetOptionsGenerator

--- JPetOptionsGenerator/JPetOptionsGeneratorTest.cpp
+++ JPetOptionsGenerator/JPetOptionsGeneratorTest.cpp
@@ -230,5 +230,125 @@ BOOST_AUTO_TEST_CASE(checkIfFunctionToTransformOptionsWork)
   BOOST_REQUIRE_EQUAL(any_cast<std::string>(mapAfterTransformation.at("inputFileType_std::string")), inputFileType);
 }
 
+BOOST_AUTO_TEST_CASE(checkIfFunctionGetConfigFileNameWork)
+{
+  JPetOptionsGenerator generator;
 
+  auto commandLine = "main.x -u example.json";
+  auto args_char = createArgs(commandLine);
+  auto argc = args_char.size();
+  auto argv = args_char.data();
+
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("userCfg_std::string,u", po::value<std::string>(), "Json file with optional user parameters.");
+  ;
+
+  po::variables_map variablesMap;
+  po::store(po::parse_command_line(argc, argv, description), variablesMap);
+  po::notify(variablesMap);
+
+  BOOST_REQUIRE_EQUAL(generator.getConfigFileName(variablesMap), "example.json");
+
+  auto commandLine2 = "main.x ";
+  auto args_char2 = createArgs(commandLine2);
+  auto argc2 = args_char2.size();
+  auto argv2 = args_char2.data();
+
+  po::options_description description2("Allowed options");
+  description2.add_options()
+    ("userCfg_std::string,u", po::value<std::string>(), "Json file with optional user parameters.");
+  ;
+
+  po::variables_map variablesMap2;
+  po::store(po::parse_command_line(argc2, argv2, description2), variablesMap2);
+  po::notify(variablesMap2);
+
+  BOOST_REQUIRE_EQUAL(generator.getConfigFileName(variablesMap2), "");
+}
+
+BOOST_AUTO_TEST_CASE(checkIfFumctionToAddOptionsFromCfgFileWork)
+{
+  JPetOptionsGenerator generator;
+
+  auto commandLine = "main.x -f unitTestData/JPetCmdParserTest/data.hld -t hld -r 2 -r 4 -p unitTestData/JPetCmdParserTest/data.hld -i 231 -b 1 -l unitTestData/JPetCmdParserTest/input.json -L output.json -u unitTestData/JPetOptionsToolsTest/inputTestCfg.json";
+  auto args_char = createArgs(commandLine);
+  auto argc = args_char.size();
+  auto argv = args_char.data();
+
+  po::options_description description("Allowed options");
+  description.add_options()
+  ("file_std::vector<std::string>,f", po::value<std::vector<std::string>>(), "File(s) to open")
+  ("type_std::string,t", po::value<std::string>(), "type of file: hld, zip, root or scope")
+  ("range_std::vector<int>,r", po::value<std::vector<int>>(), "Range of events to process.")
+  ("param_std::string,p", po::value<std::string>(), "File with TRB numbers.")
+  ("runId_int,i", po::value<int>(), "Run id.")
+  ("progressBar_bool,b", po::bool_switch()->default_value(false), "Progress bar.")
+  ("localDB_std::string,l", po::value<std::string>(), "The file to use as the parameter database.")
+  ("localDBCreate_std::string,L", po::value<std::string>(), "Where to save the parameter database.")
+  ("userCfg_std::string,u", po::value<std::string>(), "Json file with optional user parameters.")
+  ;
+
+  po::variables_map variablesMap;
+  po::store(po::parse_command_line(argc, argv, description), variablesMap);
+  po::notify(variablesMap);
+
+  auto options = generator.variablesMapToOption(variablesMap);
+  auto cfgFileName = generator.getConfigFileName(variablesMap);
+  if (!cfgFileName.empty()) {
+    generator.addNewOptionsFromCfgFile(cfgFileName, options);
+  }
+  BOOST_REQUIRE(options.count("myOption_std::string"));
+  BOOST_REQUIRE(options.count("myAnotherOption_std::string"));
+}
+
+// BOOST_AUTO_TEST_CASE(checkIfFumctionToAddMissingDefaultOptionsWork)
+// {
+//   JPetOptionsGenerator generator;
+
+//   auto commandLine = "main.x -f unitTestData/JPetCmdParserTest/data.hld -t hld -r 2 -r 4 -p unitTestData/JPetCmdParserTest/data.hld -i 231 -b 1 -l unitTestData/JPetCmdParserTest/input.json -L output.json";
+//   auto args_char = createArgs(commandLine);
+//   auto argc = args_char.size();
+//   auto argv = args_char.data();
+
+//   po::options_description description("Allowed options");
+//   description.add_options()
+//   ("file_std::vector<std::string>,f", po::value<std::vector<std::string>>(), "File(s) to open")
+//   ("type_std::string,t", po::value<std::string>(), "type of file: hld, zip, root or scope")
+//   ("range_std::vector<int>,r", po::value<std::vector<int>>(), "Range of events to process.")
+//   ("param_std::string,p", po::value<std::string>(), "File with TRB numbers.")
+//   ("runId_int,i", po::value<int>(), "Run id.")
+//   ("progressBar_bool,b", po::bool_switch()->default_value(false), "Progress bar.")
+//   ("localDB_std::string,l", po::value<std::string>(), "The file to use as the parameter database.")
+//   ("localDBCreate_std::string,L", po::value<std::string>(), "Where to save the parameter database.")
+//   ;
+
+//   po::variables_map variablesMap;
+//   po::store(po::parse_command_line(argc, argv, description), variablesMap);
+//   po::notify(variablesMap);
+
+//   auto options = generator.variablesMapToOption(variablesMap);
+//   generator.addMissingDefaultOptions(options);
+//   BOOST_REQUIRE_EQUAL(any_cast<std::string>(mapAfterTransformation.at("outputPath_std::string")), (pathForCorrection + '/'));
+//   BOOST_REQUIRE_EQUAL(any_cast<int>(mapAfterTransformation.at("lastEvent_int")), 2);
+// }
+BOOST_AUTO_TEST_CASE(checkIfGetOptionAndIsOptionWork)
+{
+  std::map<std::string, boost::any> options = {
+  {"firstEvent_int", -1},
+  {"lastEvent_int", -1},
+  {"progressBar_bool", false},
+  {"runId_int", -1},
+  {"unpackerConfigFile_std::string", std::string("conf_trb3.xml")},
+  {"unpackerCalibFile_std::string", std::string("")}
+};
+
+  JPetOptionsGenerator generator;
+  BOOST_REQUIRE(generator.isOptionSet(options, "firstEvent_int"));
+  BOOST_REQUIRE(generator.isOptionSet(options, "lastEvent_int"));
+  
+  BOOST_REQUIRE_EQUAL(any_cast<int>(generator.getOptionValue(options, "firstEvent_int")), -1);
+  BOOST_REQUIRE_EQUAL(any_cast<int>(generator.getOptionValue(options, "lastEvent_int")), -1);
+
+}
 BOOST_AUTO_TEST_SUITE_END()
