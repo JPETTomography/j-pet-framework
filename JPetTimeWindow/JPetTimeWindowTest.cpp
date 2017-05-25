887a5632e049fcbf5c6d36891e291434fa5b3ba0
Aleksander Gajos <alek.gajos@gmail.com>
2017-05-26 00:09:07 +0200

    Update TimeWindow UT after major change

--- JPetTimeWindow/JPetTimeWindowTest.cpp
+++ JPetTimeWindow/JPetTimeWindowTest.cpp
@@ -3,16 +3,8 @@
 #include <boost/test/unit_test.hpp>
 
 #include "../JPetSigCh/JPetSigCh.h"
+#include "../JPetHit/JPetHit.h"
 #include "../JPetTimeWindow/JPetTimeWindow.h"
-
-
-
-
-/// @todo update methods tests are outdated
-//#include <TError.h> /// gErrorIgnoreLevel
-//  gErrorIgnoreLevel = 7000;
-
-
 
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
@@ -20,31 +12,37 @@ BOOST_AUTO_TEST_SUITE(FirstSuite)
 BOOST_AUTO_TEST_CASE( default_constructor )
 {
   JPetTimeWindow test;
-  BOOST_REQUIRE(test.getNumberOfSigCh() == 0);
-  BOOST_REQUIRE(test.getSigChVect().size() == 0);
+  BOOST_REQUIRE(test.getNumberOfEvents() == 0);
 
 }
 
 BOOST_AUTO_TEST_CASE( some_channels )
 {
-  JPetTimeWindow test;
+  JPetTimeWindow test("JPetSigCh");
   JPetSigCh ch_test(JPetSigCh::Trailing, 1.2), ch_test2(JPetSigCh::Leading, 1.5), ch_test3(JPetSigCh::Leading, 98);
-  test.addCh(ch_test);
-  test.addCh(ch_test2);
-  test.addCh(ch_test3);
+  test.add<JPetSigCh>(ch_test);
+  test.add<JPetSigCh>(ch_test2);
+  test.add<JPetSigCh>(ch_test3);
 
-  BOOST_REQUIRE(test.getNumberOfSigCh() == 3);
-  BOOST_REQUIRE(test.getSigChVect().size() == 3);
+  BOOST_REQUIRE_EQUAL(test.getNumberOfEvents(), 3);
   double epsilon = 0.001;
-  BOOST_REQUIRE_CLOSE(test[0].getValue(), 1.2, epsilon);
-  BOOST_REQUIRE_CLOSE(test[1].getValue(), 1.5, epsilon);
-  BOOST_REQUIRE_CLOSE(test[2].getValue(), 98, epsilon);
+  BOOST_REQUIRE_CLOSE((dynamic_cast<const JPetSigCh&>(test[0])).getValue(), 1.2, epsilon);
+  BOOST_REQUIRE_CLOSE(test.getEvent<JPetSigCh>(1).getValue(), 1.5, epsilon);
+  BOOST_REQUIRE_CLOSE(test.getEvent<JPetSigCh>(2).getValue(), 98, epsilon);
 
-  const std::vector<JPetSigCh> array = test.getSigChVect();
-  BOOST_REQUIRE_CLOSE((array.at(0)).getValue(), 1.2, epsilon);
-  BOOST_REQUIRE_CLOSE((array.at(1)).getValue(), 1.5, epsilon);
-  BOOST_REQUIRE_CLOSE((array.at(2)).getValue(), 98, epsilon);
+}
 
+BOOST_AUTO_TEST_CASE( clearing )
+{
+  JPetTimeWindow test("JPetHit");
+  JPetHit hit1;
+  JPetHit hit2;
+  test.add<JPetHit>(hit1);
+  test.add<JPetHit>(hit2);
+  BOOST_REQUIRE_EQUAL(test.getNumberOfEvents(), 2);
+
+  test.Clear();
+  BOOST_REQUIRE_EQUAL(test.getNumberOfEvents(), 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
