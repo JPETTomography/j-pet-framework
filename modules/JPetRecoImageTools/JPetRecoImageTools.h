27855f5beefa54ea5ea342fe75cf46e8f64bd61b
Kamil Rakoczy <grey277@wp.pl>
2017-07-07 16:02:57 +0200

    Change FT implementation to FFTW library

--- modules/JPetRecoImageTools/JPetRecoImageTools.h
+++ modules/JPetRecoImageTools/JPetRecoImageTools.h
@@ -20,37 +20,49 @@
 #include <functional>
 #include <vector>
 
+#include <cassert>
+#include <cmath>
+#include <fftw3.h>
+#include <functional>
+#include <memory>
+#include <utility>
+#include <vector>
+
 #include <boost/function_types/function_type.hpp>
 #include <boost/function_types/parameter_types.hpp>
 #include <boost/function_types/result_type.hpp>
 
-class JPetRecoImageTools {
+class JPetRecoImageTools
+{
 public:
-  using Matrix2D = std::vector<std::vector<int>>;
-  using Matrix2DProj = std::vector<std::vector<double>>;
-  using InterpolationFunc =
-      std::function<double(int i, double y, std::function<double(int, int)> &)>;
-  using RescaleFunc = std::function<void(Matrix2DProj &v, double minCutoff,
-                                         double rescaleFactor)>;
+  using Matrix2D = std::vector< std::vector< int > >;
+  using Matrix2DProj = std::vector< std::vector< double > >;
+  using InterpolationFunc = std::function< double(
+      int i, double y, std::function< double(int, int) > &) >;
+  using RescaleFunc = std::function< void(Matrix2DProj &v, double minCutoff,
+                                          double rescaleFactor) >;
+  using FilterFunction = std::function< double(double) >;
 
-  /*template <typename Functor, typename... T>
-  static void FilterFunc(Functor f, Matrix2DProj &sinogram, T... args) {
-    namespace ft = boost::function_types;
-
-    typedef typename ft::result_type<Functor>::type result_type;
-    typedef ft::parameter_types<Functor> parameter_types;
-    typedef typename boost::mpl::push_front<parameter_types, result_type>::type
-        sequence_type;
-    // sequence_type is now a Boost.MPL sequence in the style of
-    // mpl::vector<int, double, long> if the signature of the
-    // analyzed functor were int(double, long)
-
-    // We now build a function type out of the MPL sequence
-    typedef typename ft::function_type<sequence_type>::type function_type;
-
-    std::function<function_type> function = std::move(f);
-    function(sinogram, std::forward(args)...);
-  }*/
+  static void FilterSinogram(FilterFunction callableFunction,
+                             Matrix2DProj &sinogram)
+  {
+    // namespace ft = boost::function_types;
+    //
+    // typedef typename ft::result_type< Functor >::type result_type;
+    // typedef ft::parameter_types< Functor > parameter_types;
+    // typedef
+    //    typename boost::mpl::push_front< parameter_types, result_type >::type
+    //        sequence_type;
+    //// sequence_type is now a Boost.MPL sequence in the style of
+    //// mpl::vector<int, double, long> if the signature of the
+    //// analyzed functor were int(double, long)
+    //
+    //// We now build a function type out of the MPL sequence
+    // typedef typename ft::function_type< sequence_type >::type function_type;
+    //
+    // std::function< function_type > callableFunction = std::move(f);
+    doFFTW(sinogram, callableFunction);
+  }
   /// Returns a matrixGetter, that can be used to return matrix elements in the
   /// following way:
   /// if isTransposed is set to false, matrixGetter returns matrix[i][j]
@@ -58,7 +70,7 @@ public:
   /// In addition if the indices goes outside of the matrix range 0 is retuned.
   /// It is assumed that the input matrix is quadratic.
   /// The produced functions can be used as an input to interpolation functions.
-  static std::function<double(int, int)>
+  static std::function< double(int, int) >
   matrixGetterFactory(const Matrix2D &emissionMatrix,
                       bool isTransposed = false);
 
@@ -70,7 +82,7 @@ public:
    *  \param func function that returns double value based on two discrete i,j.
   */
   static double nearestNeighbour(int i, double y,
-                                 std::function<double(int, int)> &func);
+                                 std::function< double(int, int) > &func);
   /*! \brief Linear interpolation function returning (1-t)*func(i,j) + t*
    * func(i,j+1).
    *  \param i discrete index being the first parameter of the function func.
@@ -78,7 +90,8 @@ public:
    * calculated.
    *  \param func function that returns double value based on two discrete i,j.
   */
-  static double linear(int i, double y, std::function<double(int, int)> &func);
+  static double linear(int i, double y,
+                       std::function< double(int, int) > &func);
 
   /// Rescale the Matrix in the following way:
   /// 1. All the values less than minCutoff are set to minCutoff
@@ -102,41 +115,81 @@ public:
    *  \param rescaleFunc function that rescales the final result (Optional,
    * default no rescaling)
   */
-  static Matrix2DProj sinogram(Matrix2D &emissionMatrix, int nViews, int nScans,
-                               double angleBeg = 0, double angleEnd = 180,
-                               InterpolationFunc interpolationFunction = linear,
-                               RescaleFunc rescaleFunc = nonRescale,
-                               int rescaleMinCutoff = 0,
-                               int rescaleFactor = 255);
+  static Matrix2DProj createSinogramWithSingleInterpolation(
+      Matrix2D &emissionMatrix, int nViews, int nScans, double angleBeg = 0,
+      double angleEnd = 180, InterpolationFunc interpolationFunction = linear,
+      RescaleFunc rescaleFunc = nonRescale, int rescaleMinCutoff = 0,
+      int rescaleFactor = 255);
+
   static double calculateProjection(const Matrix2D &emissionMatrix, double phi,
                                     int scanNumber, int nScans,
                                     InterpolationFunc &interpolationFunction);
-  static Matrix2DProj sinogram2(Matrix2D &emissionMatrix, int nAngles,
-                                RescaleFunc rescaleFunc = nonRescale,
-                                int rescaleMinCutoff = 0,
-                                int rescaleFactor = 255);
-  static double calculateProjection2(int step, double cos, double sin,
-                                     int imageSize, double center,
-                                     double length,
-                                     std::function<double(int, int)> matrixGet);
 
+  /*! \brief Function returning sinogram matrix with both variables interpolated
+   *  \param emissionMatrix matrix, needs to be NxN
+   *  \param nAngles angle step is calculated as PI / nAngles
+   *  \param rescaleFunc function that rescales the final result (Optional,
+   * default no rescaling)
+   *  \param rescaleMinCutoff min value to set in rescale (Optional)
+   *  \param rescaleFactor max value to set in rescale (Optional)
+  */
+  static Matrix2DProj
+  createSinogramWithDoubleInterpolation(Matrix2D &emissionMatrix, int nAngles,
+                                        RescaleFunc rescaleFunc = nonRescale,
+                                        int rescaleMinCutoff = 0,
+                                        int rescaleFactor = 255);
+
+  static double
+  calculateProjection2(int step, double cos, double sin, int imageSize,
+                       double center, double length,
+                       std::function< double(int, int) > matrixGet);
+
+  /*! \brief Function image from sinogram matrix
+   *  \param sinogram matrix containing sinogram to backProject
+   *  \param nAngles angle step is calculated as PI / nAngles
+   *  \param rescaleFunc function that rescales the final result (Optional,
+   * default no rescaling)
+   *  \param rescaleMinCutoff min value to set in rescale (Optional)
+   *  \param rescaleFactor max value to set in rescale (Optional)
+  */
   static Matrix2DProj backProject(Matrix2DProj &sinogram, int angles,
                                   RescaleFunc rescaleFunc, int rescaleMinCutoff,
                                   int rescaleFactor);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+  * used directly
+  *  Dummy filter, not filtering data at all.
+  */
+  static double NoneFilter(double radius);
 
-  //static void doFFTW(Matrix2DProj &sinogram, std::vector<double> &filter);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+   * used directly
+   * Ramp filter: F(x) = |x|
+  */
+  static double RamLakFilter(double radius);
 
-  static void None(Matrix2DProj &sinogram);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+   * used directly
+   * F(x) = sin(x * pi) / pi
+  */
+  static double SheppLoganFilter(double radius);
 
-  static void RamLak(Matrix2DProj &sinogram);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+   * used directly
+   * F(x) = x * cos(x * pi)
+  */
+  static double CosineFilter(double radius);
 
-  static void SheppLogan(Matrix2DProj &sinogram);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+   * used directly
+   * Not usable for now
+  */
+  static double HammingFilter(double radius, double alpha);
 
-  static void Cosine(Matrix2DProj &sinogram);
-
-  static void Hamming(Matrix2DProj &sinogram);
-
-  static void Ridgelet(Matrix2DProj &sinogram);
+  /*! \brief Filter used in FT by function FilterSinogram, should be passed, not
+   * used directly
+   * F(x) = sqrt(x)
+  */
+  static double RidgeletFilter(double radius);
 
 private:
   JPetRecoImageTools();
@@ -144,16 +197,10 @@ private:
   JPetRecoImageTools(const JPetRecoImageTools &) = delete;
   JPetRecoImageTools &operator=(const JPetRecoImageTools &) = delete;
 
-  static void doFFT1D(Matrix2DProj &sinogram, int nAngles, int nScanSize,
-                      int padlen, std::vector<double> &Filt);
+  static void doFFTW(Matrix2DProj &sinogram, FilterFunction &filter);
 
-  static void doFFT1D(std::vector<double> &Re, std::vector<double> &Im,
-                      int size, int shift, bool fast);
-
-  static void doIFFT1D(std::vector<double> &Re, std::vector<double> &Im,
-                       int size, int shift, bool fast);
-
-  static inline double setToZeroIfSmall(double value, double epsilon) {
+  static inline double setToZeroIfSmall(double value, double epsilon)
+  {
     if (std::abs(value) < epsilon)
       return 0;
     else
