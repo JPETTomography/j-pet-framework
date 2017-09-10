55f91598bff7bf95fb89f39063eb81439a0fe07b
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-09-10 23:52:38 +0200

    Add class JPetParams which should replace JPetOptions
    JPetParams contains inside ParamMangager instance and options.
    Also, general JPetParamsInterface was added.

---
+++ JPetParams/JPetParamsTest.cpp
@@ -0,0 +1,50 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE JPetParamsTest
+#include <boost/test/unit_test.hpp>
+#include <boost/any.hpp>
+
+#include "../JPetParams/JPetParams.h"
+
+
+BOOST_AUTO_TEST_SUITE(FirstSuite)
+
+BOOST_AUTO_TEST_CASE(defaultConstr)
+{
+  JPetParams params;
+
+  BOOST_REQUIRE(params.getParamManager()== nullptr);
+  BOOST_REQUIRE(params.getOptions().empty());
+}
+
+BOOST_AUTO_TEST_CASE(nonDefaultConstr)
+{
+  jpet_options_tools::OptionsStrAny opts;
+  opts["blaOption"] ="value";
+  JPetParamManager* mgr = new JPetParamManager;
+  JPetParams params(opts, mgr);
+  BOOST_REQUIRE_EQUAL(params.getParamManager().get(), mgr);
+  BOOST_REQUIRE_EQUAL(params.getOptions().size(), 1u);
+}
+
+BOOST_AUTO_TEST_CASE(copyCtr)
+{
+  jpet_options_tools::OptionsStrAny opts;
+  opts["blaOption"] ="value";
+  JPetParamManager* mgr = new JPetParamManager;
+  JPetParams params(opts, mgr);
+  auto params2 = params;
+  BOOST_REQUIRE_EQUAL(params2.getParamManager().get(), mgr);
+  BOOST_REQUIRE_EQUAL(params2.getOptions().size(), params.getOptions().size());
+}
+
+BOOST_AUTO_TEST_CASE(memoryLeaks)
+{
+  jpet_options_tools::OptionsStrAny opts;
+  opts["blaOption"] ="value";
+  JPetParams* params= new JPetParams(opts, new JPetParamManager);
+  auto mgr = params->getParamManager();
+  delete params;
+  BOOST_REQUIRE(mgr != nullptr);
+   
+}
+BOOST_AUTO_TEST_SUITE_END()
