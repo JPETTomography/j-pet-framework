b54b35502548f694591129411d2c5b8fb1bd4b88
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-09-13 00:13:53 +0200

    Change JPetTaskInterface to be used as based interface for all the tasks
    Also, modify JPetManager to use JPetManager. Also, make the threads
    enabled and introduce a flag to make it enable. THIS CAN  BE BROKEN BEFORE
    ADDING A NEW VERSION OF JPETTASKCHAINEXECUTOR

--- JPetManager/JPetManagerTest.cpp
+++ JPetManager/JPetManagerTest.cpp
@@ -1,21 +1,8 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE JPetManagerTest
 #include <boost/test/unit_test.hpp>
-#include <boost/any.hpp>
-using boost::any_cast;
 
 #include "../JPetManager/JPetManager.h"
-#include "../JPetOptionValidator/JPetAdditionalValidators.cpp"
-#include "../JPetOptionsGenerator/JPetAdditionalTransformations.cpp"
-#include "../JPetCommonTools/JPetCommonTools.h"
-//JPetManger
-// public methods:
-// static JPetManager& getManager();
-// ~JPetManager();
-// void run();
-// void registerTask(const TaskGenerator& taskGen);
-// void parseCmdLine(int argc, char** argv);
-// inline std::vector<JPetOptions> getOptions() const { return fOptions;}
 
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
@@ -30,51 +17,29 @@ BOOST_AUTO_TEST_CASE( create_unique_manager )
   BOOST_REQUIRE_EQUAL(pManager, pManager2);
 }
 
+BOOST_AUTO_TEST_CASE( threadsEnabled )
+{
+  JPetManager& manager = JPetManager::getManager();
+  BOOST_REQUIRE(!manager.areThreadsEnabled());
+  manager.setThreadsEnabled(true);
+  BOOST_REQUIRE(manager.areThreadsEnabled());
+  manager.setThreadsEnabled(false);
+  BOOST_REQUIRE(!manager.areThreadsEnabled());
+}
+
 BOOST_AUTO_TEST_CASE( manager_getOptions )
 {
   JPetManager& manager = JPetManager::getManager();
   auto options = manager.getOptions();
-  BOOST_REQUIRE_EQUAL(options.size(), 0);
+  BOOST_REQUIRE_EQUAL(options.size(), 0u);
 }
 
 BOOST_AUTO_TEST_CASE( emptyRun )
 {
   JPetManager& manager = JPetManager::getManager();
-  BOOST_REQUIRE(manager.run());
+  BOOST_REQUIRE(!manager.run(0, nullptr));
   auto options = manager.getOptions();
-  BOOST_REQUIRE_EQUAL(options.size(), 0);
-}
-
-BOOST_AUTO_TEST_CASE( additionalValidator )
-{
-
-  auto commandLine = "main.x -t hld -f unitTestData/JPetCmdParserTest/testfile.hld -i 10";
-  auto args_char = JPetCommonTools::createArgs(commandLine);
-  auto argc = args_char.size();
-  auto argv = args_char.data();
-
-
-  JPetManager& manager = JPetManager::getManager();
-  manager.addValidationFunctionForUserOptions("runId_int", additionalCheckIfRunIdIsOk);
-  manager.parseCmdLine(argc, const_cast<const char**>(argv));
-  auto options = manager.getOptions();
-}
-
-
-BOOST_AUTO_TEST_CASE( additionalTransformator )
-{
-
-  auto commandLine = "main.x -t hld -f unitTestData/JPetCmdParserTest/testfile.hld -i 10";
-  auto args_char = JPetCommonTools::createArgs(commandLine);
-  auto argc = args_char.size();
-  auto argv = args_char.data();
-
-
-  JPetManager& manager = JPetManager::getManager();
-  manager.addTransformationFunctionForUserOption("runId_int", setAdditionalRunIdInTheMap);
-  manager.parseCmdLine(argc, const_cast<const char**>(argv));
-  auto options = manager.getOptions();
-  BOOST_REQUIRE_EQUAL(any_cast<int>(options.front().getOptions().at("additionalRunID_int")), 10);
+  BOOST_REQUIRE_EQUAL(options.size(), 0u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
