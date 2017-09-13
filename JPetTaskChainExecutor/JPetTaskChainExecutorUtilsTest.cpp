85db536acde0218a9697d75213d51d457443f69b
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-09-13 15:42:42 +0200

    Refactoring of JPetTaskChainExecutorUtils
    Add more error checks and one test of process() method.

--- JPetTaskChainExecutor/JPetTaskChainExecutorUtilsTest.cpp
+++ JPetTaskChainExecutor/JPetTaskChainExecutorUtilsTest.cpp
@@ -83,4 +83,12 @@ BOOST_AUTO_TEST_CASE(generateParams2)
   BOOST_REQUIRE_EQUAL(first.getParamManager(), second.getParamManager());
 }
 
+BOOST_AUTO_TEST_CASE(process)
+{
+  JPetParams params;
+  std::list<JPetTaskInterface*> tasks;
+  BOOST_REQUIRE(!JPetTaskChainExecutorUtils::process(params, tasks));
+  BOOST_REQUIRE(tasks.empty());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
