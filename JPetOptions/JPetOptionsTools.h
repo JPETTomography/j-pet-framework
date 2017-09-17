e8fae8b1ce8a3a71e49f599d6265991843110b53
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-09-17 21:42:31 +0200

    Refactoring JPetOptionsGenerator and JPetOptionsTools
    Also, add bunch of tests.

--- JPetOptions/JPetOptionsTools.h
+++ JPetOptions/JPetOptionsTools.h
@@ -18,10 +18,72 @@
 #include <map>
 #include <boost/any.hpp>
 
+/**
+ * @brief Set of helper methods to operate on options provided by users.
+ *
+ * Options are represented as:
+ *  1)std::map<std::string, boost::any> a.k.a. OptionsStrAny
+ *  2)std::map<std::string, std::string> a.k.a. OptionsStrStr
+ *
+ */
 namespace jpet_options_tools
 {
-typedef std::map<std::string, std::string> Options;
-bool createConfigFileFromOptions(const Options& options, const std::string& outFile);
-std::map<std::string, boost::any> createOptionsFromConfigFile(const std::string& inFile);
+
+typedef std::map<std::string, std::string> OptionsStrStr;
+typedef std::map<std::string, boost::any> OptionsStrAny;
+
+bool isOptionSet(const OptionsStrAny& opts, const std::string& optionName);
+boost::any getOptionValue(const OptionsStrAny& opts, std::string optionName);
+
+class FileTypeChecker
+{
+public:
+  FileTypeChecker();
+  enum FileType {
+    kNoType, kScope, kRaw, kRoot, kHld, kZip, kPhysEve, kPhysHit, kPhysSig, kRawSig, kRecoSig, kTslotCal, kTslotRaw, kUndefinedFileType
+  };
+  static FileType getInputFileType(const OptionsStrAny& opts);
+  static FileType getOutputFileType(const OptionsStrAny& opts);
+
+private:
+  static void handleErrorMessage(const std::string& errorMessage, const std::out_of_range& outOfRangeException);
+  static FileType getFileType(const OptionsStrAny& opts, const std::string& fileType);
+
+  static std::map<std::string, FileType> fStringToFileType;
+};
+
+///Helper functions to extract option values with correct types
+
+/// This function returns a valid result only if the inputFile option is given
+/// in the untransformed form std::vector<std::string>
+std::vector<std::string> getInputFiles(const OptionsStrAny& opts);
+
+/// This function returns a valid result only if the inputFile option is given
+/// in the transformed form std::string
+const char* getInputFile(const OptionsStrAny& opts);
+
+const char* getScopeConfigFile(const OptionsStrAny& opts);
+const char* getScopeInputDirectory(const OptionsStrAny& opts);
+const char* getOutputFile(const OptionsStrAny& opts);
+const char* getOutputPath(const OptionsStrAny& opts);
+long long getFirstEvent(const OptionsStrAny& opts);
+long long getLastEvent(const OptionsStrAny& opts);
+long long getTotalEvents(const OptionsStrAny& opts);
+int getRunNumber(const OptionsStrAny& opts);
+bool isProgressBar(const OptionsStrAny& opts);
+bool isLocalDB(const OptionsStrAny& opts);
+std::string getLocalDB(const OptionsStrAny& opts);
+bool isLocalDBCreate(const OptionsStrAny& opts);
+std::string getLocalDBCreate(const OptionsStrAny& opts);
+const char* getUnpackerConfigFile(const OptionsStrAny& opts);
+const char* getUnpackerCalibFile(const OptionsStrAny& opts);
+
+OptionsStrAny resetEventRange(const OptionsStrAny& srcOpts);
+
+bool createConfigFileFromOptions(const OptionsStrStr& options, const std::string& outFile = "");
+OptionsStrAny createOptionsFromConfigFile(const std::string& inFile);
+
+void printOptionsToLog(const OptionsStrAny& opts, const std::string& firstLine);
+
 }
 #endif /*  !JPETOPTIONSTOOLS_H */
--- JPetOptionsGenerator/JPetOptionsGenerator.cpp
+++ JPetOptionsGenerator/JPetOptionsGenerator.cpp
@@ -17,6 +17,7 @@
 #include <stdexcept>
 #include <vector>
 #include <string>
+#include <cassert>
 
 #include "JPetOptionsGenerator.h"
 #include "../JPetCommonTools/JPetCommonTools.h"
@@ -29,6 +30,7 @@
 
 using boost::any_cast;
 using namespace std;
+using namespace jpet_options_tools;
 
 std::map<std::string, boost::any> JPetOptionsGenerator::kDefaultOptions = {
   {"inputFile_std::string", std::string("")},
@@ -36,8 +38,8 @@ std::map<std::string, boost::any> JPetOptionsGenerator::kDefaultOptions = {
   {"scopeConfigFile_std::string", std::string("")},
   {"scopeInputDirectory_std::string", std::string("")},
   {"outputPath_std::string", std::string("")},
-  {"outputFile_std::string", std::string("root")},
-  {"outputFileType_std::string", std::string("test.root")},
+  {"outputFile_std::string", std::string("test.root")},
+  {"outputFileType_std::string", std::string("root")},
   {"firstEvent_int", -1},
   {"lastEvent_int", -1},
   {"progressBar_bool", false},
@@ -46,11 +48,116 @@ std::map<std::string, boost::any> JPetOptionsGenerator::kDefaultOptions = {
   {"unpackerCalibFile_std::string", std::string("")}
 };
 
+std::map<std::string, std::string> JPetOptionsGenerator::kOptCmdLineNameToExtendedName = {
+  {"type", "type_std::string"},
+  {"file", "file_std::vector<std::string>"},
+  {"outputPath", "outputPath_std::string"},
+  {"range", "range_std::vector<int>"},
+  {"unpackerConfigFile", "unpackerConfigFile_std::string"},
+  {"unpackerCalibFile", "unpackerCalibFile_std::string"},
+  {"runId", "runId_int"},
+  {"progressBar", "progressBar_bool"},
+  {"localDB", "localDB_std::string"},
+  {"localDBCreate", "localDBCreate_std::string"},
+  {"userCfg", "userCfg_std::string"}
+};
 
-JPetOptionsGenerator::JPetOptionsGenerator(): fValidator()
+JPetOptionsGenerator::JPetOptionsGenerator()
 {
   fTransformationMap = generateTransformationMap();
 }
+
+/// Function returns a map of Options
+/// based on the number of registered tasks and provided command line arguments.
+/// The elements of the map are pairs filename -> OptionsForAllTasks
+/// , where OptionsForAllTasks is a container with elements correspondig to the
+/// options assigned for given task.
+/// e.g. if there are 4 files and for each file there are 3 tasks, then the map
+/// should have 4 containers with 3 option elements each.
+/// In this version it is assumed that for every file the same number of tasks are
+/// provided defined by nbOfRegisteredTasks arguments.
+JPetOptionsGenerator::OptsForFiles JPetOptionsGenerator::generateOptions(const po::variables_map& cmdLineArgs, int nbOfRegisteredTasks)
+{
+  using namespace jpet_options_tools;
+  OptsForFiles  optsForAllFiles;
+
+  if (cmdLineArgs.empty()) {
+    ERROR("cmdLineArgs is empty");
+    return optsForAllFiles;
+  }
+  if (nbOfRegisteredTasks <= 0) {
+    ERROR("nbOfRegisteredTasks <= 0");
+    return optsForAllFiles;
+  }
+
+  ///Generate general set of options
+  auto options = JPetOptionsGenerator::generateAndValidateOptions(cmdLineArgs);
+  auto files = getInputFiles(options);
+
+  ///Now generate set of options for every file
+  std::map<std::string, OptionsStrAny> optionsPerFile;
+
+  /// In case of scope there is one special input file
+  /// which is a json config file which must be parsed.
+  /// Based on its content the set of input directories are generated.
+  /// The input directories contain data files.
+  /// The config input file name also should be stored in a special option field.
+  if (any_cast<std::string>(getOptionValue(options, "type_std::string")) == "scope") {
+    assert(files.size() == 1); /// there should be only file which is config.
+    auto configFileName = files.front();
+    options["scopeConfigFile_std::string"] =  configFileName;
+    JPetScopeConfigParser scopeConfigParser;
+    /// The scope module must use a fake input file name which will be used to
+    /// produce the correct output file names by the following modules.
+    /// At the same time, the input directory with true input files must be
+    /// also added. The container of pairs <directory, fileName> is generated
+    /// based on the content of the configuration file.
+    JPetScopeConfigParser::DirFileContainer dirsAndFiles = scopeConfigParser.getInputDirectoriesAndFakeInputFiles(configFileName);
+    for (const auto& dirAndFile : dirsAndFiles) {
+      options["scopeInputDirectory_std::string"] = dirAndFile.first;
+      options["inputFile_std::string"] = dirAndFile.second;
+      optionsPerFile[dirAndFile.second] = options;
+    }
+  } else {
+    for (const auto& file : files) {
+      options["inputFile_std::string"] = file;
+      optionsPerFile[file] = options;
+    }
+  }
+  assert(nbOfRegisteredTasks > 0);
+
+  /// Finally, multiple the options for every task.
+  for (const auto& el : optionsPerFile) {
+    std::vector<OptionsStrAny>  currOpts(nbOfRegisteredTasks, el.second);
+    optsForAllFiles[el.first] = JPetOptionsGenerator::setCorrectRangeAndOutputForNonFirstOption(currOpts);
+  }
+  return optsForAllFiles;
+}
+
+JPetOptionsGenerator::OptsStrAny JPetOptionsGenerator::generateAndValidateOptions(const po::variables_map& cmdLineArgs)
+{
+  JPetOptionValidator validator;
+  OptsStrAny options;
+  if (cmdLineArgs.empty()) {
+    ERROR("cmdLineArgs are empty");
+    return options;
+  }
+  options = addTypeSuffixes(transformToStrAnyMap(cmdLineArgs));
+  auto cfgFileName = getConfigFileName(options);
+  if (!cfgFileName.empty()) {
+    addNewOptionsFromCfgFile(cfgFileName, options);
+  }
+  createMapOfBoolOptionFromUser(options);
+
+  addMissingDefaultOptions(options);
+  options = transformOptions(options);
+
+  if (!validator.areCorrectOptions(options, fVectorOfOptionFromUser)) {
+    throw std::invalid_argument("Wrong user options provided! Check the log!");
+  }
+  return options;
+}
+
 
 bool JPetOptionsGenerator::isOptionSet(const std::map<std::string, boost::any>& optionsMap, const std::string& option) const
 {
@@ -96,26 +203,30 @@ std::pair <std::string, boost::any>JPetOptionsGenerator::setInputFileType(boost:
   return std::make_pair("inputFileType_std::string", inputFileType);
 }
 
-std::map<std::string, boost::any> JPetOptionsGenerator::variablesMapToOption(const po::variables_map& variablesMap)
-{
 
-  std::map<std::string, boost::any> optionsMap;
-  for (auto& option : variablesMap) {
-    optionsMap[option.first] = option.second.value();
+std::map<std::string, boost::any> JPetOptionsGenerator::transformToStrAnyMap(const po::variables_map& inMap)
+{
+  std::map<std::string, boost::any> outMap;
+  for (auto& option : inMap) {
+    outMap[option.first] = option.second.value();
   }
-/// @todo bad code ;) should be clean up later
-  std::map<std::string, std::string> optCmdLineNameToExtendedName = {{"type", "type_std::string"}, {"file", "file_std::vector<std::string>"}, {"outputPath", "outputPath_std::string"}, {"range", "range_std::vector<int>"}, {"unpackerConfigFile", "unpackerConfigFile_std::string"}, {"unpackerCalibFile", "unpackerCalibFile_std::string"}, {"runId", "runId_int"}, {"progressBar", "progressBar_bool"}, {"localDB", "localDB_std::string"}, {"localDBCreate", "localDBCreate_std::string"}, {"userCfg", "userCfg_std::string"}};
-  for (auto& elem : optCmdLineNameToExtendedName) {
+  return outMap;
+}
+
+JPetOptionsGenerator::OptsStrAny JPetOptionsGenerator::addTypeSuffixes(const std::map<std::string, boost::any>& oldMap)
+{
+  std::map<std::string, boost::any> newMap(oldMap);
+
+  for (auto& elem : kOptCmdLineNameToExtendedName) {
     auto oldKey = elem.first;
     auto newKey = elem.second;
-    const auto it = optionsMap.find(oldKey);
-    if (it != optionsMap.end()) {
-      optionsMap[newKey] = it->second;
-      optionsMap.erase(it);
+    const auto it = newMap.find(oldKey);
+    if (it != newMap.end()) {
+      newMap[newKey] = it->second;
+      newMap.erase(it);
     }
   }
-/// end of the bad code
-  return optionsMap;
+  return newMap;
 }
 
 std::map<std::string, std::vector<JPetOptionsGenerator::Transformer> > JPetOptionsGenerator::generateTransformationMap() const
@@ -177,68 +288,9 @@ void JPetOptionsGenerator::addMissingDefaultOptions(std::map<std::string, boost:
   options.insert(defaultOptions.begin(), defaultOptions.end());
 }
 
-JPetOptionValidator& JPetOptionsGenerator::getValidator()
+std::vector<OptionsStrAny> JPetOptionsGenerator::setCorrectRangeAndOutputForNonFirstOption(const std::vector<OptionsStrAny>& oldOptions)
 {
-  return fValidator;
-}
-
-/// It throws exceptions
-std::vector<JPetOptions> JPetOptionsGenerator::generateOptions(const po::variables_map& cmdLineArgs)
-{
-  auto options = variablesMapToOption(cmdLineArgs);
-  auto cfgFileName = getConfigFileName(options);
-  if (!cfgFileName.empty()) {
-    addNewOptionsFromCfgFile(cfgFileName, options);
-  }
-  createMapOfBoolOptionFromUser(options);
-
-  addMissingDefaultOptions(options);
-  options = transformOptions(options);
-
-  if (!fValidator.areCorrectOptions(options, fVectorOfOptionFromUser)) {
-    throw std::invalid_argument("Wrong user options provided! Check the log!");
-  }
-
-  auto files = any_cast<std::vector<std::string>>(getOptionValue(options, "file_std::vector<std::string>"));
-  std::vector<JPetOptions>  optionContainer;
-
-  /// @todo change it to be properly initialized
-// JPetOptionsTypeHandler optTypeHandler({"int", "std::string", "bool"});
-
-  /// In case of scope there is one special input file
-  /// which is a json config file which must be parsed.
-  /// Based on its content the set of input directories are generated.
-  /// The input directories contain data files.
-  /// The config input file name also should be stored in a special option field.
-  if (any_cast<std::string>(getOptionValue(options, "type_std::string")) == "scope") {
-    assert(files.size() == 1); /// there should be only file which is config.
-    auto configFileName = files.front();
-    options["scopeConfigFile_std::string"] =  configFileName;
-    JPetScopeConfigParser scopeConfigParser;
-    /// The scope module must use a fake input file name which will be used to
-    /// produce the correct output file names by the following modules.
-    /// At the same time, the input directory with true input files must be
-    /// also added. The container of pairs <directory, fileName> is generated
-    /// based on the content of the configuration file.
-    JPetScopeConfigParser::DirFileContainer dirsAndFiles = scopeConfigParser.getInputDirectoriesAndFakeInputFiles(configFileName);
-    for (const auto& dirAndFile : dirsAndFiles) {
-      options["scopeInputDirectory_std::string"] = dirAndFile.first;
-      options["inputFile_std::string"] = dirAndFile.second;
-      optionContainer.push_back(JPetOptions(options));
-    }
-  } else {
-    for (const auto& file : files) {
-      options["inputFile_std::string"] = file;
-      optionContainer.push_back(JPetOptions(options));
-    }
-  }
-  return optionContainer;
-}
-
-/// @todo add unit test
-std::vector<JPetOptions> JPetOptionsGenerator::setCorrectRangeAndOutputForNonFirstOption(const std::vector<JPetOptions>& oldOptions)
-{
-  std::vector<JPetOptions> newOptions;
+  std::vector<OptionsStrAny> newOptions;
   newOptions.reserve(oldOptions.size());
   auto it = oldOptions.begin();
   /// We don't change the first element
@@ -248,18 +300,23 @@ std::vector<JPetOptions> JPetOptionsGenerator::setCorrectRangeAndOutputForNonFir
   }
   /// we start with the second element, or it is already end.
   for (; it != oldOptions.end(); ++it) {
-    auto currOpts = it->getOptions();
+    auto currOpts = *it;
     /// Ignore the event range options for all but the first task.
-    currOpts = JPetOptions::resetEventRange(currOpts);
+    currOpts = resetEventRange(currOpts);
     /// For all but the first task,
-    /// the input path must be changed if
+    /// the input file must be changed if
     /// the output path argument -o was given, because the input
     /// data for them will lay in the location defined by -o.
-    auto outPath  = any_cast<std::string>(currOpts.at("outputPath_std::string"));
+    std::string outPath  = getOutputPath(currOpts);
     if (!outPath.empty()) {
-      currOpts.at("inputFile_std::string") = outPath + JPetCommonTools::extractPathFromFile(any_cast<std::string>(currOpts.at("inputFile_std::string"))) + JPetCommonTools::extractFileNameFromFullPath(any_cast<std::string>(currOpts.at("inputFile_std::string")));
+      currOpts.at("inputFile_std::string") = outPath + JPetCommonTools::extractFileNameFromFullPath(getInputFile(currOpts));
     }
-    newOptions.push_back(JPetOptions(currOpts));
+    newOptions.push_back(currOpts);
   }
   return newOptions;
+}
+
+std::map<std::string, boost::any> JPetOptionsGenerator::getDefaultOptions()
+{
+  return kDefaultOptions;
 }
