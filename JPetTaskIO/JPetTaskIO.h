009f5681b43919cf00c997dd9dfe86c0e43166c6
Aleksander Gajos <alek.gajos@gmail.com>
2017-09-26 14:42:31 +0200

    Modify the scheme of registering tasks

--- JPetTaskIO/JPetTaskIO.h
+++ JPetTaskIO/JPetTaskIO.h
@@ -39,7 +39,7 @@ class JPetStatistics;
 class JPetTaskIO: public JPetTask
 {
 public:
-  JPetTaskIO(const char* name = "");
+  JPetTaskIO(const char* name = "", const char* in_file_type="", const char* out_file_type="");
   virtual bool init(const JPetParamsInterface& inOptions) override;
   virtual bool run(const JPetDataInterface& inData) override;
   virtual bool terminate(JPetParamsInterface& outOptions) override;
@@ -62,6 +62,27 @@ protected:
   const JPetParamBank& getParamBank();
   JPetParamManager& getParamManager();
 
+  std::string generateProperNameFile(const std::string& srcFilename, const std::string& fileType) const;
+
+  /**
+   * @brief Strips the framework file "extension" from full file path
+   *
+   * The "extension" can be composed of multiple parts, dot-separated.
+   * The extension carries information not only about file type but also
+   * about types of the objects within the ROOT file.
+   * Everything following the first dot in the file name (but not in the file
+   * path) is treated as an extension.
+   *
+   * Example:
+   * ./data/somefile.phys.sig.root
+   * the extension is "phys.sig.root"
+   *
+   */
+  std::string getBaseFilePath(const std::string& srcName) const;
+
+  std::string fInFileType;
+  std::string fOutFileType;
+  
   int fEventNb = -1;
   JPetParams fParams;
   JPetWriter* fWriter = 0;
