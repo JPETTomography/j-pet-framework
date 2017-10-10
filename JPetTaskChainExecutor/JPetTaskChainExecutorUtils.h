e90a39ae7e91de93c7947c238de39368429b3102
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-10-10 18:17:16 +0200

    Remove unused functions and code from JPetTaskChainExecutorUtils

--- JPetTaskChainExecutor/JPetTaskChainExecutorUtils.h
+++ JPetTaskChainExecutor/JPetTaskChainExecutorUtils.h
@@ -16,28 +16,20 @@
 #ifndef JPETTASKCHAINEXECUTORUTILS_H
 #define JPETTASKCHAINEXECUTORUTILS_H
 
-#include "../JPetUnpacker/JPetUnpacker.h"
 #include "../JPetParamManager/JPetParamManager.h"
-#include "../JPetScopeLoader/JPetScopeLoader.h"
 #include "../JPetParams/JPetParams.h"
-#include <boost/concept_check.hpp>
 
 /**
- * JPetTaskChainExecutorUtils contains methods that can be used by JPetTaskExecutor
- * to execute some tasks e.g. unpack hld file based on options. This preprocessing
- * is tipically done before running main tasks.
+ * JPetTaskChainExecutorUtils contains methods that can be used by JPetTaskExecutor.
+ * This preprocessing
+ * is typically done before running main tasks.
  */
 class JPetTaskChainExecutorUtils
 {
 public:
   using OptionsPerFile = std::vector<jpet_options_tools::OptsStrAny>;
-  /// process() method depends on the options can: 1.saves paramBank locally in ASCII format , 2. generate and add ScopeLoader
-  /// 3. unpack the hld file.
+  /// process() method depends on the options can: 1.saves paramBank locally in ASCII format
   static bool process(const JPetParams& params);
-  static void unpackFile(const char* filename, long long nevents, const char* configfile, const char* calibfile);
-  /// system(...) is returning integer, 0 when everything went smoothly and error code when not.
-  /// Here I just convert return value into boolean type - Sz.N.
-  static bool unzipFile(const char* filename);
   static std::vector<JPetParams> generateParams(const OptionsPerFile& opts);
   static std::shared_ptr<JPetParamManager> generateParamManager(const std::map<std::string, boost::any>& options);
 };
