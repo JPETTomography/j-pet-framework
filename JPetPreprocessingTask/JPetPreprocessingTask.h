aeb9a088214098e9a8eb20983a5a9596824aea6e
Kamil Rakoczy <grey277@wp.pl>
2017-10-19 17:57:33 +0200

    Create template for preprocessing task

---
+++ JPetPreprocessingTask/JPetPreprocessingTask.h
@@ -0,0 +1,35 @@
+/**
+ *  @copyright Copyright 2017 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file JPetPreProcessingTask.h
+ */
+
+
+#ifndef JPETPREPROCESSINGTASK_H
+#define JPETPREPROCESSINGTASK_H
+
+#include "../JPetTask/JPetTask.h"
+#include "../JPetParams/JPetParams.h"
+
+class JPetPreprocessingTask : public JPetTask
+{
+public:
+  explicit JPetPreprocessingTask(const char* name = "");
+  bool init(const JPetParamsInterface& inOptions) override;
+  bool run(const JPetDataInterface& inData) override;
+  bool terminate(JPetParamsInterface& outOptions) override;
+
+private:
+  JPetParams params;
+};
+
+#endif /*  !JPETPREPROCESSINGTASK_H */
