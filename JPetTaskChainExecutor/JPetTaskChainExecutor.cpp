20cd125f5fc4775ec3c70c9d57297780b869cb87
Kamil Rakoczy <grey277@wp.pl>
2017-10-23 13:45:39 +0200

    Adapt to new JPetTimer methods

--- JPetTaskChainExecutor/JPetTaskChainExecutor.cpp
+++ JPetTaskChainExecutor/JPetTaskChainExecutor.cpp
@@ -100,8 +100,8 @@ bool JPetTaskChainExecutor::process()
 
     timer.stopMeasurement("task " + taskName);
   }
-  timer.printElapsedTimeToInfo();
-  timer.printTotalElapsedTimeToInfo();
+  INFO(timer.getElapsedTime());
+  INFO(timer.getTotalElapsedTime());
   return true;
 }
 
