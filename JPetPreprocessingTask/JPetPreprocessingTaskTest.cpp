aad5e4d76c7f125384e6a64caf22bbfd44126566
Kamil Rakoczy <grey277@wp.pl>
2017-10-27 12:37:53 +0200

    Rename JPetPreprocessingTask to JPetParamBankHandlerTask

--- JPetPreprocessingTask/JPetPreprocessingTaskTest.cpp
+++
@@ -1,8 +0,0 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE JPetPreprocessingTask
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(FirstSuite)
-
-
-BOOST_AUTO_TEST_SUITE_END()
