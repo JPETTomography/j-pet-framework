54d9ecf00f56280347c569bcdc3ab5ef75b157a1
Kamil Rakoczy <grey277@wp.pl>
2017-11-19 15:09:03 +0100

    Change ../ to ./ in includes

--- unitTestUtils/DummyClassTest/DummyClassTest.cpp
+++ unitTestUtils/DummyClassTest/DummyClassTest.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE DummyClassTest 
 #include <boost/test/unit_test.hpp>
-#include "../DummyClass/DummyClass.h"
+#include "./DummyClass/DummyClass.h"
 
 
 BOOST_AUTO_TEST_SUITE(FirstSuite)
