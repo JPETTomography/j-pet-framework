25b7873c290fc583d8aad3bee36ab9df8c2fb813
Alek Gajos <alek.gajos@gmail.com>
2017-11-20 05:04:22 +0100

    Remove downloading of DB configs

--- download_data.sh
+++ download_data.sh
@@ -19,24 +19,6 @@ function check_sums {
 }
 CHECKSUM_COMMAND="check_sums"
 
-#for scp
-CFG_DIR=/home/framework_configs
-CFG_OUTPUT=$BASE_PATH/DBHandler/Config
-declare -a CFG_FILES=("configDB.json" "configDB.json.sphinx" "ReadMe.txt")
-CFG_SERVER=sphinx.if.uj.edu.pl
-
-declare -a CFG_FILES_IN=()
-for FILE in "${CFG_FILES[@]}"
-do
-  CFG_FILES_IN+=" ${CFG_DIR}/${FILE}"
-done
-
-declare -a CFG_FILES_OUT=()
-for FILE in "${CFG_FILES[@]}"
-do
-  CFG_FILES_OUT+=" ${CFG_OUTPUT}/${FILE}"
-done
-
 #for wget
 TEST_CHECKSUM_FILE="all_data.sha"
 TEST_BASE_URL="http://sphinx.if.uj.edu.pl/framework"
@@ -62,26 +44,3 @@ else
   echo "All unit test data OK."
 fi
 rm $LOCAL_TEST_CHECKSUM_FILE
-
-#check if scp files are already downloaded
-#if at least one exists assume it was downloaded (because you usually overwrite one with the other)
-DOWNLOADED=""
-for FILE in ${CFG_FILES_OUT[@]}
-do
-  if [ -f $FILE ]; then
-    DOWNLOADED="true"
-    break
-  fi
-done
-if [ ! -z $DOWNLOADED ] ; then
-  echo "Config files seem to exist, not downloading them again."
-else
-  #downloading sensinble data via scp
-  mkdir -p $CFG_OUTPUT
-  echo "Fetching database config files."
-  echo "To avoid downloading leave the username empty."
-  read -p "username at ${CFG_SERVER}:" USERNM;
-  if [ ! -z $USERNM ]; then
-    scp ${USERNM}@${CFG_SERVER}:"${CFG_FILES_IN}" ${CFG_OUTPUT}
-  fi
-fi
