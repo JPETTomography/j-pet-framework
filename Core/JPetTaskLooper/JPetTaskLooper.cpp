b6618aaeba1b7f99db9b301f0bc4e2748dbb9c56
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2017-12-15 14:00:14 +0100

    Add JPetTaskLooper that can be used to run tasks in some loops
    Also, some tests added.

---
+++ Core/JPetTaskLooper/JPetTaskLooper.cpp
@@ -0,0 +1,62 @@
+/**
+ *  @copyright Copyright 2017 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file JPetTaskLooper.cpp
+ */
+
+#include "JPetTaskLooper.h"
+#include "JPetLoggerInclude.h"
+#include "JPetParams/JPetParams.h"
+#include "JPetData/JPetData.h"
+
+JPetTaskLooper::JPetTaskLooper(const char* name, std::unique_ptr<JPetTask> subtask, Predicate isCondition):
+  JPetTask(name),
+  fIsCondition(isCondition)
+{
+  addSubTask(std::move(subtask));
+}
+
+bool JPetTaskLooper::init(const JPetParamsInterface&)
+{
+  return true;
+}
+
+bool JPetTaskLooper::run(const JPetDataInterface&)
+{
+  JPetParams inParams;
+  JPetParams outParams;
+  JPetDataInterface nullDataObject;
+  auto subTasks = getSubTasks();
+  if (subTasks.empty()) {
+    WARNING("No subtask defined in JPetTaskLooper");
+  }
+  for (auto subTask : subTasks) {
+    if (!subTask) {
+      ERROR("some subTasks are nullptr");
+      return false;
+    }
+  }
+  while (fIsCondition(inParams)) {
+    for (auto subTask : subTasks) {
+      subTask->init(inParams);
+      subTask->run(nullDataObject);
+      subTask->terminate(outParams);
+      inParams = outParams;
+    }
+  }
+  return true;
+}
+
+bool JPetTaskLooper::terminate(JPetParamsInterface&)
+{
+  return true;
+}
