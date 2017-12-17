4d63358a6ac5962a0a6d84e4ac2a74c91642f4b2
Kamil Rakoczy <grey277@wp.pl>
2017-12-17 19:04:05 +0100

    Add headers and init function

--- Core/JPetLogger/JPetLogger.h
+++ Core/JPetLogger/JPetLogger.h
@@ -11,7 +11,7 @@
  *  limitations under the License.
  *
  *  @file JPetLogger.h
- *  @brief Simple logger class. Don't use directly. Macros from JPetLoggerInclude.h should be used instead. 
+ *  @brief Simple logger class. Don't use directly. Macros from JPetLoggerInclude.h should be used instead.
  *  JPetLogger class implements a simple logging functionality.
  *  For every message the logfile stream is opened, the message is written and the stream is closed again. This assures that no messages are lost in case of errrors.
  */
@@ -27,49 +27,82 @@
 #include <boost/uuid/uuid.hpp>            // uuid class
 #include <boost/uuid/uuid_generators.hpp> // generators
 #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
 #endif
 
-class JPetLogger {
- public:
+class JPetLogger
+{
+public:
+  void init()
+  {
+    logging::add_file_log(
+      keywords::file_name = generateFilename(),
+      keywords::format = "[%TimeStamp%]: %Message%");
+
+    logging::core::get()->set_filter(
+      logging::trivial::severity >= logging::trivial::info);
+  }
   static void dateAndTime();
-  inline static void warning(const char* func, const char* msg) {
+  inline static void warning(const char* func, const char* msg)
+  {
     logMessage(func, msg, kWarning);
   }
-  inline static void error(const char* func, const char* msg) {
+  inline static void error(const char* func, const char* msg)
+  {
     logMessage(func, msg, kError);
   }
-  inline static void info(const char* func, const char* msg) {
+  inline static void info(const char* func, const char* msg)
+  {
     logMessage(func, msg, kInfo);
   }
-  inline static void debug(const char* func, const char* msg) {
+  inline static void debug(const char* func, const char* msg)
+  {
     logMessage(func, msg, kDebug);
   }
 
-  inline static void warning(const char* func, const std::string& msg) {
+  inline static void warning(const char* func, const std::string& msg)
+  {
     logMessage(func , msg.c_str(), kWarning);
   }
-  inline static void error(const char* func, const std::string& msg) {
+  inline static void error(const char* func, const std::string& msg)
+  {
     logMessage(func, msg.c_str(), kError);
   }
-  inline static void info(const char* func, const std::string& msg) {
+  inline static void info(const char* func, const std::string& msg)
+  {
     logMessage(func, msg.c_str(), kInfo);
   }
-  inline static void debug(const char* func, const std::string& msg) {
+  inline static void debug(const char* func, const std::string& msg)
+  {
     logMessage(func, msg.c_str(), kDebug);
   }
- private:
+private:
   enum MessageType {kInfo, kWarning, kError, kDebug};
 
   JPetLogger();
   JPetLogger(const JPetLogger&);
   JPetLogger& operator=(const JPetLogger&);
 
-  #ifndef __CINT__
-  inline static const std::string generateFilename(){
+#ifndef __CINT__
+  inline static const std::string generateFilename()
+  {
     return std::string("JPet_") + to_string(boost::uuids::random_generator()()) + std::string(".log");
   }
-  #endif
-  
+#endif
+
   static void logMessage(const char* func, const char* msg, MessageType type);
 
   static const std::string fFileName;
