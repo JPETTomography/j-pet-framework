eba28425f62857114c6fb9ea9eb021c9bd3c78c2
Kamil Rakoczy <grey277@wp.pl>
2018-04-23 16:14:40 +0200

    Fix CINT error

--- Core/JPetLogger/JPetLogger.h
+++ Core/JPetLogger/JPetLogger.h
@@ -1,9 +1,8 @@
 /**
- *  @copyright Copyright 2016 The J-PET Framework Authors. All rights reserved.
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may find a copy of the License in the LICENCE file.
- *
+inline static void setLogLevel(boost::log::trivial::severity_level level)
+{
+  boost::log::core::get()->set_filter(
+      boost::log::trivial::severity >= level);
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,11 +68,6 @@ public:
 
     out_stream << rec[boost::log::expressions::smessage];
   }
-#else
-  static void getSeverity();
-  static void formatter();
-#endif
-
 
   inline static void setLogLevel(boost::log::trivial::severity_level level)
   {
@@ -81,7 +75,11 @@ public:
       boost::log::trivial::severity >= level
     );
   }
-
+#else
+  static void getSeverity();
+  static void formatter();
+  static void setLogLevel();
+#endif
 
 private:
   JPetLogger();
