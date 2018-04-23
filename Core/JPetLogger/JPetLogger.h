3615da7672be64c2b32718ea206d29bc3b68b810
Kamil Rakoczy <grey277@wp.pl>
2018-04-23 16:11:07 +0200

    Fix copyright

--- Core/JPetLogger/JPetLogger.h
+++ Core/JPetLogger/JPetLogger.h
@@ -1,4 +1,5 @@
-/** setLogLevel()right 2016 The J-PET Framework Authors. All rights reserved.
+/**
+ *  @copyright Copyright 2016 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
