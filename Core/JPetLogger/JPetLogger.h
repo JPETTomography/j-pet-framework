1c2ab4ff73ea68dabb0303b79c2c9ab43df68efe
Kamil Rakoczy <grey277@wp.pl>
2018-05-08 10:34:02 +0200

    Fix header

--- Core/JPetLogger/JPetLogger.h
+++ Core/JPetLogger/JPetLogger.h
@@ -1,8 +1,9 @@
 /**
-inline static void setLogLevel(boost::log::trivial::severity_level level)
-{
-  boost::log::core::get()->set_filter(
-      boost::log::trivial::severity >= level);
+ *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
