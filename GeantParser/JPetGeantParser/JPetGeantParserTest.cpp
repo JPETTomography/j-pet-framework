110b1a60a20a8b5e5be1aa9a27a0e968f49faec3
Daria Kisielewska <dk.dariakisielewska@gmail.com>
2018-05-31 14:31:10 +0200

    added test for basics MC structures

---
+++ GeantParser/JPetGeantParser/JPetGeantParserTest.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE JPetGeantScinHitsTest
+#include <boost/test/unit_test.hpp>
+
+#include "./JPetGeantParser/JPetGeantParser.h"
+
+
+BOOST_AUTO_TEST_SUITE(FirstSuite)
+
+BOOST_AUTO_TEST_CASE( default_constructor )
+{
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
