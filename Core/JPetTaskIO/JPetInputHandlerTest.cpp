60fa663a041c8beed8ae33da03d93a85d7f7e889
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2018-11-16 14:57:41 +0100

    Add some more tests to InputHandler

--- Core/JPetTaskIO/JPetInputHandlerTest.cpp
+++ Core/JPetTaskIO/JPetInputHandlerTest.cpp
@@ -29,4 +29,109 @@ BOOST_AUTO_TEST_CASE(getEventRange)
   BOOST_REQUIRE_EQUAL(lastEvent, 100);
 }
 
+BOOST_AUTO_TEST_CASE(getEventRange2)
+{
+  using namespace jpet_options_generator_tools;
+  auto opts = getDefaultOptions();
+
+  opts["firstEvent_int"] = 1;
+  opts["lastEvent_int"] = 1;
+  auto mgr = std::make_shared<JPetParamManager>(new JPetParamManager(new JPetParamGetterAscii(dataFileName)));
+  JPetParams params(opts, mgr);
+
+  JPetInputHandler handler;
+  handler.openInput("unitTestData/JPetTaskChainExecutorTest/dabc_17025151847.unk.evt.root", params);
+  auto firstEvent = 0ll;
+  auto lastEvent = 0ll;
+  bool isOK = false;
+  std::tie(isOK, firstEvent, lastEvent) = handler.getEventRange(opts);
+  BOOST_REQUIRE(isOK);
+  BOOST_REQUIRE_EQUAL(firstEvent, 1);
+  BOOST_REQUIRE_EQUAL(lastEvent, 1);
+}
+
+BOOST_AUTO_TEST_CASE(getEventRange3)
+{
+  using namespace jpet_options_generator_tools;
+  auto opts = getDefaultOptions();
+
+  opts["firstEvent_int"] = 0;
+  opts["lastEvent_int"] = 1;
+  auto mgr = std::make_shared<JPetParamManager>(new JPetParamManager(new JPetParamGetterAscii(dataFileName)));
+  JPetParams params(opts, mgr);
+
+  JPetInputHandler handler;
+  handler.openInput("unitTestData/JPetTaskChainExecutorTest/dabc_17025151847.unk.evt.root", params);
+  auto firstEvent = 0ll;
+  auto lastEvent = 0ll;
+  bool isOK = false;
+  std::tie(isOK, firstEvent, lastEvent) = handler.getEventRange(opts);
+  BOOST_REQUIRE(isOK);
+  BOOST_REQUIRE_EQUAL(firstEvent, 0);
+  BOOST_REQUIRE_EQUAL(lastEvent, 1);
+}
+
+BOOST_AUTO_TEST_CASE(getEventRange4)
+{
+  using namespace jpet_options_generator_tools;
+  auto opts = getDefaultOptions();
+
+  opts["firstEvent_int"] = 1;
+  opts["lastEvent_int"] = -1;
+  auto mgr = std::make_shared<JPetParamManager>(new JPetParamManager(new JPetParamGetterAscii(dataFileName)));
+  JPetParams params(opts, mgr);
+
+  JPetInputHandler handler;
+  handler.openInput("unitTestData/JPetTaskChainExecutorTest/dabc_17025151847.unk.evt.root", params);
+  auto firstEvent = 0ll;
+  auto lastEvent = 0ll;
+  bool isOK = false;
+  std::tie(isOK, firstEvent, lastEvent) = handler.getEventRange(opts);
+  BOOST_REQUIRE(isOK);
+  BOOST_REQUIRE_EQUAL(firstEvent, 1);
+  BOOST_REQUIRE_EQUAL(lastEvent, 100);
+}
+
+BOOST_AUTO_TEST_CASE(getEventRange5)
+{
+  using namespace jpet_options_generator_tools;
+  auto opts = getDefaultOptions();
+
+  opts["firstEvent_int"] = -1;
+  opts["lastEvent_int"] = -1;
+  auto mgr = std::make_shared<JPetParamManager>(new JPetParamManager(new JPetParamGetterAscii(dataFileName)));
+  JPetParams params(opts, mgr);
+
+  JPetInputHandler handler;
+  handler.openInput("unitTestData/JPetTaskChainExecutorTest/dabc_17025151847.unk.evt.root", params);
+  auto firstEvent = 0ll;
+  auto lastEvent = 0ll;
+  bool isOK = false;
+  std::tie(isOK, firstEvent, lastEvent) = handler.getEventRange(opts);
+  BOOST_REQUIRE(isOK);
+  BOOST_REQUIRE_EQUAL(firstEvent, 0);
+  BOOST_REQUIRE_EQUAL(lastEvent, 100);
+}
+
+BOOST_AUTO_TEST_CASE(getEventRange6)
+{
+  using namespace jpet_options_generator_tools;
+  auto opts = getDefaultOptions();
+
+  opts["firstEvent_int"] = -1;
+  opts["lastEvent_int"] = 5;
+  auto mgr = std::make_shared<JPetParamManager>(new JPetParamManager(new JPetParamGetterAscii(dataFileName)));
+  JPetParams params(opts, mgr);
+
+  JPetInputHandler handler;
+  handler.openInput("unitTestData/JPetTaskChainExecutorTest/dabc_17025151847.unk.evt.root", params);
+  auto firstEvent = 0ll;
+  auto lastEvent = 0ll;
+  bool isOK = false;
+  std::tie(isOK, firstEvent, lastEvent) = handler.getEventRange(opts);
+  BOOST_REQUIRE(isOK);
+  BOOST_REQUIRE_EQUAL(firstEvent, 0);
+  BOOST_REQUIRE_EQUAL(lastEvent, 5);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
