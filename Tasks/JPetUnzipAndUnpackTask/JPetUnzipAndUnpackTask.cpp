c8f340b4e5043c61e3b5f9f673000266ebcfc092
Kamil Rakoczy <grey277@wp.pl>
2018-12-18 22:32:45 +0100

    Refactor run method

--- Tasks/JPetUnzipAndUnpackTask/JPetUnzipAndUnpackTask.cpp
+++ Tasks/JPetUnzipAndUnpackTask/JPetUnzipAndUnpackTask.cpp
@@ -59,40 +59,48 @@ bool JPetUnzipAndUnpackTask::init(const JPetParams& inParams)
 bool JPetUnzipAndUnpackTask::run(const JPetDataInterface&)
 {
   using namespace jpet_options_tools;
-  auto inputFile = getInputFile(fOptions);
-  auto inputFileType = FileTypeChecker::getInputFileType(fOptions);
-  auto unpackerConfigFile = getUnpackerConfigFile(fOptions);
-  bool unpackFlag = false;
+  const auto inputFile = getInputFile(fOptions);
+  const auto inputFileType = FileTypeChecker::getInputFileType(fOptions);
+  const auto unzippedFilename = JPetCommonTools::stripFileNameSuffix(inputFile);
+  const auto unpackerConfigFile = getUnpackerConfigFile(fOptions);
 
-  if (inputFileType == FileTypeChecker::kHld) {
-    unpackFlag = unpackFile(inputFile, getTotalEvents(fOptions), unpackerConfigFile, fTOToffsetCalibFile, fTDCnonlinearityCalibFile);
-    if (!unpackFlag) {
-      ERROR("Error occurred while unpacking file: " + inputFile);
-    }
+  bool runStatus = false;
+
+  switch (inputFileType) {
+  case FileTypeChecker::kHld:
+    INFO("Unpacking file " + inputFile);
+    runStatus = unpackFile(inputFile, getTotalEvents(fOptions), unpackerConfigFile, fTOToffsetCalibFile, fTDCnonlinearityCalibFile);
     fUnpackHappened = true;
-  } else if ( inputFileType == FileTypeChecker::kZip) {
-    INFO( std::string("Unzipping file before unpacking") );
-    if ( !unzipFile(inputFile) ) {
-      ERROR( std::string("Problem with Unzipping file: ") + inputFile );
-      return false;
-    } else {
-      INFO( std::string("Unpacking") );
-      std::string unzippedFilename = JPetCommonTools::stripFileNameSuffix(std::string(inputFile)).c_str();
-      unpackFlag = unpackFile(unzippedFilename, getTotalEvents(fOptions), unpackerConfigFile, fTOToffsetCalibFile, fTDCnonlinearityCalibFile);
-      if (!unpackFlag) {
-        ERROR("Error occurred while unpacking file: " + unzippedFilename);
-      }
-      fUnpackHappened = true;
+    break;
+  case FileTypeChecker::kZip:
+    INFO("Unzipping file before unpacking, file name: " + inputFile);
+    runStatus = unzipFile(inputFile);
+    if (!runStatus) {
+      ERROR("Problem with unzipping file: " + inputFile );
+      break;
     }
-  } else { // for any other type just set unpack flag to true
-    unpackFlag = true;
+    INFO("Unpacking file " + unzippedFilename);
+    runStatus = unpackFile(unzippedFilename, getTotalEvents(fOptions), unpackerConfigFile, fTOToffsetCalibFile, fTDCnonlinearityCalibFile);
+    fUnpackHappened = true;
+    break;
+  case FileTypeChecker::kUndefinedFileType:
+    runStatus = false;
+    break;
+  case FileTypeChecker::kNoType:
+  case FileTypeChecker::kScope:
+  case FileTypeChecker::kHldRoot:
+  case FileTypeChecker::kMCGeant:
+  case FileTypeChecker::kRoot:
+  default:
+    runStatus = true;
+    break;
   }
 
-  if (FileTypeChecker::getInputFileType(fOptions) == FileTypeChecker::kUndefinedFileType) {
-    ERROR( Form("Unknown file type provided for file: %s", getInputFile(fOptions).c_str()) );
-    return false;
+  if (!runStatus) {
+    ERROR("Error occurred while unpacking/unzipping file: " + inputFile);
   }
-  return unpackFlag;
+
+  return runStatus;
 }
 
 bool JPetUnzipAndUnpackTask::terminate(JPetParams& outParams)
