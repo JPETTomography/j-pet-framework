fccca3a0b4daed2ab8619d06ccf8ac9f2dccfa8a
Kamil Rakoczy <grey277@wp.pl>
2019-07-24 08:12:02 +0200

    Add root 6.18 test

---
+++ jenkins/ubuntu16,cmake351,default_gcc,root612/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function executeCommand {
+    $@
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+    echo "Exit code[" $@ "]: $rc"
+}
+executeCommand "export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-dependencies/lib"
+executeCommand "export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include"
+executeCommand "source /root-system/bin/thisroot.sh"
+executeCommand "rm -rf Unpacker2 || true"
+executeCommand "git clone --single-branch --branch cmake https://github.com/grey277/Unpacker2.git"
+executeCommand "mkdir -p Unpacker2/build"
+executeCommand "cd Unpacker2/build"
+executeCommand "cmake .."
+executeCommand "cmake --build ."
+executeCommand "sudo make install"
+executeCommand "cd ../.."
+executeCommand "mkdir -p build"
+executeCommand "cd build"
+executeCommand "cmake .."
+executeCommand "make all tests -j4"
+executeCommand "source src/thisframework.sh"
+executeCommand "ctest -j6 -C Debug -T test --output-on-failure"
