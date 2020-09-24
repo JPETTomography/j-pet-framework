6da1223e16f999526425b4e4b44fc54e3f4d043c
kamil.dulski <kamil.dulski@gmail.com>
2020-09-24 11:59:41 +0200

    Update of the DecayTree structure

--- include/GeantParser/JPetGeantDecayTree/JPetGeantDecayTree.h
+++ include/GeantParser/JPetGeantDecayTree/JPetGeantDecayTree.h
@@ -16,9 +16,22 @@
 #ifndef JPETGEANTDECAYTREE_H
 #define JPETGEANTDECAYTREE_H
 
+#include "TVector3.h" 
 #include "TObject.h"
 #include <vector>
-#include "TVector3.h" 
+#include <tuple>
+#include <map>
+
+/**
+ * @class JPetGeantDecayTree
+ * @brief Class stores decay tree structures (in form of vertices and tracks)
+ * Class is not yet fully implemented
+ */
+
+enum InteractionType {
+  primaryGamma, scattActivePart, scattNonActivePart, secondaryPart, unknown
+};
+
 
 
 class JPetGeantDecayTree : public TObject 
@@ -27,11 +40,24 @@ class JPetGeantDecayTree : public TObject
     public:
         JPetGeantDecayTree();
         ~JPetGeantDecayTree();
+        
+        void Clean();
+        void ClearVectors();
+        InteractionType GetInteractionType(int nodeID, int trackID);
+        int GetPreviousNodeID(int nodeID, int trackID);
+        int GetPrimaryNodeID(int nodeID, int trackID);
+        void AddNode(int nodeID, int previousNodeID, int trackID, InteractionType interactionType);
 
     private:
+// fNodeTrackConnections is constructed as {nodeID, previous NodeID, connecting trackID}
+// previous node for primary gamma = -1
+        std::vector<std::tuple<int, int, int>> fNodeConnections;
+// fNodeInteractionType connects nodeID with the type of interaction and trackID
+// types of interaction: 100 - scattering in active part, 10 scattering in non-active part
+// 0 - secondary particle generation
+        std::vector<std::tuple<int, InteractionType, int>> fNodeInteractionType;
 
-
-     ClassDef(JPetGeantDecayTree,2)
+     ClassDef(JPetGeantDecayTree,3)
 
 };
 
--- src/GeantParser/JPetGeantDecayTree/JPetGeantDecayTree.cpp
+++ src/GeantParser/JPetGeantDecayTree/JPetGeantDecayTree.cpp
@@ -21,4 +21,59 @@ ClassImp(JPetGeantDecayTree)
 {
 }
 
-JPetGeantDecayTree::~JPetGeantDecayTree() {}
+JPetGeantDecayTree::~JPetGeantDecayTree() 
+{
+  fNodeConnections.clear();
+  fNodeInteractionType.clear();
+}
+
+void JPetGeantDecayTree::Clean()
+{
+  this->ClearVectors();
+}
+
+void JPetGeantDecayTree::ClearVectors() 
+{
+  fNodeConnections.clear();
+  fNodeInteractionType.clear();
+}
+
+// cppcheck-suppress unusedFunction
+InteractionType JPetGeantDecayTree::GetInteractionType(int nodeID, int trackID)
+{
+  for (int i=fNodeInteractionType.size()-1; i>=0; i--) {
+    if (std::get<0>(fNodeInteractionType[i]) == nodeID && std::get<2>(fNodeInteractionType[i]) == trackID)
+      return std::get<1>(fNodeInteractionType[i]);
+  }
+  return InteractionType::unknown;
+}
+
+int JPetGeantDecayTree::GetPreviousNodeID(int nodeID, int trackID)
+{
+  int previousNodeID = nodeID;
+  for (int i=fNodeConnections.size()-1; i>=0; i--) {
+    if (std::get<0>(fNodeConnections[i]) == nodeID && std::get<1>(fNodeConnections[i]) != -1 
+                                                    && std::get<2>(fNodeConnections[i]) == trackID)
+      return std::get<1>(fNodeConnections[i]);
+    else if (std::get<0>(fNodeConnections[i]) == nodeID && std::get<1>(fNodeConnections[i]) != -1)
+      return std::get<1>(fNodeConnections[i]);
+  }
+  return previousNodeID;
+}
+
+// cppcheck-suppress unusedFunction
+int JPetGeantDecayTree::GetPrimaryNodeID(int nodeID, int trackID)
+{
+  int previousNodeID = nodeID, primaryNodeID = nodeID;
+  while (previousNodeID >= 10) {
+    primaryNodeID = previousNodeID;
+    previousNodeID = GetPreviousNodeID(primaryNodeID, trackID);
+  }
+  return previousNodeID;
+}
+
+void JPetGeantDecayTree::AddNode(int nodeID, int previousNodeID, int trackID, InteractionType interactionType)
+{
+  fNodeConnections.push_back(std::make_tuple(nodeID, previousNodeID, trackID));
+  fNodeInteractionType.push_back(std::make_tuple(nodeID, interactionType, trackID));
+}
