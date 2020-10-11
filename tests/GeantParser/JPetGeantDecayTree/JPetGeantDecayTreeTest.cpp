128b05af3fea2e6c5a7eea7ce84e4a492b42a96b
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2020-10-11 23:26:46 +0200

    Fix filename in test header

--- tests/GeantParser/JPetGeantDecayTree/JPetGeantDecayTreeTest.cpp
+++ tests/GeantParser/JPetGeantDecayTree/JPetGeantDecayTreeTest.cpp
@@ -1,5 +1,5 @@
 /**
- *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
+ *  @copyright Copyright 2020 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
@@ -10,11 +10,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *  @file JPetGeantParserToolsTest.cpp
+ *  @file JPetGeantParserDecayTreeTest.cpp
  */
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE JPetGeantParserToolsTest
+#define BOOST_TEST_MODULE JPetGeantParserDecayTreeTest
 #include <boost/test/unit_test.hpp>
 
 #include "JPetGeantDecayTree/JPetGeantDecayTree.h"
