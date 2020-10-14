75a517ff4185371e7b1de6705da928da3e836bb2
Kamil Rakoczy <grey277@wp.pl>
2020-10-14 20:49:18 +0200

    Add Jenkinsfile

--- jenkins/ubuntu16,cmake315,default_gcc,root618/docker-entrypoint.sh
+++ jenkins/ubuntu16,cmake315,default_gcc,root618/docker-entrypoint.sh
@@ -4,6 +4,14 @@ function executeCommand {
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     echo "Exit code[" $@ "]: $rc"
 }
+
+executeCommand "ls -la"
+executeCommand "ls -la /root-system/"
+executeCommand "ls -la /root-system/include/"
+executeCommand "ls -la /usr/local/bin/"
+executeCommand "which cmake"
+executeCommand "cmake --version"
+
 executeCommand "export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-dependencies/lib"
 executeCommand "export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include"
 executeCommand "source /root-system/bin/thisroot.sh"
