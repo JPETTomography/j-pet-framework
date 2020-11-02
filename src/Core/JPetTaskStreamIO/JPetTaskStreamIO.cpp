1a4d967f10241577800acd68005eed4f6a24b7d6
Alek Gajos <alek.gajos@gmail.com>
2020-11-03 00:30:05 +0100

    Clang-format updated code

--- src/Core/JPetTaskStreamIO/JPetTaskStreamIO.cpp
+++ src/Core/JPetTaskStreamIO/JPetTaskStreamIO.cpp
@@ -15,20 +15,20 @@
 
 #include "JPetTaskStreamIO/JPetTaskStreamIO.h"
 
-#include "./JPetUserTask/JPetUserTask.h"
 #include "./JPetData/JPetData.h"
 #include "./JPetOptionsGenerator/JPetOptionsGeneratorTools.h"
+#include "./JPetUserTask/JPetUserTask.h"
 
-JPetTaskStreamIO::JPetTaskStreamIO(const char* name,
-                                   const char* in_file_type,
-                                   const char* out_file_type):
-  JPetTaskIO(name, in_file_type, out_file_type)
+JPetTaskStreamIO::JPetTaskStreamIO(const char* name, const char* in_file_type, const char* out_file_type)
+    : JPetTaskIO(name, in_file_type, out_file_type)
 {
-  if (std::string(out_file_type).empty()) {
+  if (std::string(out_file_type).empty())
+  {
     WARNING("JPetTaskStreamIO is not intended to be executed without an output file.");
     fIsOutput = false;
   }
-  if (std::string(in_file_type).empty()) {
+  if (std::string(in_file_type).empty())
+  {
     WARNING("JPetTaskStreamIO is not intended to be executed without an input file.");
     fIsInput = false;
   }
@@ -37,21 +37,26 @@ JPetTaskStreamIO::JPetTaskStreamIO(const char* name,
 bool JPetTaskStreamIO::run(const JPetDataInterface&)
 {
   using namespace jpet_options_tools;
-  if (fSubTasks.empty()) {
+  if (fSubTasks.empty())
+  {
     ERROR("No subTask set");
     return false;
   }
-  if (isInput()) {
-    if (!fInputHandler) {
+  if (isInput())
+  {
+    if (!fInputHandler)
+    {
       ERROR("No inputHandler set");
       return false;
     }
   }
 
   // init all subtasks before any processing
-  for (const auto& pTask : fSubTasks) {
+  for (const auto& pTask : fSubTasks)
+  {
     auto subTaskName = pTask->getName();
-    if (!pTask->init(fParams)) {
+    if (!pTask->init(fParams))
+    {
       // if init of any task fails, execution of the task stream makes no sense
       ERROR("Init() of: " + subTaskName + " failed. Aborting execution of the whole task stream. ");
       return false;
@@ -59,17 +64,20 @@ bool JPetTaskStreamIO::run(const JPetDataInterface&)
   }
 
   assert(fInputHandler);
-  if (!fInputHandler->setEntryRange(fParams.getOptions())) {
+  if (!fInputHandler->setEntryRange(fParams.getOptions()))
+  {
     ERROR("An error occured in setEntryRange");
     return false;
   }
 
-  auto lastEvent =  fInputHandler->getLastEntryNumber();
+  auto lastEvent = fInputHandler->getLastEntryNumber();
   assert(lastEvent >= 0);
 
-  do{
+  do
+  {
 
-    if (isProgressBar(fParams.getOptions())) {
+    if (isProgressBar(fParams.getOptions()))
+    {
       displayProgressBar(getName(), fInputHandler->getCurrentEntryNumber(), lastEvent);
     }
 
@@ -79,11 +87,13 @@ bool JPetTaskStreamIO::run(const JPetDataInterface&)
     // iterator
     auto subtask_it = fSubTasks.begin();
 
-    while (subtask_it != fSubTasks.end()) {
+    while (subtask_it != fSubTasks.end())
+    {
 
       auto& current_task = *subtask_it;
 
-      if (!current_task->run(JPetData(*output_event))) {
+      if (!current_task->run(JPetData(*output_event)))
+      {
         ERROR("In run() of: " + current_task->getName() + ". ");
       }
 
@@ -94,21 +104,25 @@ bool JPetTaskStreamIO::run(const JPetDataInterface&)
 
     auto& lastTask = fSubTasks.back();
 
-    if (isOutput()) {
-      if (!fOutputHandler->writeEventToFile(lastTask.get())) {
+    if (isOutput())
+    {
+      if (!fOutputHandler->writeEventToFile(lastTask.get()))
+      {
         WARNING("Some problems occured while writing the event to file.");
         return false;
       }
     }
 
-  }while(fInputHandler->nextEntry());
+  } while (fInputHandler->nextEntry());
 
   // terminate all subtasks after all processing
-  for (const auto& pTask : fSubTasks) {
+  for (const auto& pTask : fSubTasks)
+  {
     JPetParams subTaskParams;
 
     bool ok = pTask->terminate(subTaskParams);
-    if (!ok) {
+    if (!ok)
+    {
       ERROR("In terminate() of:" + pTask->getName() + ". ");
     }
     fParams = mergeWithExtraParams(fParams, subTaskParams);
@@ -117,6 +131,4 @@ bool JPetTaskStreamIO::run(const JPetDataInterface&)
   return true;
 }
 
-JPetTaskStreamIO::~JPetTaskStreamIO()
-{
-}
+JPetTaskStreamIO::~JPetTaskStreamIO() {}
