45c3c94abe3b00f5c7d28e1c1d2a510d08f9f6f9
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2021-01-23 13:22:20 -0300

    adding JPetBaseHit class

---
+++ include/DataObjects/JPetBaseHit/JPetBaseHit.h
@@ -0,0 +1,64 @@
+/**
+ *  @copyright Copyright 2021 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file JPetBaseHit.h
+ */
+
+#ifndef JPETBASEHIT_H
+#define JPETBASEHIT_H
+
+#include "JPetScin/JPetScin.h"
+#include <TObject.h>
+#include <TRef.h>
+
+/**
+ * @brief Base class for hit data classes
+ */
+class JPetBaseHit : public TObject
+{
+public:
+  JPetBaseHit();
+  JPetBaseHit(double time, double energy, TVector3& position, JPetScin& scin);
+  virtual ~JPetBaseHit();
+  double getTime() const;
+  double getEnergy() const;
+  double getPosX() const;
+  double getPosY() const;
+  double getPosZ() const;
+  double getPos(int index) const;
+  const TVector3& getPos() const;
+  const JPetScin& getScin() const;
+  void setTime(double time);
+  void setEnergy(double energy);
+  void setPosX(double x);
+  void setPosY(double y);
+  void setPosZ(double z);
+  void setPos(double x, double y, double z);
+  void setPos(TVector3& position);
+  void setScin(const JPetScin& scin);
+  void Clear(Option_t* opt = "");
+  static JPetBaseHit& getDummyResult();
+
+protected:
+#ifndef __CINT__
+  double fTime = 0.0;
+  double fEnergy = 0.0;
+#else
+  double fTime;
+  double fEnergy;
+#endif
+  TVector3 fPos;
+  TRef fScin = NULL;
+
+  ClassDef(JPetBaseHit, 1);
+};
+#endif /* !JPETBASEHIT_H */
---
+++ tests/DataObjects/JPetBaseHit/JPetBaseHitTest.cpp
@@ -0,0 +1,81 @@
+/**
+ *  @copyright Copyright 2021 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file JPetBaseHitTest.cpp
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE JPetBaseHitTest
+
+#include "JPetBaseHit/JPetBaseHit.h"
+#include "JPetScin/JPetScin.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(BaseHitTest)
+
+BOOST_AUTO_TEST_CASE(default_constructor)
+{
+  double epsilon = 0.0001;
+  JPetBaseHit hit;
+  BOOST_REQUIRE_CLOSE(hit.getTime(), 0.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getEnergy(), 0.0, epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(constructor)
+{
+  double epsilon = 0.0001;
+  JPetScin scin(456, 1.0, 2.0, 3.0, 4.0);
+  TVector3 position(11.0, 22.0, 33.0);
+  JPetBaseHit hit(10.0, 511.0, position, scin);
+  BOOST_REQUIRE_CLOSE(hit.getTime(), 10.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getEnergy(), 511.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosX(), 11.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosY(), 22.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosZ(), 33.0, epsilon);
+  BOOST_REQUIRE_EQUAL(hit.getScin(), scin);
+}
+
+BOOST_AUTO_TEST_CASE(SetAndGetTest)
+{
+  double epsilon = 0.0001;
+
+  JPetBaseHit hit;
+  hit.setTime(33.3);
+  hit.setEnergy(511.0);
+  BOOST_REQUIRE_CLOSE(hit.getTime(), 33.3, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getEnergy(), 511.0, epsilon);
+
+  hit.setPos(1.0, 2.0, 3.0);
+  BOOST_REQUIRE_CLOSE(hit.getPosX(), 1.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosY(), 2.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosZ(), 3.0, epsilon);
+
+  hit.setPosX(4.0);
+  hit.setPosY(5.0);
+  hit.setPosZ(6.0);
+  BOOST_REQUIRE_CLOSE(hit.getPosX(), 4.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosY(), 5.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosZ(), 6.0, epsilon);
+
+  TVector3 position(7.0, 8.0, 9.0);
+  hit.setPos(position);
+  BOOST_REQUIRE_CLOSE(hit.getPosX(), 7.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosY(), 8.0, epsilon);
+  BOOST_REQUIRE_CLOSE(hit.getPosZ(), 9.0, epsilon);
+
+  JPetScin scin(456, 1.0, 2.0, 3.0, 4.0);
+  hit.setScin(scin);
+  BOOST_REQUIRE_EQUAL(hit.getScin(), scin);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
