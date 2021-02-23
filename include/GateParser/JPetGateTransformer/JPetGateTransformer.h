02aabb564d7fe53af4ae14f7e82b41decddfb6be
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2021-02-23 15:24:50 +0100

    Add JPetGateTransformer changes
    I added temporarly the code from gate converter.
    Some of the things are hardcoded.

--- include/GateParser/JPetGateTransformer/JPetGateTransformer.h
+++ include/GateParser/JPetGateTransformer/JPetGateTransformer.h
@@ -1,5 +1,5 @@
 /**
- *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
+ *  @copyright Copyright 2021 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
@@ -16,7 +16,170 @@
 #ifndef JPETGATETRANSFORMER_H
 #define JPETGATETRANSFORMER_H
 
-class JPetGateTransformer
+#include "JPetTask/JPetTask.h"
+#include <GateHit/GateHit.h>
+
+class Writer
 {
+public:
+  Writer() {}
+  void init();
+  void write(GateHit gh);
+  void close();
+  void test();
+  void set_output_file_path(std::string path);
+
+  std::string output_file_path = "";
+  TFile* p_file = nullptr;
+  TTree* p_tree = nullptr;
+  GateHit* p_gate_hit = nullptr;
 };
+
+class Reader
+{
+public:
+  enum class DetectorGeometry
+  {
+    Unknown = 0,
+    ThreeLayers = 1,
+    TwentyFourModules = 2
+  };
+
+  Reader() {}
+  void init();
+  bool read();
+  GateHit* get();
+  void close();
+
+  void set_geometry(DetectorGeometry dg);
+  int get_scintillator_id();
+  void set_input_file_path(std::string path);
+
+  std::string input_file_path = "";
+  TFile* p_file = nullptr;
+  TTree* p_tree = nullptr;
+  int entries = 0;
+  int entry_index = 0;
+  DetectorGeometry detector_geometry = DetectorGeometry::Unknown;
+
+  GateHit gate_hit;
+  // Zmienne branchów
+  int event_id = -1;
+  int track_id = -1;
+  int parent_id = -1;
+  int pdg = 0;
+  int volID[10];
+
+  float edep = 0.0;
+  double time = 0.0;
+  float posx = 0.0;
+  float posy = 0.0;
+  float posz = 0.0;
+  float sourcex = 0.0;
+  float sourcey = 0.0;
+  float sourcez = 0.0;
+
+  Char_t process_name[20];
+
+  int level1ID = 0;
+  int baseID = 0;
+
+  unsigned int counter = 0;
+
+  // Branche
+  TBranch* b_event_id = nullptr;
+  TBranch* b_track_id = nullptr;
+  TBranch* b_parent_id = nullptr;
+  TBranch* b_pdg = nullptr;
+
+  TBranch* b_edep = nullptr;
+  TBranch* b_time = nullptr;
+  TBranch* b_posx = nullptr;
+  TBranch* b_posy = nullptr;
+  TBranch* b_posz = nullptr;
+  TBranch* b_sourcex = nullptr;
+  TBranch* b_sourcey = nullptr;
+  TBranch* b_sourcez = nullptr;
+  TBranch* b_process_name = nullptr;
+  TBranch* b_volID = nullptr;
+};
+
+class JPetGateTransformer : public JPetTask
+{
+public:
+  enum class DetectorGeometry
+  {
+    kUnknown = 0,
+    kThreeLayers = 1,
+    kTwentyFourModules = 2
+  };
+  using OptsStrAny = std::map<std::string, boost::any>;
+  explicit JPetGateTransformer(const char* name = "");
+  bool init(const JPetParams& inOptions) override;
+  bool run(const JPetDataInterface& inData) override;
+  bool terminate(JPetParams& outOptions) override;
+
+private:
+  ////void init();
+  // bool read();
+  // GateHit* get();
+  // void close();
+
+  // void set_geometry(DetectorGeometry dg);
+  // int get_scintillator_id();
+  // void set_input_file_path(std::string path);
+
+  // std::string input_file_path = "";
+  // TFile* p_file = nullptr;
+  // TTree* p_tree = nullptr;
+  // int entries = 0;
+  // int entry_index = 0;
+  // DetectorGeometry detector_geometry = DetectorGeometry::kUnknown;
+
+  // GateHit gate_hit;
+
+  //// Zmienne branchów
+  // int event_id = -1;
+  // int track_id = -1;
+  // int parent_id = -1;
+  // int pdg = 0;
+  // int volID[10];
+
+  // float edep = 0.0;
+  // double time = 0.0;
+  // float posx = 0.0;
+  // float posy = 0.0;
+  // float posz = 0.0;
+  // float sourcex = 0.0;
+  // float sourcey = 0.0;
+  // float sourcez = 0.0;
+
+  // Char_t process_name[20];
+
+  // int level1ID = 0;
+  // int baseID = 0;
+
+  // unsigned int counter = 0;
+
+  //// Branche
+  // TBranch* b_event_id = nullptr;
+  // TBranch* b_track_id = nullptr;
+  // TBranch* b_parent_id = nullptr;
+  // TBranch* b_pdg = nullptr;
+
+  // TBranch* b_edep = nullptr;
+  // TBranch* b_time = nullptr;
+  // TBranch* b_posx = nullptr;
+  // TBranch* b_posy = nullptr;
+  // TBranch* b_posz = nullptr;
+  // TBranch* b_sourcex = nullptr;
+  // TBranch* b_sourcey = nullptr;
+  // TBranch* b_sourcez = nullptr;
+  // TBranch* b_process_name = nullptr;
+  // TBranch* b_volID = nullptr;
+
+protected:
+  OptsStrAny fOptions;
+};
+
 #endif /*  !JPETGATETRANSFORMER_H */
--- src/GateParser/JPetGateTransformer/JPetGateTransformer.cpp
+++ src/GateParser/JPetGateTransformer/JPetGateTransformer.cpp
@@ -13,4 +13,258 @@
  *  @file JPetGateTransformer.cpp
  */
 
-#include "./JPetGateTransformer.h"
+#include "JPetOptionsTools/JPetOptionsTools.h"
+#include <JPetGateTransformer/JPetGateTransformer.h>
+using namespace jpet_options_tools;
+
+JPetGateTransformer::JPetGateTransformer(const char* name) : JPetTask(name) {}
+
+bool JPetGateTransformer::init(const JPetParams& inParams)
+{
+  INFO("GateTransformer started.");
+  fOptions = inParams.getOptions();
+
+  // p_file = new TFile(input_file_path.c_str(), "READ");
+  // p_tree = dynamic_cast<TTree*>(p_file->Get("Hits"));
+  // entries = p_tree->GetEntries();
+
+  // p_tree->SetBranchAddress("PDGEncoding", &pdg, &b_pdg);
+  // p_tree->SetBranchAddress("trackID", &track_id, &b_track_id);
+  // p_tree->SetBranchAddress("parentID", &parent_id, &b_parent_id);
+  // p_tree->SetBranchAddress("time", &time, &b_time);
+  // p_tree->SetBranchAddress("eventID", &event_id, &b_event_id);
+  // p_tree->SetBranchAddress("posX", &posx, &b_posx);
+  // p_tree->SetBranchAddress("posY", &posy, &b_posy);
+  // p_tree->SetBranchAddress("posZ", &posz, &b_posz);
+  // p_tree->SetBranchAddress("edep", &edep, &b_edep);
+  // p_tree->SetBranchAddress("processName", process_name, &b_process_name);
+  // p_tree->SetBranchAddress("sourcePosX", &sourcex, &b_sourcex);
+  // p_tree->SetBranchAddress("sourcePosY", &sourcey, &b_sourcey);
+  // p_tree->SetBranchAddress("sourcePosZ", &sourcez, &b_sourcez);
+  // p_tree->SetBranchAddress("volumeID", &volID, &b_volID);
+
+  return true;
+}
+
+bool JPetGateTransformer::run(const JPetDataInterface&)
+{
+  std::string g_output_file_name = "test.out";
+  std::string g_input_file_name = "test.root";
+  Reader::DetectorGeometry g_detector_geometry = Reader::DetectorGeometry::ThreeLayers;
+
+  Writer w;
+  w.set_output_file_path(g_output_file_name);
+  w.init();
+  Reader r;
+  r.set_geometry(g_detector_geometry);
+  r.set_input_file_path(g_input_file_name);
+  r.init();
+  while (r.read())
+  {
+    GateHit* p_gh = r.get();
+    if (p_gh != nullptr)
+      w.write(*p_gh);
+  }
+  r.close();
+  w.close();
+  // auto inputFileWithPath = getInputFile(fOptions);
+  // auto outputPath = getOutputPath(fOptions);
+  // if(outputPath == "") { outputPath = "./"; }
+  // INFO(Form("Unzipping file: %s into %s", inputFileWithPath.c_str(), outputPath.c_str()));
+  // if (!unzipFile(inputFileWithPath, outputPath)) {
+  // ERROR(Form("Problem with unzipping file: %s", inputFileWithPath.c_str()));
+  // return false;
+  //}
+  return true;
+}
+
+bool JPetGateTransformer::terminate(JPetParams& outParams)
+{
+  OptsStrAny new_opts;
+  // setOutputFileType(new_opts, "hld");
+  // auto outputFile =
+  // getOutputPath(fOptions)
+  //+ JPetCommonTools::stripFileNameSuffix(getInputFile(fOptions));
+  // setOutputFile(new_opts, outputFile);
+
+  // if (isOptionSet(fOptions, "firstEvent_int") && isOptionSet(fOptions, "lastEvent_int")) {
+  // if (getOptionAsInt(fOptions, "firstEvent_int") != -1 && getOptionAsInt(fOptions, "lastEvent_int") != -1) {
+  // setResetEventRangeOption(new_opts, true);
+  //}
+  //}
+
+  // outParams = JPetParams(new_opts, outParams.getParamManagerAsShared());
+  // INFO(Form(
+  //"GateTransformer finished, unzipped file name: %s", outputFile.c_str()
+  //));
+  return true;
+}
+
+// bool JPetGateTransformer::read()
+//{
+// if (entry_index < entries)
+// p_tree->GetEntry(entry_index);
+// else
+// return false;
+//++entry_index;
+// return true;
+//}
+
+// GateHit* JPetGateTransformer::get()
+//{
+// bool is_ok = parent_id == 0;
+// is_ok = is_ok && pdg == 22;
+// is_ok = is_ok && std::string(process_name) == "compt";
+// if (!is_ok)
+// return nullptr;
+//++counter;
+// gate_hit.track_id = track_id;
+// gate_hit.event_id = event_id;
+// gate_hit.edep = edep;
+// gate_hit.time = time;
+// gate_hit.posx = posx / 10.0; // mm --> cm
+// gate_hit.posy = posy / 10.0;
+// gate_hit.posz = posz / 10.0;
+// gate_hit.sourcex = sourcex;
+// gate_hit.sourcey = sourcey;
+// gate_hit.sourcez = sourcez;
+// gate_hit.sci_id = get_scintillator_id();
+// return &gate_hit;
+//}
+
+// void JPetGateTransformer::close()
+//{
+// p_file->Close();
+// delete p_file;
+// std::cout << counter << std::endl;
+//}
+
+// void JPetGateTransformer::set_geometry(DetectorGeometry dg) { detector_geometry = dg; }
+
+// int JPetGateTransformer::get_scintillator_id()
+//{
+// switch (detector_geometry)
+//{
+// case DetectorGeometry::kThreeLayers:
+// return 1 + volID[1];
+// case DetectorGeometry::kTwentyFourModules:
+// return 201 + volID[1] * 13 + volID[2];
+// default:
+// return 0;
+//};
+//}
+
+// void JPetGateTransformer::set_input_file_path(std::string path) { input_file_path = path; }
+
+void Writer::init()
+{
+  p_file = new TFile(output_file_path.c_str() /*"data.mcGate.root"*/, "RECREATE");
+  p_tree = new TTree("T", "T");
+  p_gate_hit = new GateHit();
+  p_tree->Branch("GHit", &p_gate_hit);
+}
+
+void Writer::write(GateHit gh)
+{
+  p_gate_hit->copy(gh);
+  p_tree->Fill();
+}
+
+void Writer::close()
+{
+  p_file->Write();
+  p_file->Close();
+}
+
+void Writer::test()
+{
+  init();
+  for (int i = 1; i < 4; ++i)
+  {
+    GateHit gh;
+    gh.event_id = i;
+    write(gh);
+  }
+  close();
+}
+
+void Writer::set_output_file_path(std::string path) { output_file_path = path; }
+
+void Reader::init()
+{
+
+  p_file = new TFile(input_file_path.c_str(), "READ");
+  p_tree = dynamic_cast<TTree*>(p_file->Get("Hits"));
+  entries = p_tree->GetEntries();
+
+  p_tree->SetBranchAddress("PDGEncoding", &pdg, &b_pdg);
+  p_tree->SetBranchAddress("trackID", &track_id, &b_track_id);
+  p_tree->SetBranchAddress("parentID", &parent_id, &b_parent_id);
+  p_tree->SetBranchAddress("time", &time, &b_time);
+  p_tree->SetBranchAddress("eventID", &event_id, &b_event_id);
+  p_tree->SetBranchAddress("posX", &posx, &b_posx);
+  p_tree->SetBranchAddress("posY", &posy, &b_posy);
+  p_tree->SetBranchAddress("posZ", &posz, &b_posz);
+  p_tree->SetBranchAddress("edep", &edep, &b_edep);
+  p_tree->SetBranchAddress("processName", process_name, &b_process_name);
+  p_tree->SetBranchAddress("sourcePosX", &sourcex, &b_sourcex);
+  p_tree->SetBranchAddress("sourcePosY", &sourcey, &b_sourcey);
+  p_tree->SetBranchAddress("sourcePosZ", &sourcez, &b_sourcez);
+  p_tree->SetBranchAddress("volumeID", &volID, &b_volID);
+}
+
+bool Reader::read()
+{
+  if (entry_index < entries)
+    p_tree->GetEntry(entry_index);
+  else
+    return false;
+  ++entry_index;
+  return true;
+}
+
+GateHit* Reader::get()
+{
+  bool is_ok = parent_id == 0;
+  is_ok = is_ok && pdg == 22;
+  is_ok = is_ok && std::string(process_name) == "compt";
+  if (!is_ok)
+    return nullptr;
+  ++counter;
+  gate_hit.track_id = track_id;
+  gate_hit.event_id = event_id;
+  gate_hit.edep = edep;
+  gate_hit.time = time;
+  gate_hit.posx = posx / 10.0; // mm --> cm
+  gate_hit.posy = posy / 10.0;
+  gate_hit.posz = posz / 10.0;
+  gate_hit.sourcex = sourcex;
+  gate_hit.sourcey = sourcey;
+  gate_hit.sourcez = sourcez;
+  gate_hit.sci_id = get_scintillator_id();
+  return &gate_hit;
+}
+
+void Reader::close()
+{
+  p_file->Close();
+  delete p_file;
+  std::cout << counter << std::endl;
+}
+
+void Reader::set_geometry(DetectorGeometry dg) { detector_geometry = dg; }
+
+int Reader::get_scintillator_id()
+{
+  switch (detector_geometry)
+  {
+  case DetectorGeometry::ThreeLayers:
+    return 1 + volID[1];
+  case DetectorGeometry::TwentyFourModules:
+    return 201 + volID[1] * 13 + volID[2];
+  default:
+    return 0;
+  };
+}
+
+void Reader::set_input_file_path(std::string path) { input_file_path = path; }
