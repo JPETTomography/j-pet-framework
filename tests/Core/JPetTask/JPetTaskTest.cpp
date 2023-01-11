31dceac4f283ea1fd7bff3b76a58137dd80beb66
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2023-01-10 22:01:34 -0300

    add test for JPetTask class

--- tests/Core/JPetTask/JPetTaskTest.cpp
+++ tests/Core/JPetTask/JPetTaskTest.cpp
@@ -1,5 +1,5 @@
 /**
- *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
+ *  @copyright Copyright 2022 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
@@ -19,8 +19,44 @@
 
 #include <boost/test/unit_test.hpp>
 
+class TestTask : public JPetTask
+{
+public:
+  explicit TestTask(const char* name = "") : JPetTask(name) {}
+  bool init(const JPetParams&) override { return true; }
+  bool run(const JPetDataInterface&) override { return true; }
+  bool terminate(JPetParams&) override { return true; }
+};
+
 BOOST_AUTO_TEST_SUITE(JPetTaskTestSuite)
 
-BOOST_AUTO_TEST_CASE(my_test) {}
+BOOST_AUTO_TEST_CASE(constructor_test)
+{
+  TestTask task;
+  BOOST_REQUIRE_EQUAL(task.getName(), "");
+  BOOST_REQUIRE_EQUAL(task.getSubTasks().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(task_name_test)
+{
+  TestTask task("the_task");
+  BOOST_REQUIRE_EQUAL(task.getName(), "the_task");
+  task.setName("same_task");
+  BOOST_REQUIRE_EQUAL(task.getName(), "same_task");
+}
+
+BOOST_AUTO_TEST_CASE(subtask_test)
+{
+  TestTask task("task");
+
+  std::unique_ptr<JPetTaskInterface> pTask1;
+  std::unique_ptr<JPetTaskInterface> pTask2;
+  std::unique_ptr<JPetTaskInterface> pTask3;
+  task.addSubTask(std::move(pTask1));
+  task.addSubTask(std::move(pTask2));
+  task.addSubTask(std::move(pTask3));
+
+  BOOST_REQUIRE_EQUAL(task.getSubTasks().size(), 3);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
